81cc9cbba5b6d3c4cdbdf32f6bf525958b309c30
Ty Voliter <tvoliter@cs.stanford.edu>
2011-12-12 12:08:11 -0800

    Checking in initial fileIO.js implementation

--- src/brackets.js
+++ src/brackets.js
@@ -1,9 +1,74 @@
 /* TODO: copyright notice, etc. */
 
 $(document).ready(function() {
+
 	var myCodeMirror = CodeMirror($('#editor').get(0), {
 		value: 'var myResponse="Yes, it will be!"\n'
 	});
 
-	// TODO: Write Brackets.
+
+	
+
+
+	// Set the "inBrowser" flag
+	var inBrowser = !window.hasOwnProperty("brackets");
+
+
+	// Temporary button to test file directory traversa;
+	$("#open-folder").click(function(){
+		if (!inBrowser) {
+			var foldername = "/Users/tvoliter/github/brackets-app/brackets"; // brackets.file.showOpenPanel(false, true, "Choose a folder");
+			
+			if (foldername != "")
+				var rootEntry = window.ProjectManager.requestNativeFileSystem( foldername, null, null ); // TODO: add callbacks
+						
+			var nestingLevel = 0;
+						
+			if( rootEntry.isDirectory )
+				readDirectory( rootEntry )
+			
+			
+			
+			// Test directory traversal
+			function readDirectory( entry ){
+				
+			
+				var reader = entry.createReader();
+				reader.readEntries( dirReaderSuccessCB, dirReaderErrorCB);
+			}
+			
+			function dirReaderSuccessCB( entries ){
+				var tabs = "";
+				for( i = 0; i < nestingLevel; i++ ){
+					tabs += "  ";
+				}
+			
+				for ( var entryI in entries ){
+					var entry = entries[entryI];
+					if( entry.isFile ){
+						// create leaf tree node using entry.name
+						console.log( tabs+ entry.name );
+					}
+					else if ( entry.isDirectory ){
+						// create branch tree node using entry.name
+						console.log( tabs + entry.name );
+						
+						nestingLevel++;
+						readDirectory( entry )
+					}
+				}
+			}
+
+			
+						
+			function dirReaderErrorCB() {
+				// handle error
+			}
+			
+			
+
+		}
+	});
+	
+	
 });
