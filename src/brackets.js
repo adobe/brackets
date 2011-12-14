0ddee95d80d24c6b91702e4d35fa87a120db5690
Ty Voliter <tvoliter@cs.stanford.edu>
2011-12-14 15:34:44 -0800

    integrate changes from origin/master

--- src/brackets.js
+++ src/brackets.js
@@ -1,4 +1,12 @@
-/* TODO: copyright notice, etc. */
+/*
+ * Copyright 2011 Adobe Systems Incorporated. All Rights Reserved.
+ */
+
+// Define core brackets namespace
+brackets = window.brackets || {};
+
+brackets.inBrowser = !brackets.hasOwnProperty("fs");
+
 
 $(document).ready(function() {
 
@@ -6,20 +14,39 @@ $(document).ready(function() {
         value: 'var myResponse="Yes, it will be!"\n'
     });
 
-    // Set the "inBrowser" flag
-    var inBrowser = !window.hasOwnProperty("brackets");
-
-
-    // Temporary button to test file directory traversa;
-    $("#menu-file-open").click(function(){
-        if (!inBrowser) {
-            window.NativeFileSystem.showOpenDialog( false, true, "Choose a folder",
-                                                    null, null,
-                                                    showOpenDialogSuccessCallback,
-                                                    showOpenDialogErrorCallback);
+	// Load a default project into the tree
+	if (brackets.inBrowser) {
+	    // In browser: dummy folder tree (hardcoded in ProjectManager)
+		ProjectManager.loadProject("DummyProject");
+	} else {
+	    // In app shell: load Brackets itself
+	    var loadedPath = window.location.pathname;
+	    var bracketsSrc = loadedPath.substr(0, loadedPath.lastIndexOf("/"));
+	    ProjectManager.loadProject(bracketsSrc);
+	}
+	
+	// Open project button
+	$("#btn-open-project").click(function() {
+		ProjectManager.openProject();
+	});
+    
+    
+    // Implements the 'Run Tests' menu to bring up the Jasmine unit test window
+    var testWindow = null;
+    $("#menu-runtests").click(function(){
+        if (!(testWindow === null)) {
+            try {
+                testWindow.location.reload();
+            } catch(e) {
+                testWindow = null;  // the window was probably closed
+            } 
+        }
+        
+        if (testWindow === null) {
+            testWindow = window.open("../test/SpecRunner.html");
+            testWindow.location.reload(); // if it was opened before, we need to reload because it will be cached
         }
     });
-    
     
     // Ty test code hooked up to "new" menu. Test reads a file and prints its constents to the log.
     // uncomment to test
@@ -65,84 +92,6 @@ $(document).ready(function() {
             };
         }
     });*/
-    
-    
-    // Implements the 'Run Tests' menu to bring up the Jasmine unit test window
-    var testWindow = null;
-    $("#menu-runtests").click(function(){
-        if (!(testWindow === null)) {
-            try {
-                testWindow.location.reload();
-            } catch(e) {
-                testWindow = null;  // the window was probably closed
-            } 
-        }
-        
-        if (testWindow === null) {
-            testWindow = window.open("../test/SpecRunner.html");
-            testWindow.location.reload(); // if it was opened before, we need to reload because it will be cached
-        }
-    });
 
-    function showOpenDialogErrorCallback( err ){
-        console.log( err )
-    }
 
-    function showOpenDialogSuccessCallback( files ) {
-        var folderName = files instanceof Array ? files[0] : files;
-        var nestingLevel = 0;
-    
-        if (folderName != "") {
-            window.NativeFileSystem.requestNativeFileSystem( folderName, 
-                                                                             requestNativeFileSystemSuccessCB, requestNativeFileSystemErrorCB ); 
-                    
-            
-        }
-        
-        function requestNativeFileSystemSuccessCB( rootEntry ){
-            var nestingLevel = 0;
-                    
-            if( rootEntry && rootEntry.isDirectory )
-                readDirectory( rootEntry );
-        }
-        
-        function requestNativeFileSystemErrorCB( err){
-            console.log( err );
-        }
-        
-        
-        // Test directory traversal
-        function readDirectory( entry ){
-            
-            var reader = entry.createReader();
-            reader.readEntries( dirReaderSuccessCB, dirReaderErrorCB);
-        }
-        
-        function dirReaderSuccessCB( entries ){
-            var tabs = "";
-            for( i = 0; i < nestingLevel; i++ ){
-                tabs += "  ";
-            }
-        
-            for ( var entryI in entries ){
-                var entry = entries[entryI];
-                if( entry.isFile ){
-                    // create leaf tree node using entry.name
-                    console.log( tabs+ entry.name );
-                }
-                else if ( entry.isDirectory ){
-                    // create branch tree node using entry.name
-                    console.log( tabs + entry.name );
-                    
-                    nestingLevel++;
-                    readDirectory( entry )
-                }
-            }
-        }
-        
-        function dirReaderErrorCB() {
-            // handle error
-        }
-    }
-            
 });
