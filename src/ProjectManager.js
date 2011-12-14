754b3542944860b6fdc5280c607b9558f415b717
Peter Flynn <pflynn@adobe.com>
2011-12-13 18:17:06 -0800

    Clean up file tree code and add more JSDoc (with Closure annotations)

--- src/ProjectManager.js
+++ src/ProjectManager.js
@@ -4,6 +4,14 @@
 
 var ProjectManager = {};
 
+
+/**
+ * The root folder of the currently loaded project, or null if no project is open (during startup).
+ * @type {DirectoryEntry}
+ */
+ProjectManager.projectRoot = null;
+
+
 /**
  * Displays a browser dialog where the user can choose a folder to load.
  * (If the user cancels the dialog, nothing ore happens).
@@ -35,6 +43,7 @@ ProjectManager.loadProject = function(rootPath) {
     // Populate file tree
     if (brackets.inBrowser) {
         // Hardcoded dummy data for local testing, in jsTree JSON format
+        // (we leave ProjectManager.projectRoot null)
         var subfolderInner = { data:"Folder_inner", children:[
             { data: "subsubfile_1" }, { data: "subsubfile_2" }
         ] };
@@ -54,134 +63,86 @@ ProjectManager.loadProject = function(rootPath) {
         ProjectManager._renderTree(treeJSONData);
         
     } else {
-        // Actually scan the folder structure on local disk
-        var rootEntry = NativeFileSystem.requestNativeFileSystem(rootPath, null, null);  // TODO: add success/error callbacks
+        // Point at a real folder structure on local disk
+        ProjectManager.projectRoot = NativeFileSystem.requestNativeFileSystem(rootPath, null, null);
+        // TODO: add success callback (should work asynchronously)
+        // TODO: real error handling
         
-        var reader = rootEntry.createReader();
-        var topLevelItems = [];
-        
-        function convertEntriesToJSON(entries) {
-            var jsonEntryList = [];
-            for (var entryI in entries) {
-                var entry = entries[entryI];
-                
-                var jsonEntry = {
-                    data: entry.name,
-                    metadata: { entry: entry }
-                };
-                if (entry.isDirectory) {
-                    jsonEntry.children = [];
-                    jsonEntry.state = "closed";
-                }
-                jsonEntryList.push(jsonEntry);
-            }
-            return jsonEntryList;
-        }
-        
-        reader.readEntries(function(entries) {
-            topLevelItems = convertEntriesToJSON(entries);
-            
-            // This will cause the tree to start calling us, pulling down the root's immediate children
-            ProjectManager._renderTree(dataProvider);
-        },
-        function(error) {
-            console.log(error);     // TODO: real error handling
-        });
-        
-        function dataProvider(treeNode, callback) {
-            // Special case: root
-            if (treeNode == -1) {
-                console.log("Displaying prefecthed root...");
-                callback(topLevelItems);
-            } else {
-                var entry = treeNode.data("entry");
-                console.log("Fetching subtree: "+entry.fullPath);
-
-                entry.createReader().readEntries(
-                    function(entries) {
-                        var subtreeJSON = convertEntriesToJSON(entries);
-                        callback(subtreeJSON);
-                    },
-                    function(error) {
-                        console.log(error);     // TODO: real error handling
-                    }
-                );
-            }
-        }
-
-        // var rootedTreeJSONData = readDirectory(rootEntry);
-        // treeJSONData = rootedTreeJSONData.children;      // we don't want the root folder to be an actual tree node
-        
-        // function readDirectory(entry){
-        //     var reader = entry.createReader();
-        //     var jsonItem = {
-        //         data: entry.name,
-        //         children: []
-        //     };
-        //     reader.readEntries(dirReaderSuccessCB, dirReaderErrorCB);
-        // }
-        //         function dirReaderSuccessCB( entries ){
-        //             var tabs = "";
-        //             for( i = 0; i < nestingLevel; i++ ){
-        //                 tabs += "  ";
-        //             }
-        //         
-        //             for ( var entryI in entries ){
-        //                 var entry = entries[entryI];
-        //                 if( entry.isFile ){
-        //                     // create leaf tree node using entry.name
-        //                     console.log( tabs+ entry.name );
-        //                 }
-        //                 else if ( entry.isDirectory ){
-        //                     // create branch tree node using entry.name
-        //                     console.log( tabs + entry.name );
-        //                     
-        //                     nestingLevel++;
-        //                     readDirectory( entry )
-        //                 }
-        //             }
-        //         }
-        // 
-        // // Convert to jsTree's JSON model format
-        // function _renderSubtree(fileListData) {
-        //  var result = [];
-        //  $(fileListData).each(function(index, item) {
-        //      var entry = {
-        //          data: (item.folder ? item.name : item)
-        //          // for additional options, see http://www.jstree.com/documentation/json_data
-        //      };
-        //      if (item.folder) {
-        //          entry.children = _renderSubtree(item.items);
-        //      }
-        // 
-        //      result.push(entry);
-        //  });
-        //  return result;
-        // }
-        // 
-        // var treeJSON = _renderSubtree(fileListData);
-
-        // TODO: fetch from real file APIs
-        // var projectFiles = JSON.parse(brackets.file.getDirectoryListing(rootPath)); //eval(
-        // fileListData = [];
-        
-        // projectFiles.forEach(function(index, item) {
-            // // Filter down to just visible files
-            // if (ProjectManager.shouldShowFile(item)) {
-                // if (brackets.file.isDirectory(rootPath + "/" + item))
-                    // fileListData.push( { name:item, folder:true, items:[] } );
-                // else
-                    // fileListData.push(item);
-            // }
-        // });
+        // The tree will invoke our "data provider" function to populate the top-level items, then
+        // go idle until a node is expanded - at which time it'll call us again to fetch the node's
+        // immediate children, and so on.
+        ProjectManager._renderTree(ProjectManager._treeDataProvider);
     }
-    
-    // ProjectManager._currentProjectRoot = rootPath;
 };
 
 /**
  * @private
+ * Called by jsTree when the user has expanded a node that has never been expanded before. We call
+ * jsTree back asynchronously with the node's immediate children data once the subfolder is done
+ * being fetched.
+ * 
+ * @param {jQueryObject} treeNode  jQ object for the DOM node being expanded
+ * @param {function(Array)} jsTreeCallback  jsTree callback to provide children to
+ */
+ProjectManager._treeDataProvider = function(treeNode, jsTreeCallback) {    
+    var dirEntry;
+    
+    if (treeNode == -1) {
+        // Special case: root of tree
+        dirEntry = ProjectManager.projectRoot;
+    } else {
+        // All other nodes: the DirectoryEntry is saved as jQ data in the tree (by _convertEntriesToJSON())
+        dirEntry = treeNode.data("entry");
+    }
+    
+    // Fetch dirEntry's contents
+    dirEntry.createReader().readEntries(
+        function(entries) {
+            var subtreeJSON = ProjectManager._convertEntriesToJSON(entries);
+            jsTreeCallback(subtreeJSON);
+        },
+        function(error) {
+            console.log(error);     // TODO: real error handling
+        }
+    );
+    
+}
+
+/**
+ * @private
+ * Given an array of NativeFileSystem entries, returns a JSON array representing them in the format
+ * required by jsTree. Saves the corresponding Entry object as metadata (which jsTree will store in
+ * the DOM via $.data()).
  *
+ * Does NOT recursively traverse the file system: folders are marked as expandable but are given no
+ * children initially.
+ *
+ * @param {Array.<Entry>} entries  Array of NativeFileSystem entry objects.
+ * @return {Array} jsTree node data: array of JSON objects
+ */
+ProjectManager._convertEntriesToJSON = function(entries) {
+    var jsonEntryList = [];
+    for (var entryI in entries) {
+        var entry = entries[entryI];
+        
+        var jsonEntry = {
+            data: entry.name,
+            metadata: { entry: entry }
+        };
+        if (entry.isDirectory) {
+            jsonEntry.children = [];
+            jsonEntry.state = "closed";
+        }
+        // For more info on jsTree's JSON format see: http://www.jstree.com/documentation/json_data
+        
+        jsonEntryList.push(jsonEntry);
+    }
+    return jsonEntryList;
+}
+
+
+/**
+ * @private
  * Given an input to jsTree's json_data.data setting, display the data in the file tree UI
  * (replacing any existing file tree that was previously displayed). This input could be
  * raw JSON data, or it could be a dataprovider function. See jsTree docs for details:
@@ -196,10 +157,12 @@ ProjectManager._renderTree = function(treeDataProvider) {
     projectTreeContainer.jstree({
         plugins : ["ui", "themes", "json_data"],
         json_data : { data:treeDataProvider },
+        
         core : { animation:0 },
         themes : { theme:"brackets", url:"styles/jsTreeTheme.css", dots:false, icons:false },
             //(note: our actual jsTree theme CSS lives in brackets.less; we specify an empty .css
             // file because jsTree insists on loading one itself)
+        
         strings : { loading : "Loading ...", new_node : "New node" }    // TODO: localization
     });
 };
