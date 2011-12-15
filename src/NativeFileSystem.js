0bc6854366a54004a9fe15de3c9405d3eafeae96
Jason San Jose <jasonsj@adobe.com>
2011-12-14 16:00:50 -0800

    Initial commit for FileWriter

--- src/NativeFileSystem.js
+++ src/NativeFileSystem.js
@@ -10,9 +10,9 @@ window.NativeFileSystem = {
      * @param {string[]} fileTypes
      * @param {function} resultCallback
      * @constructor
-     */ 
+     */
     showOpenDialog: function (  allowMultipleSelection,
-                                chooseDirectories, 
+                                chooseDirectories,
                                 title,
                                 initialPath,
                                 fileTypes,
@@ -20,9 +20,9 @@ window.NativeFileSystem = {
                                 errorCallback ) {
         if( !successCallback )
             return;
-            
+
         var files = brackets.fs.showOpenDialog( allowMultipleSelection,
-            chooseDirectories, 
+            chooseDirectories,
             title,
             initialPath,
             fileTypes,
@@ -31,8 +31,8 @@ window.NativeFileSystem = {
                     successCallback( data );
                 else if (errorCallback)
                     errorCallback(NativeFileSystem._nativeToFileError(err));
-            });                                                   
-        
+            });
+
     },
 
     /** requestNativeFileSystem
@@ -50,14 +50,14 @@ window.NativeFileSystem = {
             else if (errorCallback) {
                 errorCallback(NativeFileSystem._nativeToFileError(err));
             }
-        }); 
+        });
     },
-     
+
     _nativeToFileError: function(nativeErr) {
         // The HTML file spec says SECURITY_ERR is a catch-all to be used in situations
         // not covered by other error codes.
         var error = FileError.SECURITY_ERR;
-        
+
         switch (nativeErr) {
             // We map ERR_UNKNOWN and ERR_INVALID_PARAMS to SECURITY_ERR,
             // since there aren't specific mappings for these.
@@ -65,20 +65,20 @@ window.NativeFileSystem = {
             case brackets.fs.ERR_INVALID_PARAMS:
                 error = FileError.SECURITY_ERR;
                 break;
-                
+
             case brackets.fs.ERR_NOT_FOUND:
                 error = FileError.NOT_FOUND_ERR;
                 break;
             case brackets.fs.ERR_CANT_READ:
                 error = FileError.NOT_READABLE_ERR;
                 break;
-                
+
             // It might seem like you should use FileError.ENCODING_ERR for this,
-            // but according to the spec that's for malformed URLs.            
+            // but according to the spec that's for malformed URLs.
             case brackets.fs.ERR_UNSUPPORTED_ENCODING:
                 error = FileError.SECURITY_ERR;
                 break;
-                
+
             case brackets.fs.ERR_CANT_WRITE:
                 error = FileError.NO_MODIFICATION_ALLOWED_ERR;
                 break;
@@ -86,7 +86,7 @@ window.NativeFileSystem = {
                 error = FileError.QUOTA_EXCEEDED_ERR;
                 break;
         }
-        
+
         return new FileError(error);
     }
 };
@@ -102,7 +102,7 @@ Entry = function( fullPath, isDirectory) {
     this.isFile = !isDirectory;
     // IMPLEMENT LATER void      getMetadata (MetadataCallback successCallback, optional ErrorCallback errorCallback);
     this.fullPath = fullPath;
-    
+
     // Extract name from fullPath
     this.name = null; // default if extraction fails
     if( fullPath ){
@@ -110,7 +110,7 @@ Entry = function( fullPath, isDirectory) {
         if( pathParts.length > 0 )
             this.name = pathParts.pop();
     }
-        
+
     // IMPLEMENT LATER var filesystem;
     // IMPLEMENT LATER void      moveTo (DirectoryEntry parent, optional DOMString newName, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
     // IMPLEMENT LATER void      copyTo (DirectoryEntry parent, optional DOMString newName, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
@@ -126,19 +126,18 @@ Entry = function( fullPath, isDirectory) {
  * @param {string} name
  * @constructor
  * @extends {Entry}
- */ 
+ */
 FileEntry = function( name ) {
     Entry.call(this, name, false);
-    
+
     // TODO: make FileEntry actually inherit from Entry by modifying prototype. I don't know how to do this yet.
-    
-    // IMPLEMENT LATER void createWriter (FileWriterCallback successCallback, optional ErrorCallback errorCallback);
+
     // IMPLEMENT LATER void file (FileCallback successCallback, optional ErrorCallback errorCallback);
 };
 
 /** createWriter
- * 
-   Creates a new FileWriter associated with the file that this FileEntry represents. 
+ *
+   Creates a new FileWriter associated with the file that this FileEntry represents.
  *
  * @param {function} successCallback
  * @param {function} errorCallback
@@ -152,13 +151,13 @@ FileEntry.prototype.createWriter = function( successCallback, errorCallback ) {
  *
  * @param {Blob} data
  * @constructor
- */ 
+ */
 FileSaver = function( data ) {
     _data = data;
 };
 
 // FileSaver constants
-Object.defineProperties(FileSaver, 
+Object.defineProperties(FileSaver,
     { INIT:     { value: 1 }
     , WRITING:  { value: 2 }
     , DONE:     { value: 3 }
@@ -176,12 +175,12 @@ FileSaver.prototype.abort = function() {
     // If readyState is DONE or INIT, terminate this overall series of steps without doing anything else..
     if (_readyState == FileSaver.INIT || _readyState == FileSaver.DONE)
         return;
-    
+
     // Terminate any steps having to do with writing a file.
 
     // Set the error attribute to a FileError object with the code ABORT_ERR.
     _error = new FileError(FileError.ABORT_ERR);
-    
+
     // Set readyState to DONE.
     _readyState = FileSaver.DONE;
 
@@ -189,17 +188,36 @@ FileSaver.prototype.abort = function() {
     // Dispatch a progress event called writeend
     // Stop dispatching any further progress events.
     // Terminate this overall set of steps.
-    
-    return err;  
+
+    return err;
 };
 
 /** class: FileWriter
  *
  * @constructor
  * @extends {FileSaver}
- */ 
+ */
 FileWriter = function( ) {
     FileSaver.call(this);
+};
+
+// FileWriter private memeber vars
+FileWriter.prototype._length = 0;
+FileWriter.prototype._position = 0;
+
+Object.defineProperties(FileWriter,
+    { length:   { value: function() { return this._length; }}
+    , position: { value: function() { return this._position; }}
+    }
+);
+
+FileWriter.prototype.write = function( data ) {
+};
+
+FileWriter.prototype.seek = function( offset ) {
+};
+
+FileWriter.prototype.truncate = function( size ) {
 };
 
 /** class: DirectoryEntry
@@ -207,10 +225,10 @@ FileWriter = function( ) {
  * @param {string} name
  * @constructor
  * @extends {Entry}
- */ 
+ */
 DirectoryEntry = function( name ) {
     Entry.call(this, name, true);
-    
+
     // TODO: make DirectoryEntry actually inherit from Entry by modifying prototype. I don't know how to do this yet.
 
     // IMPLEMENT LATERvoid            getFile (DOMString path, optional Flags options, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
@@ -222,15 +240,15 @@ DirectoryEntry = function( name ) {
 DirectoryEntry.prototype.createReader = function() {
     var dirReader = new DirectoryReader();
     dirReader._directory = this;
-    
+
     return dirReader;
 };
 
 
 /** class: DirectoryReader
- */ 
+ */
 DirectoryReader = function() {
-    
+
 };
 
 
@@ -239,7 +257,7 @@ DirectoryReader = function() {
  * @param {function} successCallback
  * @param {function} errorCallback
  * @returns {Entry[]}
- */ 
+ */
 DirectoryReader.prototype.readEntries = function( successCallback, errorCallback ){
     var rootPath = this._directory.fullPath;
     var jsonList = brackets.fs.readdir( rootPath, function( err, filelist ) {
@@ -248,33 +266,33 @@ DirectoryReader.prototype.readEntries = function( successCallback, errorCallback
             var entries = [];
             filelist.forEach(function(item){
                 var itemFullPath = rootPath + "/" + item;
-                
+
                 brackets.fs.stat( itemFullPath, function( err, statData) {
-                
+
                     if( !err ){
                         if( statData.isDirectory( itemFullPath ) )
                             entries.push( new DirectoryEntry( itemFullPath ) );
-                        else if( statData.isFile( itemFullPath ) ) 
+                        else if( statData.isFile( itemFullPath ) )
                             entries.push( new FileEntry( itemFullPath ) );
                     }
                     else if (errorCallback) {
                         errorCallback(NativeFileSystem._nativeToFileError(err));
                     }
-                    
-                })    
+
+                })
             });
 
-            successCallback( entries );        
+            successCallback( entries );
         }
         else if (errorCallback) {
             errorCallback(NativeFileSystem._nativeToFileError(err));
         }
-    });    
+    });
 };
 
 /** class: FileError
  *
- * Implementation of HTML file API error code return class. Note that the 
+ * Implementation of HTML file API error code return class. Note that the
  * various HTML file API specs are not consistent in their definition of
  * some error code values like ABORT_ERR; I'm using the definitions from
  * the Directories and System spec since it seems to be the most
@@ -289,7 +307,7 @@ FileError = function(code) {
 };
 
 // FileSaver constants
-Object.defineProperties(FileError, 
+Object.defineProperties(FileError,
     { NOT_FOUND_ERR:                { value: 1 }
     , SECURITY_ERR:                 { value: 2 }
     , ABORT_ERR:                    { value: 3 }
--- test/spec/NativeFileSystem-test.js
+++ test/spec/NativeFileSystem-test.js
@@ -1,101 +1,122 @@
-describe("NativeFileSystem", function(){
+describe("NativeFileSystem", function() {
 
-  describe("Reading", function() {
-
-    beforeEach(function() {
-      this.addMatchers({
-        toContainDirectoryWithName: function(expected) {
-          for (var i = 0 ; i < this.actual.length; ++i) {
-            if (this.actual[i].isDirectory && this.actual[i].name === expected) {
-              return true;
-            }
-          }
-          return false;
-        }
-        , toContainFileWithName: function(expected) {
-          for (var i = 0 ; i < this.actual.length; ++i) {
-            if (this.actual[i].isFile && this.actual[i].name === expected) {
-              return true;
-            }
-          }
-          return false;                   
-        }
-      });
-
-      this.path = SpecRunnerUtils.getTestPath("/spec/NativeFileSystem-test-files");
+    beforeEach(function () {
+        this.path = SpecRunnerUtils.getTestPath("/spec/NativeFileSystem-test-files");
     });
 
-    it("should read a directory from disk", function() {
-      var entries = null;
-      var readComplete = false;
+    describe("Reading", function() {
 
-      var nfs = window.NativeFileSystem.requestNativeFileSystem(this.path, requestNativeFileSystemSuccessCB);      
-        function requestNativeFileSystemSuccessCB( nfs ){
-            var reader = nfs.createReader();
-        
-            var successCallback = function(e) { entries = e; readComplete = true; }
-            // TODO: not sure what parameters error callback will take because it's not implemented yet
-            var errorCallback = function() { readComplete = true; }
-            
-            reader.readEntries(successCallback, errorCallback);            
-            
-            waitsFor(function() { return readComplete; }, 1000);
-            
-            runs(function() {
-                expect(entries).toContainDirectoryWithName("dir1");
-                expect(entries).toContainFileWithName("file1");
-                expect(entries).not.toContainFileWithName("file2");
+        beforeEach(function() {
+            this.addMatchers({
+                toContainDirectoryWithName: function(expected) {
+                    for (var i = 0 ; i < this.actual.length; ++i) {
+                        if (this.actual[i].isDirectory && this.actual[i].name === expected) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                , toContainFileWithName: function(expected) {
+                    for (var i = 0 ; i < this.actual.length; ++i) {
+                        if (this.actual[i].isFile && this.actual[i].name === expected) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
             });
-        }  
+        });
+
+        it("should read a directory from disk", function() {
+            var entries = null;
+            var readComplete = false;
+
+            var nfs = window.NativeFileSystem.requestNativeFileSystem(this.path, requestNativeFileSystemSuccessCB);
+            function requestNativeFileSystemSuccessCB( nfs ){
+                var reader = nfs.createReader();
+
+                var successCallback = function(e) { entries = e; readComplete = true; }
+                // TODO: not sure what parameters error callback will take because it's not implemented yet
+                var errorCallback = function() { readComplete = true; }
+
+                reader.readEntries(successCallback, errorCallback);
+
+                waitsFor(function() { return readComplete; }, 1000);
+
+                runs(function() {
+                    expect(entries).toContainDirectoryWithName("dir1");
+                    expect(entries).toContainFileWithName("file1");
+                    expect(entries).not.toContainFileWithName("file2");
+                });
+            }
+        });
+
+        it("should return an error if the directory doesn't exist", function() {
+            var successCalled = false, errorCalled = false, error = null;
+            window.NativeFileSystem.requestNativeFileSystem(this.path + '/nonexistent-dir', function(data) {
+                successCalled = true;
+            }, function(err) {
+                errorCalled = true;
+                error = err;
+            });
+
+            waitsFor(function() { return successCalled || errorCalled; }, 1000);
+
+            runs(function() {
+                expect(successCalled).toBe(false);
+                expect(errorCalled).toBe(true);
+                expect(error.code).toBe(FileError.NOT_FOUND_ERR);
+            });
+        });
+
+        it("should return an error if you pass a bad parameter", function() {
+            var successCalled = false, errorCalled = false, error = null;
+            window.NativeFileSystem.requestNativeFileSystem(0xDEADBEEF, function(data) {
+                successCalled = true;
+            }, function(err) {
+                errorCalled = true;
+                error = err;
+            });
+
+            waitsFor(function() { return successCalled || errorCalled; }, 1000);
+
+            runs(function() {
+                expect(successCalled).toBe(false);
+                expect(errorCalled).toBe(true);
+                expect(error.code).toBe(FileError.SECURITY_ERR);
+            });
+        });
+
+        it("should be okay to not pass an error callback", function() {
+            var entries = null;
+            window.NativeFileSystem.requestNativeFileSystem(this.path, function(data) {
+                entries = data;
+            });
+
+            waitsFor(function() { return entries != null; }, 1000);
+
+            runs(function() {
+                expect(entries).not.toBe(null);
+            });
+        });
     });
-    
-    it("should return an error if the directory doesn't exist", function() {
-        var successCalled = false, errorCalled = false, error = null;
-        window.NativeFileSystem.requestNativeFileSystem(this.path + '/nonexistent-dir', function(data) {
-            successCalled = true;
-        }, function(err) {
-            errorCalled = true;
-            error = err;
+
+    describe("Writing", function() {
+
+        it("should write new files", function() {
+           expect(1);
         });
-        
-        waitsFor(function() { return successCalled || errorCalled; }, 1000);
-        
-        runs(function() {
-            expect(successCalled).toBe(false);
-            expect(errorCalled).toBe(true);
-            expect(error.code).toBe(FileError.NOT_FOUND_ERR);
+
+        it("should append to existing files", function() {
+           expect(1);
         });
-    });
-    
-    it("should return an error if you pass a bad parameter", function() {
-        var successCalled = false, errorCalled = false, error = null;
-        window.NativeFileSystem.requestNativeFileSystem(0xDEADBEEF, function(data) {
-            successCalled = true;
-        }, function(err) {
-            errorCalled = true;
-            error = err;
+
+        it("should seek into a file before writing", function() {
+           expect(1);
         });
-        
-        waitsFor(function() { return successCalled || errorCalled; }, 1000);
-        
-        runs(function() {
-            expect(successCalled).toBe(false);
-            expect(errorCalled).toBe(true);
-            expect(error.code).toBe(FileError.SECURITY_ERR);
-        });        
-    });
-    
-    it("should be okay to not pass an error callback", function() {
-        var entries = null;
-        window.NativeFileSystem.requestNativeFileSystem(this.path, function(data) {
-            entries = data;
-        });
-        
-        waitsFor(function() { return entries != null; }, 1000);
-        
-        runs(function() {
-            expect(entries).not.toBe(null);
+
+        it("should truncate files", function() {
+           expect(1);
         });
     });
-  });
 });
