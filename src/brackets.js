c94ebc139b7ca8ae681c71d28e051f4270d493c4
Peter Flynn <pflynn@adobe.com>
2012-01-06 17:58:37 -0800

    Fixup indentation that was broken on Ty's end, probably by him making the same mistake when merging from master as I did earlier.

--- src/brackets.js
+++ src/brackets.js
@@ -17,10 +17,10 @@ define(function(require, exports, module) {
     ,   KeyMap                  = require("KeyBindingManager").KeyMap
     ,   Commands                = require("Commands")
     ;
-
-// Define core brackets namespace
-brackets = window.brackets || {};
-
+    
+    // Define core brackets namespace
+    brackets = window.brackets || {};
+    
     // TODO: Make sure the "test" object is not included in final builds
     // All modules that need to be tested from the context of the application 
     // must to be added to this object. The unit tests cannot just pull
@@ -33,180 +33,180 @@ brackets = window.brackets || {};
         , CommandManager        : require("CommandManager")
         };
 
-brackets.inBrowser = !brackets.hasOwnProperty("fs");
+    brackets.inBrowser = !brackets.hasOwnProperty("fs");
 
-brackets.DIALOG_BTN_CANCEL = "cancel";
-brackets.DIALOG_BTN_OK = "ok";
-brackets.DIALOG_BTN_DONTSAVE = "dontsave";
+    brackets.DIALOG_BTN_CANCEL = "cancel";
+    brackets.DIALOG_BTN_OK = "ok";
+    brackets.DIALOG_BTN_DONTSAVE = "dontsave";
 
-brackets.DIALOG_ID_ERROR = "error-dialog";
-brackets.DIALOG_ID_SAVE_CLOSE = "save-close-dialog";
+    brackets.DIALOG_ID_ERROR = "error-dialog";
+    brackets.DIALOG_ID_SAVE_CLOSE = "save-close-dialog";
 
-/**
- * General purpose modal dialog. Assumes that the HTML for the dialog contains elements with "title"
- * and "message" classes, as well as a number of elements with "dialog-button" class, each of which has
- * a "data-button-id".
- *
- * @param {string} id The ID of the dialog node in the HTML.
- * @param {string} title The title of the error dialog. Can contain HTML markup.
- * @param {string} message The message to display in the error dialog. Can contain HTML markup.
- * @return {Deferred} a $.Deferred() that will be resolved with the ID of the clicked button when the dialog
- *     is dismissed.
- */
-brackets.showModalDialog = function(id, title, message, callback) {
-    var result = $.Deferred();
-    var dlg = $("#" + id);
+    /**
+     * General purpose modal dialog. Assumes that the HTML for the dialog contains elements with "title"
+     * and "message" classes, as well as a number of elements with "dialog-button" class, each of which has
+     * a "data-button-id".
+     *
+     * @param {string} id The ID of the dialog node in the HTML.
+     * @param {string} title The title of the error dialog. Can contain HTML markup.
+     * @param {string} message The message to display in the error dialog. Can contain HTML markup.
+     * @return {Deferred} a $.Deferred() that will be resolved with the ID of the clicked button when the dialog
+     *     is dismissed.
+     */
+    brackets.showModalDialog = function(id, title, message, callback) {
+        var result = $.Deferred();
+        var dlg = $("#" + id);
     
-    // Set title and message
-    $(".dialog-title", dlg).html(title);
-    $(".dialog-message", dlg).html(message);
+        // Set title and message
+        $(".dialog-title", dlg).html(title);
+        $(".dialog-message", dlg).html(message);
     
-    // Click handler for buttons
-    dlg.on("click", ".dialog-button", function(e) {
-        result.resolve($(this).attr("data-button-id"));
-        dlg.modal(true).hide();
-    });
+        // Click handler for buttons
+        dlg.on("click", ".dialog-button", function(e) {
+            result.resolve($(this).attr("data-button-id"));
+            dlg.modal(true).hide();
+        });
     
-    // Enter/Return handler for the primary button. Need to 
-    // add both keydown and keyup handlers here to make sure
-    // the enter key was pressed while the dialog was showing.
-    // Otherwise, if a keydown or keypress from somewhere else
-    // triggered an alert, the keyup could immediately dismiss it.
-    var enterKeyPressed = false;
-    $(document).on("keydown.modal", function(e) {
-        if (e.keyCode === 13) {
-            enterKeyPressed = true;
-        }
-    }).on("keyup.modal", function(e) {
-        if (e.keyCode === 13 && enterKeyPressed) {
-            var primaryBtn = dlg.find(".primary");
-            if (primaryBtn) {
-                result.resolve(primaryBtn.attr("data-button-id"));
-                dlg.modal(true).hide();
+        // Enter/Return handler for the primary button. Need to 
+        // add both keydown and keyup handlers here to make sure
+        // the enter key was pressed while the dialog was showing.
+        // Otherwise, if a keydown or keypress from somewhere else
+        // triggered an alert, the keyup could immediately dismiss it.
+        var enterKeyPressed = false;
+        $(document).on("keydown.modal", function(e) {
+            if (e.keyCode === 13) {
+                enterKeyPressed = true;
             }
-        }
-        enterKeyPressed = false;
-    });
+        }).on("keyup.modal", function(e) {
+            if (e.keyCode === 13 && enterKeyPressed) {
+                var primaryBtn = dlg.find(".primary");
+                if (primaryBtn) {
+                    result.resolve(primaryBtn.attr("data-button-id"));
+                    dlg.modal(true).hide();
+                }
+            }
+            enterKeyPressed = false;
+        });
     
     
-    // Run the dialog
-    dlg.modal(
-        { backdrop: "static" 
-        , show: true
-        }
-    ).on("hide", function(e) {
-        // Remove all handlers in the .modal namespace
-        $(document).off(".modal"); 
-    });
-    return result;
-};
+        // Run the dialog
+        dlg.modal(
+            { backdrop: "static" 
+            , show: true
+            }
+        ).on("hide", function(e) {
+            // Remove all handlers in the .modal namespace
+            $(document).off(".modal"); 
+        });
+        return result;
+    };
 
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    var editor = CodeMirror($('#editor').get(0));
+        var editor = CodeMirror($('#editor').get(0));
     
-    initProject();
-    initMenus();
-    initCommandHandlers();
-    initKeyBindings();
+        initProject();
+        initMenus();
+        initCommandHandlers();
+        initKeyBindings();
     
-    function initProject() {    
-        // Load a default project into the tree
-        if (brackets.inBrowser) {
-            // In browser: dummy folder tree (hardcoded in ProjectManager)
-            ProjectManager.loadProject("DummyProject");
-        } else {
-            // In app shell: load Brackets itself
-            var loadedPath = window.location.pathname;
-            var bracketsSrc = loadedPath.substr(0, loadedPath.lastIndexOf("/"));
-            ProjectManager.loadProject(bracketsSrc);
+        function initProject() {    
+            // Load a default project into the tree
+            if (brackets.inBrowser) {
+                // In browser: dummy folder tree (hardcoded in ProjectManager)
+                ProjectManager.loadProject("DummyProject");
+            } else {
+                // In app shell: load Brackets itself
+                var loadedPath = window.location.pathname;
+                var bracketsSrc = loadedPath.substr(0, loadedPath.lastIndexOf("/"));
+                ProjectManager.loadProject(bracketsSrc);
+            }
+    
+            // Handle toggling top level disclosure arrows of file list area
+            $("#open-files-disclosure-arrow").click(function(){
+                $(this).toggleClass( "disclosure-arrow-closed");
+                $("#open-files-container").toggle();
+            });
+            // Display close "x" icon when user hovers over working set file
+            $(".working-set-list-item").hover(
+                function() {
+                    $(this).prepend("<div class=\"close-file-icon\"></div>");
+                },
+                function() {
+                    $(this).children(".close-file-icon").remove();
+                }
+            );
+		
+            $(".close-file-icon").click( function() {
+                // close file
+            });
+		
         }
-    
-		// Handle toggling top level disclosure arrows of file list area
-		$("#open-files-disclosure-arrow").click(function(){
-			$(this).toggleClass( "disclosure-arrow-closed");
-			$("#open-files-container").toggle();
-		});
-		// Display close "x" icon when user hovers over working set file
-		$(".working-set-list-item").hover(
-			function() {
-				$(this).prepend("<div class=\"close-file-icon\"></div>");
-			},
-			function() {
-				$(this).children(".close-file-icon").remove();
-			}
-		);
-		
-		$(".close-file-icon").click( function() {
-			// close file
-		});
-		
-    }
  
-    function initMenus() {
-        // Implements the File menu items
-        $("#menu-file-new").click(function() {
-            CommandManager.execute(Commands.FILE_NEW);
-        });
-        $("#menu-file-open").click(function() {
-            CommandManager.execute(Commands.FILE_OPEN);
-        });
-        $("#menu-file-close").click(function() {
-            CommandManager.execute(Commands.FILE_CLOSE);
-        });
-        $("#menu-file-save").click(function() {
-            CommandManager.execute(Commands.FILE_SAVE);
-        });
+        function initMenus() {
+            // Implements the File menu items
+            $("#menu-file-new").click(function() {
+                CommandManager.execute(Commands.FILE_NEW);
+            });
+            $("#menu-file-open").click(function() {
+                CommandManager.execute(Commands.FILE_OPEN);
+            });
+            $("#menu-file-close").click(function() {
+                CommandManager.execute(Commands.FILE_CLOSE);
+            });
+            $("#menu-file-save").click(function() {
+                CommandManager.execute(Commands.FILE_SAVE);
+            });
     
-        // Implements the 'Run Tests' menu to bring up the Jasmine unit test window
-        var testWindow = null;
-        $("#menu-runtests").click(function(){
-            if (!(testWindow === null)) {
-                try {
-                    testWindow.location.reload();
-                } catch(e) {
-                    testWindow = null;  // the window was probably closed
-                } 
-            }
+            // Implements the 'Run Tests' menu to bring up the Jasmine unit test window
+            var testWindow = null;
+            $("#menu-runtests").click(function(){
+                if (!(testWindow === null)) {
+                    try {
+                        testWindow.location.reload();
+                    } catch(e) {
+                        testWindow = null;  // the window was probably closed
+                    } 
+                }
         
-            if (testWindow === null) {
-                testWindow = window.open("../test/SpecRunner.html");
-                testWindow.location.reload(); // if it was opened before, we need to reload because it will be cached
-            }
-        });
-    }
+                if (testWindow === null) {
+                    testWindow = window.open("../test/SpecRunner.html");
+                    testWindow.location.reload(); // if it was opened before, we need to reload because it will be cached
+                }
+            });
+        }
     
-    function initCommandHandlers() {    
-        FileCommandHandlers.init(editor, $("#main-toolbar .title"));
-    }
+        function initCommandHandlers() {    
+            FileCommandHandlers.init(editor, $("#main-toolbar .title"));
+        }
     
-    function initKeyBindings() {
-        // Register keymaps and install the keyboard handler
-        // TODO: show keyboard equivalents in the menus
-        var _globalKeymap = new KeyMap(
-            { "Ctrl-O": Commands.FILE_OPEN
-            , "Ctrl-S": Commands.FILE_SAVE
-            , "Ctrl-W": Commands.FILE_CLOSE
-            }
-        );
-        KeyBindingManager.installKeymap(_globalKeymap);
+        function initKeyBindings() {
+            // Register keymaps and install the keyboard handler
+            // TODO: show keyboard equivalents in the menus
+            var _globalKeymap = new KeyMap(
+                { "Ctrl-O": Commands.FILE_OPEN
+                , "Ctrl-S": Commands.FILE_SAVE
+                , "Ctrl-W": Commands.FILE_CLOSE
+                }
+            );
+            KeyBindingManager.installKeymap(_globalKeymap);
     
-        $(document.body).keydown(function(event) {
-            var keyDescriptor = [];
-            if (event.metaKey || event.ctrlKey) {
-                keyDescriptor.push("Ctrl");
-            }
-            if (event.altKey) {
-                keyDescriptor.push("Alt");
-            }
-            if (event.shiftKey) {
-                keyDescriptor.push("Shift");
-            }
-            keyDescriptor.push(String.fromCharCode(event.keyCode).toUpperCase());
-            if (KeyBindingManager.handleKey(keyDescriptor.join("-"))) {
-                event.preventDefault();
-            }
-        });
-    }
-});
+            $(document.body).keydown(function(event) {
+                var keyDescriptor = [];
+                if (event.metaKey || event.ctrlKey) {
+                    keyDescriptor.push("Ctrl");
+                }
+                if (event.altKey) {
+                    keyDescriptor.push("Alt");
+                }
+                if (event.shiftKey) {
+                    keyDescriptor.push("Shift");
+                }
+                keyDescriptor.push(String.fromCharCode(event.keyCode).toUpperCase());
+                if (KeyBindingManager.handleKey(keyDescriptor.join("-"))) {
+                    event.preventDefault();
+                }
+            });
+        }
+    });
 });
