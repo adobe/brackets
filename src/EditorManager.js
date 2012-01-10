69bf39072dfa4131c26dcb9bd9b4e1662b72a7cd
Ty Voliter <tvoliter@cs.stanford.edu>
2012-01-09 17:21:24 -0800

    Early implementation of programmatically generating working list from data model

--- src/EditorManager.js
+++ src/EditorManager.js
@@ -25,14 +25,45 @@ define(function(require, exports, module) {
     $(DocumentManager).on("workingSetAdd", function(event, addedDoc) {
         console.log("Working set ++ " + addedDoc);
         console.log("  set: " + DocumentManager.getWorkingSet().join());
+		
+		// Add new item to bottom of list
+		var newItem = $("<li id='" + addedDoc.file.fullPath + "' class='working-set-list-item'><a href='#'>" + addedDoc.file.name +  "</a></li>");
+		$("#open-files-container").children("ul").append(newItem);
+		
+		
+		// Show close icon on hover
+		newItem.hover(
+                function() {
+					var closeItem = $("<div class=\"close-file-icon\"></div>");
+                    $(this).prepend(closeItem);
+					
+					// Handle clicking on close icon
+		            $(".close-file-icon").click( function() {
+		                // close file
+						console.log("closed clicked");
+						
+		            });
+					
+                },
+                function() {
+                    $(this).children(".close-file-icon").remove();
+                }
+            );
+			
+            
+		
+		
     });
     $(DocumentManager).on("workingSetRemove", function(event, removedDoc) {
         console.log("Working set -- " + removedDoc);
         console.log("  set: " + DocumentManager.getWorkingSet().join());
+		
+		
+		$("#" + removedDoc.file.fullPath).remove();
     });
     
-    $(DocumentManager).on("dirtyFlagChange", function(event, removedDoc) {
-        console.log("Dirty flag change: " + removedDoc);
+    $(DocumentManager).on("dirtyFlagChange", function(event, doc ) {
+        console.log("Dirty flag change: " + doc);
     });
     
     // No public API for now
