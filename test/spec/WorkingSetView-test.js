46b13636149fe9bd8e5ed7165d07ba60b8c2d979
Ty Voliter <tvoliter@cs.stanford.edu>
2012-01-13 14:35:35 -0800

    Added working set open doc test

---
+++ test/spec/WorkingSetView-test.js
@@ -0,0 +1,109 @@
+define(function(require, exports, module) {   
+    
+    // Load dependent modules
+    var CommandManager      // loaded from brackets.test
+    ,   Commands            // loaded from brackets.test
+    ,   DocumentManager     // loaded from brackets.test
+	,	ProjectManager		// loaded from brackets.test
+	,	WorkingSetView		// loaded from brackets.test
+    ,   SpecRunnerUtils     = require("./SpecRunnerUtils.js");
+    ;
+    
+    
+    // FIXME (jasonsj): these tests are ommitted when launching in the main app window
+    if (window.opener) {
+
+        beforeEach(function() {
+    //        this.app = window.open(SpecRunnerUtils.getBracketsSourceRoot() + "/index.html");
+            // TODO: this will only work if run from the main Brackets window (not from jasmine.sh)
+            this.app = window.opener;
+            
+            // Load module instances from brackets.test
+            CommandManager      = this.app.brackets.test.CommandManager;
+            Commands            = this.app.brackets.test.Commands;
+            DocumentManager     = this.app.brackets.test.DocumentManager;
+			WorkingSetView		= this.app.brackets.test.WorkingSetView;
+            ProjectManager	    = this.app.brackets.test.ProjectManager;            
+            
+            this.app.location.reload();
+            this.testPath = SpecRunnerUtils.getTestPath("/spec/WorkingSetView-test-files/");
+            var isReady = false;
+            $(this.app.document).ready(function() {
+                isReady = true;
+            });
+            waitsFor(function() { return isReady; }, 5000);
+        });
+
+    describe("WorkingSetView", function(){
+
+		it("should add a list item when a file is dirtied", function() {			
+			var didOpen = false, gotError = false;
+
+			// Open a directory
+			ProjectManager.loadProject(this.testPath);
+			
+			// open file
+            runs(function() {
+                CommandManager.execute(Commands.FILE_OPEN, this.testPath + "/file_one.txt")
+                    .done(function() { didOpen = true; })
+                    .fail(function() { gotError = true; });
+            });
+            waitsFor(function() { return didOpen && !gotError; }, "FILE_OPEN on file timeout", 1000);
+
+			// change editor content to make doc dirty
+            runs(function() {
+                var editor = DocumentManager.getCurrentDocument()._editor;
+                editor.setValue("dirty document");
+                expect(DocumentManager.getCurrentDocument().isDirty).toBe(true);
+            });
+																			
+			// check if file is added to work set and dirty icon is present
+			runs(function() {
+				var listItems = this.app.$("#open-files-container").children("ul").children();
+				expect( listItems.length ).toBe(1);
+				expect( listItems.find("a").text() == "file_one.txt" ).toBeTruthy();
+				expect( listItems.find(".file-status-icon").length).toBe(1);
+			});
+			
+		});
+		
+		// depends on previous test
+		// it("should remove a list item when a file is closed", function() {
+		// 			
+		// 			
+		// 			
+		// 			// close the document
+		//             runs(function() {
+		//                 CommandManager.execute(Commands.FILE_CLOSE)
+		//                     .done(function() { didClose = true; })
+		//                     .fail(function() { gotError = true; });
+		//             });
+		// 			
+		// 			// check there are no list items
+		// 			runs(function() {
+		// 				var listItems = this.app.$("#open-files-container").children("ul").children();
+		// 				expect( listItems.length ).toBe(0);
+		// 			});
+		// 			
+		// 			
+		// 						
+		// 		});
+		
+		// 		
+		// 		it("should rebuild the ui from the model correctly", function() {
+		// 			
+		// 		});
+		// 		
+		// 		it("should close a file when the user clicks the close button", function() {
+		// 			
+		// 		});
+		// 		
+		// 		it("should make a file that is clicked the current one in the editor", function() {
+		// 			
+		// 		});
+		
+		
+		
+		});
+	}	
+});
