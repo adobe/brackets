c62cb93f760242a13e6d557580860ce7cc5db7a9
Glenn Ruehle <gruehle@gmail.com>
2012-01-27 13:11:52 -0800

    Add jslint settings comments.

--- src/DocumentManager.js
+++ src/DocumentManager.js
@@ -2,6 +2,9 @@
  * Copyright 2011 Adobe Systems Incorporated. All Rights Reserved.
  */
 
+/*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define: false */
+
 /**
  * DocumentManager is the model for the set of currently 'open' files and their contents. It controls
  * which file is currently shown in the editor, the dirty bit for all files, and the list of documents
--- src/FileCommandHandlers.js
+++ src/FileCommandHandlers.js
@@ -1,6 +1,10 @@
 /*
  * Copyright 2011 Adobe Systems Incorporated. All Rights Reserved.
  */
+
+/*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define: false */
+
 define(function(require, exports, module) {
     require("thirdparty/path-utils/path-utils.min");
     
