4dd52c5ce41f640575d570f002065af25b6717e9
Chris Bank <cbank@adobe.com>
2012-02-15 11:56:15 -0800

    Fix TODO is keymap by normalizing the keys so we don't need to worry about what order they are entered in

--- src/KeyMap.js
+++ src/KeyMap.js
@@ -7,15 +7,109 @@
 
 define(function (require, exports, module) {
     'use strict';
+
+    /** 
+     * builds the keyDescriptor string from the given parts
+     */
+    function _buildKeyDescriptor(hasCtrl, hasAlt, hasShift, key) {
+        if (!key) {
+            console.log("KeyMap _buildKeyDescriptor() - No key provided!");
+            return "";
+        }
+        key = key.trim().toUpperCase();
+        if (key.length === 0) {
+            console.log("KeyMap _buildKeyDescriptor() - Empty key passed in!");
+            return "";
+        }
+        
+        var keyDescriptor = [];
+        if (hasCtrl) {
+            keyDescriptor.push("Ctrl");
+        }
+        if (hasAlt) {
+            keyDescriptor.push("Alt");
+        }
+        if (hasShift) {
+            keyDescriptor.push("Shift");
+        }
+        keyDescriptor.push(key);
+        
+        return keyDescriptor.join("-");
+    }
+    
+    /** 
+     * normalizes the incoming key descriptor so the modifier keys are always specified in the correct order
+     * @param {string} The string for a key descriptor, can be in any order, the result will be Ctrl-Alt-Shift-<Key>
+     */
+    function _normalizeKeyDescriptorString(origDescriptor) {
+        var hasCtrl = false,
+            hasAlt = false,
+            hasShift = false,
+            key = "";
+        
+        function _isModifier(left, right, previouslyFound) {
+            if (!left || !right) {
+                return false;
+            }
+            left = left.trim().toLowerCase();
+            right = right.trim().toLowerCase();
+            var matched = (left.length > 0 && left === right);
+            if (matched && previouslyFound) {
+                console.log("KeyMap _normalizeKeyDescriptorString() - Modifier defined twice: " + origDescriptor);
+            }
+            return matched;
+        }
+        
+        origDescriptor.split("-").forEach(function parseDescriptor(ele, i, arr) {
+            if (_isModifier("ctrl", ele, hasCtrl)) {
+                hasCtrl = true;
+            } else if (_isModifier("alt", ele, hasAlt)) {
+                hasAlt = true;
+            } else if (_isModifier("shift", ele, hasShift)) {
+                hasShift = true;
+            } else if (key.length > 0) {
+                console.log("KeyMap _normalizeKeyDescriptorString() - Multiple keys defined. Using key: " + key + " from: " + origDescriptor);
+            } else {
+                key = ele.trim();
+            }
+        });
+        
+        return _buildKeyDescriptor(hasCtrl, hasAlt, hasShift, key);
+    }
+    
+    /** 
+     * normalizes the incoming map so all the key descriptors are specified the correct way
+     * @param {map} The string for a key descriptor, can be in any order, the result will be Ctrl-Alt-Shift-<Key>
+     */
+    function _normalizeMap(map) {
+        var finalMap = {};
+        Object.keys(map).forEach(function normalizeKey(ele, i, arr) {
+            var val = map[ele];
+            var normalizedKey = _normalizeKeyDescriptorString(ele);
+            if (normalizedKey.length === 0) {
+                console.log("KeyMap _normalizeMap() - Rejecting malformed key: " + ele + " (value: " + val + ")");
+            } else if (!val) {
+                console.log("KeyMap _normalizeMap() - Rejecting key for falsy value: " + ele + " (value: " + val + ")");
+            } else if (finalMap[normalizedKey]) {
+                console.log("KeyMap _normalizeMap() - Rejecting key because it was defined twice: " + ele + " (value: " + val + ")");
+            } else {
+                if (normalizedKey !== ele) {
+                    console.log("KeyMap _normalizeMap() - Corrected a malformed key: " + ele + " (value: " + val + ")");
+                }
+                finalMap[normalizedKey] = val;
+            }
+        });
+        return finalMap;
+    }
+    
     /** class Keymap
      *
      * A keymap specifies how keys are mapped to commands. This currently just holds the map, but in future
      * it will likely be extended to include other metadata about the keymap.
      *
      * Keys are described by strings of the form "[modifier-modifier-...-]key", where modifier is one of
-     * Ctrl, Alt, or Shift. If multiple modifiers are specified, they must be specified in that order
-     * (i.e. "Ctrl-Alt-Shift-P" is legal, "Alt-Ctrl-Shift-P" is not). 
-     * (TODO: the above restriction is to simplify mapping--is it too onerous?)
+     * Ctrl, Alt, or Shift. If multiple modifiers are specified, they will get normalized to the form
+     * "Ctrl-Alt-Shift-<Key>" so modifiers are always stored and lookedup in that order.
      *    -- Ctrl maps to Cmd on Mac. (This means that you can't specifically bind to the Ctrl key on Mac.)
      *    -- Alt maps to the Option key on Mac.
      *    -- Letters must be uppercase, but do not require Shift by default. To indicate that Shift must be held
@@ -28,11 +122,12 @@ define(function (require, exports, module) {
         if (map === undefined) {
             throw new Error("All parameters to the KeyMap constructor must be specified");
         }
-        this.map = map;
+        this.map = _normalizeMap(map);
     };
     
     /**
      * simple creator
+     * @param {map} map An object mapping key-description strings to command IDs.
      */
     function create(map) {
         return new KeyMap(map);
@@ -41,19 +136,13 @@ define(function (require, exports, module) {
     /**
      * Takes a keyboard event and translates it into a key in a key map
      */
-    function translateKeyboardEvent(evt) {
-        var keyDescriptor = [];
-        if (event.metaKey || event.ctrlKey) {
-            keyDescriptor.push("Ctrl");
-        }
-        if (event.altKey) {
-            keyDescriptor.push("Alt");
-        }
-        if (event.shiftKey) {
-            keyDescriptor.push("Shift");
-        }
-        keyDescriptor.push(String.fromCharCode(event.keyCode).toUpperCase());
-        return keyDescriptor.join("-");
+    function translateKeyboardEvent(event) {
+        var hasCtrl = (event.metaKey || event.ctrlKey),
+            hasAlt = (event.altKey),
+            hasShift = (event.shiftKey),
+            key = String.fromCharCode(event.keyCode);
+        
+        return _buildKeyDescriptor(hasCtrl, hasAlt, hasShift, key);
     }
     
     // Define public API
