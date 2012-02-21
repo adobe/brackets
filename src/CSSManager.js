db89c72200647dce04e0162269b2bd01f13bd86a
Jason San Jose <jasonsj@adobe.com>
2012-02-21 15:02:25 -0800

    CSSManager attempt 2
    * Use LESS parser instead of JSCSSP + Slick
    * Begin work to simplify selector matching based on minispec
    * Remove unused 3rd party code

--- src/CSSManager.js
+++ src/CSSManager.js
@@ -3,7 +3,7 @@
  */
 
 /*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define: false, $: false, require: false, CSSParser: false, Slick: false */
+/*global define: false, $: false, require: false, less: false */
 
 /**
  * CSSManager
@@ -11,86 +11,22 @@
 define(function (require, exports, module) {
     'use strict';
     
-    require("thirdparty/JSCSSP/cssParser");
-    require("thirdparty/slick/Source/Slick.Parser");
-    
     var DocumentManager = require("DocumentManager");
     
-    // Adapted from Slick.Finder.js to work without a DOM tree
-    function matchSelector(tagInfo, tag, id, classes, attributes, pseudos) {
-        if (tag) {
-            tag = tag.toLowerCase();
-            
-            var nodeName = tagInfo.tag.toLowerCase();
-            if (tag === '*') {
-                if (nodeName < '@') {
-                    return false; // Fix for comment nodes and closed nodes
-                }
-            } else {
-                if (nodeName !== tag) {
-                    return false;
-                }
-            }
-        }
-    
-        // Case-insensitive ID match
-        // https://developer.mozilla.org/en/Case_Sensitivity_in_class_and_id_Names
-        if (id) {
-            if (!tagInfo.id) {
-                return false;
-            } else if (tagInfo.id.toLowerCase() !== id.toLowerCase()) {
-                return false;
-            }
-        }
-    
-        var part, cls;
-        if (classes) {
-            // if the selector has classes, return true if the tagInfo
-            // uses a matching class
-            var matchingClass = classes.some(function (cls, i) {
-                // Class names are case insensitive. Fix the RegExp flags.
-                // https://developer.mozilla.org/en/Case_Sensitivity_in_class_and_id_Names
-                if (!cls.regexp.ignoreCase) {
-                    cls.regexp = new RegExp(cls.regexp.source, "i");
-                }
-                
-                return ((tagInfo.clazz && cls.regexp.test(tagInfo.clazz.toLowerCase())));
-            });
-            
-            if (!matchingClass) {
-                return false;
-            }
-        }
-        /*
-        if (attributes) for (i = attributes.length; i--;){
-            part = attributes[i];
-            if (part.operator ? !part.test(this.getAttribute(node, part.key)) : !this.hasAttribute(node, part.key)) return false;
-        }
-        if (pseudos) for (i = pseudos.length; i--;){
-            part = pseudos[i];
-            if (!this.matchPseudo(node, part.key, part.value)) return false;
-        }
-        */
-        return true;
-    }
-    
-    // Adapted from Slick.Finder.js
-    function matchNode(selector, tagInfo) {
-        // simple (single) selectors
-        return selector.expressions.some(function (currentExpression) {
-            if (currentExpression.length === 1) {
-                var exp = currentExpression[0];
-                if (matchSelector(tagInfo, exp.tag, exp.id, exp.classes, exp.attributes, exp.pseudos)) {
-                    return true;
-                }
-            }
-            
-            return false;
-        });
+    /**
+     * Adapter for LESS RuleSet
+     */
+    function RuleSetInfo(ruleset) {
+        this.ruleset = ruleset;
+        
+        // FIXME (jasonsj): Patch LESS parser to keep whitespace and line endings
+        // offsetStart = ...
+        // offsetEnd = ...
     }
     
     function CSSManager() {
         this._rules = [];
+        this._parser = new less.Parser();
     }
     
     /**
@@ -106,33 +42,34 @@ define(function (require, exports, module) {
     CSSManager.prototype.loadFile = function (fileEntry) {
         var result = new $.Deferred(),
             textResult = DocumentManager.readAsText(fileEntry),
-            self = this;
+            self = this,
+            rulesets = [];
+        
+        // TODO (jasonsj): handle .less vs. css
+        // Use the LESS parser for both .less and .css files.
+        // Current support is CSS only, so we only consider leaf rulesets
+        function addRuleset(ruleset) {
+            var children = ruleset.rulesets();
+            
+            // depth-first search for leaf rulesets
+            if (children.length > 0) {
+                children.forEach(function (value, index) {
+                    addRuleset(value);
+                });
+            } else {
+                rulesets.push(new RuleSetInfo(ruleset));
+            }
+        }
         
         textResult.done(function (text) {
-            var parser = new CSSParser(),
-                sheet = parser.parse(text, false, true),
-                styleRules = [],
-                selector = null;
+            self._parser.parse(text, function (error, root) {
+                addRuleset(root);
             
-            // post process style rules only
-            sheet.cssRules.forEach(function (value, index) {
-                if (value.selectorText) {
-                    // add a selector property using Slick's selector parsing
-                    selector = Slick.parse(value.selectorText());
-                    value.selector = selector;
-                    
-                    // note the file for this rule
-                    value.fileEntry = fileEntry;
-                    
-                    styleRules.push(value);
-                }
+                // resolve with rules from this file
+                result.resolve(rulesets);
+                
+                self._rules = self._rules.concat(rulesets);
             });
-            
-            // collect rules from each file
-            self._rules = self._rules.concat(styleRules);
-        
-            // resolve with rules from this file
-            result.resolve(styleRules);
         });
         
         return result;
@@ -152,12 +89,30 @@ define(function (require, exports, module) {
      * Finds matching CSS rules based on the tag, id and/or classes
      */
     // after sprint 4 we should make this more robust
-    CSSManager.prototype.findMatchingRules = function (tagInfo) {
-        function matches(element, index, array) {
-            return matchNode(element.selector, tagInfo);
-        }
-        
-        return this._rules.filter(matches);
+    CSSManager.prototype.findMatchingRules = function (selectorString) {
+        return this._rules.filter(function (rulesetInfo, index, array) {
+            var selectors = rulesetInfo.ruleset.selectors;
+            
+            // find a matching selector for the input selectorString
+            return selectors.some(function (selector) {
+                // TODO (jasonsj): Combinators (descendant ' ', child '>', sibling '+')
+                //                 Specificity
+                
+                if (selector.elements.length > 0) {
+                    // Use last selector element
+                    var element = selector.elements[selector.elements.length - 1];
+                    
+                    // Always match the universal selector (sprint 4)
+                    if (element.value.charAt(0) === '*') {
+                        return true;
+                    }
+                    
+                    return element.value.toLowerCase() === selectorString.toLowerCase();
+                }
+                
+                return false;
+            });
+        });
     };
     
     exports.CSSManager = CSSManager;
--- test/spec/CSSManager-test.js
+++ test/spec/CSSManager-test.js
@@ -12,21 +12,59 @@ define(function (require, exports, module) {
         CSSManager       = require("CSSManager"),
         SpecRunnerUtils  = require("./SpecRunnerUtils.js");
     
+    var testPath                = SpecRunnerUtils.getTestPath("/spec/CSSManager-test-files"),
+        simpleCssFileEntry      = new NativeFileSystem.FileEntry(testPath + "/simple.css"),
+        universalCssFileEntry   = new NativeFileSystem.FileEntry(testPath + "/universal.css");
+    
+    // jasmine matcher for sprint 4 selector matching
+    var toMatchLastSelectorElement = function (expected) {
+        var info    = this.actual,
+            ruleset = this.actual.ruleset;
+        
+        if (ruleset) {
+            var selectorCount   = ruleset.selectors.length;
+            
+            if (selectorCount) {
+                var selector = ruleset.selectors[selectorCount - 1],
+                    elementCount = selector.elements.length;
+                
+                if (elementCount) {
+                    return selector.elements[elementCount - 1].value === expected;
+                }
+            }
+        }
+        
+        return false;
+    };
+    
+    function init(spec, fileEntry) {
+        spec.cssManager = new CSSManager.CSSManager();
+        
+        if (fileEntry) {
+            spec.addMatchers({toMatchLastSelectorElement: toMatchLastSelectorElement});
+            
+            var styleRules;
+            
+            runs(function () {
+                spec.cssManager.loadFile(fileEntry).done(function (result) {
+                    styleRules = result;
+                });
+            });
+            
+            waitsFor(function () { return styleRules; }, 1000);
+        }
+    }
+    
     describe("CSSManager", function () {
         
-        var testPath                = SpecRunnerUtils.getTestPath("/spec/CSSManager-test-files"),
-            simpleCssFileEntry      = new NativeFileSystem.FileEntry(testPath + "/simple.css"),
-            universalCssFileEntry   = new NativeFileSystem.FileEntry(testPath + "/universal.css"),
-            cssManager;
-        
         beforeEach(function () {
-            cssManager = new CSSManager.CSSManager();
+            init(this);
         });
         
         describe("initial state", function () {
             
             it("should initialize with no rules", function () {
-                expect(cssManager.getStyleRules().length).toEqual(0);
+                expect(this.cssManager.getStyleRules().length).toEqual(0);
             });
             
         });
@@ -38,7 +76,7 @@ define(function (require, exports, module) {
                     loadFile   = false;
                 
                 runs(function () {
-                    cssManager.loadFile(simpleCssFileEntry).done(function (result) {
+                    this.cssManager.loadFile(simpleCssFileEntry).done(function (result) {
                         styleRules = result;
                     });
                 });
@@ -47,171 +85,88 @@ define(function (require, exports, module) {
                 
                 runs(function () {
                     expect(styleRules.length).toEqual(6);
-                    expect(cssManager.getStyleRules()).toEqual(styleRules);
+                    expect(this.cssManager.getStyleRules()).toEqual(styleRules);
                 });
             });
         });
         
         describe("findMatchingRules() with simple selectors", function () {
-            
+        
             beforeEach(function () {
-                var styleRules;
-                
-                runs(function () {
-                    cssManager.loadFile(simpleCssFileEntry).done(function (result) {
-                        styleRules = result;
-                    });
-                });
-                
-                waitsFor(function () { return styleRules; }, 1000);
+                init(this, simpleCssFileEntry);
             });
             
-            it("should match tag name selectors", function () {
-                var tagInfo = {
-                    tag: "html"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+            it("should match type selectors", function () {
+                var matches = this.cssManager.findMatchingRules("html");
                 
                 expect(matches.length).toEqual(2);
-                expect(matches[0].mSelectorText).toBe("html");
-                expect(matches[1].mSelectorText).toBe("HTML");
+                expect(matches[0]).toMatchLastSelectorElement("html");
+                expect(matches[1]).toMatchLastSelectorElement("HTML");
             });
             
             it("should match class name selectors", function () {
-                var tagInfo = {
-                    tag     : "p",
-                    clazz   : "firstGrade"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+                var matches = this.cssManager.findMatchingRules(".firstGrade");
                 
                 expect(matches.length).toEqual(2);
                 
                 // Parser will save selectors as lowercase
-                expect(matches[0].mSelectorText).toBe(".firstGrade");
-                expect(matches[1].mSelectorText).toBe(".FIRSTGRADE");
+                expect(matches[0]).toMatchLastSelectorElement(".firstGrade");
+                expect(matches[1]).toMatchLastSelectorElement(".FIRSTGRADE");
             });
             
-            it("should match case-insensetive IDs", function () {
-                var tagInfo = {
-                    tag     : "p",
-                    id      : "JasonSanJose"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+            it("should match IDs", function () {
+                var matches = this.cssManager.findMatchingRules("#brack3ts");
                 
                 expect(matches.length).toEqual(2);
-                expect(matches[0].mSelectorText).toBe("#JasonSanJose");
-                expect(matches[1].mSelectorText).toBe("#jasonsanjose");
+                expect(matches[0]).toMatchLastSelectorElement("#brack3ts");
+                expect(matches[1]).toMatchLastSelectorElement("#BRACK3TS");
             });
             
             it("should not find a universal selector", function () {
-                var tagInfo = {
-                    tag: "jason"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+                var matches = this.cssManager.findMatchingRules("pre");
                 
                 // html and universal selector
                 expect(matches.length).toEqual(0);
             });
         });
         
+        describe("findMatchingRules() with the universal selector", function () {
         
-        describe("findMatchingRules() with compound selectors", function () {
-            
             beforeEach(function () {
-                var styleRules;
-                
-                runs(function () {
-                    cssManager.loadFile(universalCssFileEntry).done(function (result) {
-                        styleRules = result;
-                    });
-                });
-                
-                waitsFor(function () { return styleRules; }, 1000);
+                init(this, universalCssFileEntry);
             });
             
-            it("should match the universal selector", function () {
-                var tagInfo = {
-                    tag: "blockquote"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+            it("should match the universal selector alone", function () {
+                var matches = this.cssManager.findMatchingRules("blockquote");
                 
                 expect(matches.length).toEqual(1);
-                expect(matches[0].mSelectorText).toBe("*");
+                expect(matches[0]).toMatchLastSelectorElement("*");
             });
             
-            it("should match tag name selectors", function () {
-                var tagInfo = {
-                    tag: "p"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+            it("should match a tag name selector", function () {
+                var matches = this.cssManager.findMatchingRules("p");
                 
                 expect(matches.length).toEqual(2);
-                expect(matches[0].mSelectorText).toBe("*");
-                expect(matches[1].mSelectorText).toBe("p");
+                expect(matches[0]).toMatchLastSelectorElement("*");
+                expect(matches[1]).toMatchLastSelectorElement("p");
             });
             
-            it("should match class name selectors", function () {
-                var tagInfo = {
-                    tag: "pre",
-                    clazz: "firstGrade"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
+            it("should match a class name selector", function () {
+                var matches = this.cssManager.findMatchingRules(".firstGrade");
                 
                 expect(matches.length).toEqual(2);
-                expect(matches[0].mSelectorText).toBe("*");
-                expect(matches[1].mSelectorText).toBe(".firstGrade");
+                expect(matches[0]).toMatchLastSelectorElement("*");
+                expect(matches[1]).toMatchLastSelectorElement(".firstGrade");
             });
             
-            it("should match a compound tag and class name selector", function () {
-                var tagInfo = {
-                    tag: "p",
-                    clazz: "firstGrade"
-                };
+            it("should match an id selector", function () {
+                var matches = this.cssManager.findMatchingRules("#brack3ts");
                 
-                var matches = cssManager.findMatchingRules(tagInfo);
-                
-                expect(matches.length).toEqual(3);
-                expect(matches[0].mSelectorText).toBe("*");
-                expect(matches[1].mSelectorText).toBe(".firstGrade");
-                expect(matches[2].mSelectorText).toBe("p");
-            });
-            
-            it("should match a compound tag name and id selector", function () {
-                var tagInfo = {
-                    tag: "p",
-                    id: "JasonSanJose"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
-                
-                expect(matches.length).toEqual(3);
-                expect(matches[0].mSelectorText).toBe("*");
-                expect(matches[1].mSelectorText).toBe("p");
-                expect(matches[2].mSelectorText).toBe("#JasonSanJose");
-            });
-            
-            it("should match a compound tag name, class name and id selector", function () {
-                var tagInfo = {
-                    tag: "p",
-                    clazz: "firstGrade",
-                    id: "JasonSanJose"
-                };
-                
-                var matches = cssManager.findMatchingRules(tagInfo);
-                
-                expect(matches.length).toEqual(4);
-                expect(matches[0].mSelectorText).toBe("*");
-                expect(matches[1].mSelectorText).toBe(".firstGrade");
-                expect(matches[2].mSelectorText).toBe("p");
-                expect(matches[3].mSelectorText).toBe("#JasonSanJose");
+                expect(matches.length).toEqual(2);
+                expect(matches[0]).toMatchLastSelectorElement("*");
+                expect(matches[1]).toMatchLastSelectorElement("#brack3ts");
             });
         });
+        
     });
 });
