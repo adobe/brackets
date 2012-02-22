35700d325084a1af97092dcec2cc779e348b8a9c
Jason San Jose <jasonsj@adobe.com>
2012-02-22 09:48:38 -0800

    Support right-most type selectors with class or ID
    Add _loadString utility for unit tests, refactor.
    Add comments for CSSManager and methods.
    Add unit tests for type selector support.

--- src/CSSManager.js
+++ src/CSSManager.js
@@ -11,7 +11,14 @@
 define(function (require, exports, module) {
     'use strict';
     
+    // Dependencies
     var DocumentManager = require("DocumentManager");
+    
+    /**
+     * Regex to match selector element values for ID '#', pseudo ':',
+     * class name '.', attribute start '[' or digit.
+     */
+    var IDENTIFIER_REGEX = /^[#:\.\[\d]/;
     
     /**
      * Adapter for LESS RuleSet
@@ -22,22 +29,65 @@ define(function (require, exports, module) {
         // FIXME (jasonsj): Patch LESS parser to keep whitespace and line endings
         // offsetStart = ...
         // offsetEnd = ...
+        this.lineStart = 0;
+        this.lineEnd = 0;
     }
     
+    // TODO (jasonsj): handle .less vs. css
+    // Use the LESS parser for both .less and .css files.
+    // Current support is CSS only, so we only consider leaf rulesets
+    function _addRuleset(results, ruleset) {
+        var children = ruleset.rulesets();
+        
+        // depth-first search for leaf rulesets
+        if (children.length > 0) {
+            children.forEach(function (value, index) {
+                _addRuleset(results, value);
+            });
+        } else {
+            results.push(new RuleSetInfo(ruleset));
+        }
+    }
+    
+    /**
+     * CSSManager loads CSS content from files (or strings) and parses
+     * the content into an abstract syntax tree using the LESS Parser.
+     * This manager maintains parsed CSS rules in-memory to provide 
+     * fast lookups of rules based on selector criteria.
+     */
     function CSSManager() {
         this._rules = [];
         this._parser = new less.Parser();
     }
     
     /**
-     * 
+     * Returns all style rules loaded for this CSSManager instance.
+     *
+     * @return {Array.<ResultSetInfo>}
      */
     CSSManager.prototype.getStyleRules = function () {
         return [].concat(this._rules);
     };
     
     /**
-     * Read CSS rules from a file
+     * Parse CSS rules from a string
+     * @param {!string} str
+     */
+    CSSManager.prototype._loadString = function (str) {
+        var rulesets = [],
+            self = this;
+        
+        this._parser.parse(str, function (error, root) {
+            _addRuleset(rulesets, root);
+            self._rules = self._rules.concat(rulesets);
+        });
+        
+        return rulesets;
+    };
+    
+    /**
+     * Parse CSS rules from a file and cache the results.
+     * @param {!FileEntry} fileEntry
      */
     CSSManager.prototype.loadFile = function (fileEntry) {
         var result = new $.Deferred(),
@@ -45,25 +95,9 @@ define(function (require, exports, module) {
             self = this,
             rulesets = [];
         
-        // TODO (jasonsj): handle .less vs. css
-        // Use the LESS parser for both .less and .css files.
-        // Current support is CSS only, so we only consider leaf rulesets
-        function addRuleset(ruleset) {
-            var children = ruleset.rulesets();
-            
-            // depth-first search for leaf rulesets
-            if (children.length > 0) {
-                children.forEach(function (value, index) {
-                    addRuleset(value);
-                });
-            } else {
-                rulesets.push(new RuleSetInfo(ruleset));
-            }
-        }
-        
         textResult.done(function (text) {
             self._parser.parse(text, function (error, root) {
-                addRuleset(root);
+                _addRuleset(rulesets, root);
             
                 // resolve with rules from this file
                 result.resolve(rulesets);
@@ -75,18 +109,23 @@ define(function (require, exports, module) {
         return result;
     };
     
+    /**
+     * Remove a file from cache
+     */
     CSSManager.prototype.removeFile = function (fileEntry) {
+        // TODO (jasonsj): integrate with FileIndexManager
     };
     
     /**
-     * Reset all rules
+     * Clear all rules from cache.
      */
-    CSSManager.prototype.clear = function () {
+    CSSManager.prototype.clearCache = function () {
         this._rules = [];
     };
     
     /**
      * Finds matching CSS rules based on the tag, id and/or classes
+     * @param {!string} selectorString
      */
     // after sprint 4 we should make this more robust
     CSSManager.prototype.findMatchingRules = function (selectorString) {
@@ -99,8 +138,28 @@ define(function (require, exports, module) {
                 //                 Specificity
                 
                 if (selector.elements.length > 0) {
-                    // Use last selector element
-                    var element = selector.elements[selector.elements.length - 1];
+                    var isTypeSelector = function (str) {
+                        return (str.search(IDENTIFIER_REGEX) !== 0);
+                    };
+                    
+                    // find the right-most type selector if the input string is a type
+                    var element             = null,
+                        elementIndex        = selector.elements.length - 1;
+                    
+                    if (isTypeSelector(selectorString)) {
+                        while (elementIndex >= 0) {
+                            element = selector.elements[elementIndex];
+                            
+                            if (isTypeSelector(element.value)) {
+                                break;
+                            }
+                            
+                            elementIndex--;
+                        }
+                    } else {
+                        // Use last selector element
+                        element = selector.elements[elementIndex];
+                    }
                     
                     // Always match the universal selector (sprint 4)
                     if (element.value.charAt(0) === '*') {
