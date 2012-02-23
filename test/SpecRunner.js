56111537257fc5318a48014a9491be384d86c68f
Peter Flynn <pflynn@adobe.com>
2012-02-22 17:58:49 -0800

    Merge my CSS tests into Jason's existing CSSManager-test suite; remove my now-defunct test spec

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -16,17 +16,16 @@ define(function (require, exports, module) {
     localStorage.setItem("preferencesKey", SpecRunnerUtils.TEST_PREFERENCES_KEY);
 
     // Load test specs
-    // require("spec/LowLevelFileIO-test.js");
-    // require("spec/FileCommandHandlers-test.js");
-    // require("spec/NativeFileSystem-test.js");
-    // require("spec/PreferencesManager-test.js");
-    // require("spec/Editor-test.js");
-    // require("spec/ProjectManager-test.js");
-    // require("spec/WorkingSetView-test.js");
-    // require("spec/KeyMap-test.js");
-    // require("spec/CodeHintUtils-test.js");
+    require("spec/LowLevelFileIO-test.js");
+    require("spec/FileCommandHandlers-test.js");
+    require("spec/NativeFileSystem-test.js");
+    require("spec/PreferencesManager-test.js");
+    require("spec/Editor-test.js");
+    require("spec/ProjectManager-test.js");
+    require("spec/WorkingSetView-test.js");
+    require("spec/KeyMap-test.js");
+    require("spec/CodeHintUtils-test.js");
     require("spec/CSSManager-test.js");
-    require("spec/CSS-test.js");
 
     // Clean up preferencesKey
     $(window).unload(function () {
