31320928ee658cef0a04902b565cb1fe63af75e2
Jason San Jose <jasonsj@adobe.com>
2012-02-23 18:03:38 -0800

    Added unit tests for project change and markDirty

--- test/spec/FileIndexManager-test.js
+++ test/spec/FileIndexManager-test.js
@@ -86,5 +86,126 @@ define(function (require, exports, module) {
                 expect(fileList[0].fullPath).toEqual(testPath + "/file_four.css");
             });
         });
+        
+        it("should update the indicies on project change", function () {
+            // Load spec/FileIndexManager-test-files
+            SpecRunnerUtils.loadProjectInTestWindow(testPath);
+
+            var allFiles;
+            runs(function () {
+                FileIndexManager.getFileInfoList("all")
+                    .done(function (result) {
+                        allFiles = result;
+                    });
+            });
+
+            waitsFor(function () { return allFiles; }, "FileIndexManager.getFileInfoList() timeout", 1000);
+            
+            runs(function () {
+                expect(allFiles.length).toEqual(8);
+            });
+            
+            // load a subfolder in the test project
+            // spec/FileIndexManager-test-files/dir1/dir2
+            SpecRunnerUtils.loadProjectInTestWindow(testPath + "/dir1/dir2/");
+
+            runs(function () {
+                FileIndexManager.getFileInfoList("all")
+                    .done(function (result) {
+                        allFiles = result;
+                    });
+            });
+
+            waitsFor(function () { return allFiles; }, "FileIndexManager.getFileInfoList() timeout", 1000);
+            
+            runs(function () {
+                expect(allFiles.length).toEqual(2);
+                expect(allFiles[0].name).toEqual("file_eight.css");
+                expect(allFiles[1].name).toEqual("file_seven.js");
+            });
+        });
+        
+        it("should update the indicies after being marked dirty", function () {
+            // Load spec/FileIndexManager-test-files
+            SpecRunnerUtils.loadProjectInTestWindow(testPath);
+
+            var allFiles, txtFiles;
+            
+            // helper function to validate base state of 8 files
+            function checkAllFileCount(fileCount) {
+                runs(function () {
+                    FileIndexManager.getFileInfoList("all")
+                        .done(function (result) {
+                            allFiles = result;
+                        });
+                });
+    
+                waitsFor(function () { return allFiles; }, "FileIndexManager.getFileInfoList() timeout", 1000);
+                
+                runs(function () {
+                    expect(allFiles.length).toEqual(fileCount);
+                });
+            }
+            
+            // verify 8 files in base state
+            checkAllFileCount(8);
+            
+            // add a temporary file to the folder
+            var entry;
+            
+            // create a 9th file
+            runs(function () {
+                var root = ProjectManager.getProjectRoot();
+                root.getFile("new-file.txt",
+                             { create: true, exclusive: true },
+                             function (fileEntry) { entry = fileEntry; });
+            });
+            
+            waitsFor(function () { return entry; }, "getFile() timeout", 1000);
+            
+            runs(function () {
+                // mark FileIndexManager dirty after new file was created
+                FileIndexManager.markDirty();
+            });
+
+            waitsFor(function () { return allFiles; }, "FileIndexManager.getFileInfoList() timeout", 1000);
+            
+            // verify 9 files
+            checkAllFileCount(9);
+            
+            var cleanupComplete = false;
+            
+            // verify the new file was added to the "all" index
+            runs(function () {
+                var filtered = allFiles.filter(function (value) {
+                    return (value.name === "new-file.txt");
+                });
+                expect(filtered.length).toEqual(1);
+                
+                // remove the 9th file
+                brackets.fs.unlink(entry.fullPath, function (err) {
+                    cleanupComplete = (err === brackets.fs.NO_ERROR);
+                });
+            });
+
+            // wait for the file to be deleted
+            waitsFor(function () { return cleanupComplete; }, 1000);
+            
+            runs(function () {
+                // mark FileIndexManager dirty after new file was deleted
+                FileIndexManager.markDirty();
+            });
+            
+            // verify that we're back to 8 files
+            checkAllFileCount(8);
+            
+            // make sure the 9th file was removed from the index
+            runs(function () {
+                var filtered = allFiles.filter(function (value) {
+                    return (value.name === "new-file.txt");
+                });
+                expect(filtered.length).toEqual(0);
+            });
+        });
     });
 });
