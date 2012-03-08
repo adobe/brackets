ac55f5334ac76629bbbbe9fd0de25e8f7059f415
Ty Voliter <tvoliter@cs.stanford.edu>
2012-03-08 15:29:08 -0800

    simplified hide/show file list header logic by only assigning click handler to div containing both header text and arrow icon

--- src/EditorManager.js
+++ src/EditorManager.js
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2011 Adobe Systems Incorporated. All Rights Reserved.
  */
@@ -452,6 +453,16 @@ define(function (require, exports, module) {
         return { content: inlineContent, editor: inlineEditor, height: 0, onAdded: afterAdded };
     }
     
+    // TODO TY: move fucntion and add comments
+       function _AutoSizeInlineWidget(hostEditor, inlineId) {
+
+        
+        var widgetHeight = inlineEditor.totalHeight(true);
+
+        hostEditor.setInlineWidgetHeight(inlineId, widgetHeight, true);
+        $(inlineEditor.getScrollerElement()).height(widgetHeight);
+        inlineEditor.refresh();
+    }
     
     /**
      * Disposes the given document's editor if the doc is no longer "open" in the UI (visible or in
