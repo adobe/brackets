58fc7b17486ddc90bd66e42fa42cfa7b9ebf1439
Ty Voliter <tvoliter@cs.stanford.edu>
2012-04-17 11:44:23 -0700

    Respect Win and Mac shortcut key ordering

--- src/command/KeyMap.js
+++ src/command/KeyMap.js
@@ -24,15 +24,23 @@ define(function (require, exports, module) {
         }
         
         var keyDescriptor = [];
-        if (hasCtrl) {
-            keyDescriptor.push("Ctrl");
-        }
+       
         if (hasAlt) {
             keyDescriptor.push("Alt");
         }
         if (hasShift) {
             keyDescriptor.push("Shift");
         }
+
+        if (hasCtrl) {
+            // Windows display Ctrl first, Mac displays Command symbol last
+            if (brackets.platform === "win") {
+                keyDescriptor.unshift("Ctrl");
+            } else {
+                keyDescriptor.push("Ctrl");
+            }
+        }
+
         keyDescriptor.push(key);
         
         return keyDescriptor.join("-");
