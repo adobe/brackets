04865feef485065875dde66de4ceade26954d623
Ty Voliter <tvoliter@cs.stanford.edu>
2012-04-24 12:53:08 -0700

    Fix focus bug with _doOpenWithOptionalPath

--- src/document/DocumentCommandHandlers.js
+++ src/document/DocumentCommandHandlers.js
@@ -185,11 +185,7 @@ define(function (require, exports, module) {
         }
         
         return _doOpenWithOptionalPath(fullPath)
-                 .always( function() {
-                    if(commandData.focusEditor) {
-                        EditorManager.focusEditor();
-                    }
-                });
+            .always(EditorManager.focusEditor);
     }
 
     function handleFileAddToWorkingSet(commandData) {
