af50d720685648bfd4e3ce259bcf4287c26b51be
Matt Fisher <fisher.matt@gmail.com>
2012-05-02 18:24:51 -0400

    Back to array length declairation moved outside of for loop. Passing JSONLint now thanks to that fancy star :).

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -824,7 +824,9 @@ define(function (require, exports, module) {
      * @param {number} inlineId  id returned by addInlineWidget().
      */
     Editor.prototype._removeInlineWidgetInternal = function (inlineId) {
-        for(var i = 0; i < this._inlineWidgets.length; i++) {
+        var i;
+        var l = this._inlineWidgets.length;
+        for (i = 0; i < l; i++) {
             if (this._inlineWidgets[i].id === inlineId) {
                 this._inlineWidgets.splice(i, 1);
                 break;
