f49c4d4b2cceb8de48a259af189f827a8ef31c55
Josh Hatwich <jhatwich@adobe.com>
2012-05-06 13:28:55 -0700

    insulate dependency on new API expected to show up in CodeMirror2

--- src/document/DocumentManager.js
+++ src/document/DocumentManager.js
@@ -542,7 +542,11 @@ define(function (require, exports, module) {
         // On any change, mark the file dirty. In the future, we should make it so that if you
         // undo back to the last saved state, we mark the file clean.
         var wasDirty = this.isDirty;
-        this.isDirty = editor._codeMirror.isDirty();
+        if (editor._codeMirror.isDirty) {
+            this.isDirty = editor._codeMirror.isDirty();
+        } else {
+            this.isDirty = true;
+        }
 
         // If file just became dirty, notify listeners, and add it to working set (if not already there)
         if (wasDirty !== this.isDirty) {
