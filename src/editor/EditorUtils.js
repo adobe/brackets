4e7a44b5e0e662bcf27ae8723e3fb098c8e065f0
David Mignot <david.mig@gmail.com>
2012-05-16 22:54:26 +0200

    Added a couple of common language color syntaxing (php, coffee, ruby, ...)

--- src/editor/EditorUtils.js
+++ src/editor/EditorUtils.js
@@ -37,6 +37,16 @@ define(function (require, exports, module) {
     require("thirdparty/CodeMirror2/mode/css/css");
     require("thirdparty/CodeMirror2/mode/less/less");
     require("thirdparty/CodeMirror2/mode/htmlmixed/htmlmixed");
+    require("thirdparty/CodeMirror2/mode/xml/xml");
+    require("thirdparty/CodeMirror2/mode/clike/clike");
+    require("thirdparty/CodeMirror2/mode/php/php");
+    require("thirdparty/CodeMirror2/mode/coffeescript/coffeescript");
+    require("thirdparty/CodeMirror2/mode/clojure/clojure");
+    require("thirdparty/CodeMirror2/mode/perl/perl");
+    require("thirdparty/CodeMirror2/mode/ruby/ruby");
+    require("thirdparty/CodeMirror2/mode/mysql/mysql");
+    require("thirdparty/CodeMirror2/mode/diff/diff");
+    require("thirdparty/CodeMirror2/mode/markdown/markdown");
 
     /**
      * @private
@@ -72,6 +82,47 @@ define(function (require, exports, module) {
         case "htm":
         case "xhtml":
             return "htmlmixed";
+
+        case "xml":
+            return "xml";
+
+        case "php":
+            return "php";
+
+        case "c":
+        case "cc":
+        case "c++":
+        case "cxx":
+        case "cp":
+        case "cpp":
+        case "h":
+        case "hpp":
+        case "vert":
+        case "frag":
+        case "java":
+            return "clike";
+
+        case "coffee":
+            return "coffeescript";
+
+        case "clj":
+            return "clojure";
+
+        case "pl":
+            return "perl";
+        
+        case "rb":
+            return "ruby";
+
+        case "sql":
+            return "mysql";
+
+        case "diff":
+        case "patch":
+            return "diff";
+
+        case "md":
+            return "markdown";
 
         default:
             console.log("Called EditorUtils.js _getModeFromFileExtensions with an unhandled file extension: " + ext);
