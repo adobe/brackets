619422868f8a96c01f2f7c992ed7661e4939dcbe
Ty Voliter <tvoliter@cs.stanford.edu>
2012-06-11 18:32:35 -0700

    Fixing bug with offsetToLineNum.
    Tested boundary cases:
    - query is at char 0 of first line
    - query is at char 0 of last line
    - query is at last char of last line
    - query is at first of last line

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -91,16 +91,23 @@ define(function (require, exports, module) {
                 total = 0,
                 line;
             for (line = 0; line < lines.length; line++) {
-                if (total <= offset) {
+                if (total < offset) {
                     // add 1 per line since /n were removed by splitting, but they needed to 
                     // contribute to the total offset count
                     total += lines[line].length + 1;
+                } else if (total === offset) {
+                    return line;
                 } else {
                     return line - 1;
                 }
             }
 
-            return undefined;
+            // if offset is NOT over the total then offset is in the last line
+            if (offset <= total) {
+                return line - 1;
+            } else {
+                return undefined;
+            }
         } else {
             return textOrLines.substr(0, offset).split("\n").length - 1;
         }
