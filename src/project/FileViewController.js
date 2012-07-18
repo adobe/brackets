2069c84deccdb3e1f1c356163192c0a9a9370ccd
Ty Voliter <tvoliter@cs.stanford.edu>
2012-07-18 14:21:33 -0700

    code review fix

--- src/project/FileViewController.js
+++ src/project/FileViewController.js
@@ -116,7 +116,7 @@ define(function (require, exports, module) {
      */
     function setFileSelectionFocus(fileSelectionFocus) {
         if (fileSelectionFocus !== PROJECT_MANAGER && fileSelectionFocus !== WORKING_SET_VIEW) {
-            throw new Error("Bad parameter passed to FileViewController.openAndSelectDocument");
+            throw new Error("Bad parameter passed to FileViewController.setFileSelectionFocus");
         }
 
         _fileSelectionFocus = fileSelectionFocus;
