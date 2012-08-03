4c10e24b9f991d24e2131d6aaa532e2f99ebdfbc
Jonathan Diehl <jonathan.diehl@gmail.com>
2012-08-03 12:27:15 +0200

    add node events

--- src/LiveDevelopment/Agents/GotoAgent.js
+++ src/LiveDevelopment/Agents/GotoAgent.js
@@ -123,7 +123,13 @@ define(function GotoAgent(require, exports, module) {
             for (i in node.trace) {
                 _makeJSTarget(targets, node.trace[i]);
             }
-            for (i in res.matchedCSSRules) {
+            for (i in node.events) {
+                var trace = node.events[i];
+                if (trace.children.length > 0) {
+                    _makeJSTarget(targets, trace.children[0].callFrames[0]);
+                }
+            }
+            for (i in res.matchedCSSRules.reverse()) {
                 _makeCSSTarget(targets, res.matchedCSSRules[i]);
             }
             RemoteAgent.call("showGoto", targets);
