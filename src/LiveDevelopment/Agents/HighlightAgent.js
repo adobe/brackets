4a309ac47f2dd17834fa27b8d76a5c65f1da7f58
Jonathan Diehl <jonathan.diehl@gmail.com>
2012-08-17 20:36:56 +0200

    added $ jslint define

--- src/LiveDevelopment/Agents/HighlightAgent.js
+++ src/LiveDevelopment/Agents/HighlightAgent.js
@@ -23,7 +23,7 @@
 
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, forin: true, maxerr: 50, regexp: true */
-/*global define */
+/*global define, $ */
 
 /**
  * HighlightAgent dispatches events for highlight requests from in-browser
