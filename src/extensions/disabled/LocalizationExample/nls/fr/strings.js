b710d4076a67ebaac85d0f39f956ecb660e44a4f
Ty Voliter <tvoliter@cs.stanford.edu>
2012-08-23 11:33:57 -0700

    Add "French" prefix text to non translated strings examples to make localization example more clear

--- src/extensions/disabled/LocalizationExample/nls/fr/strings.js
+++ src/extensions/disabled/LocalizationExample/nls/fr/strings.js
@@ -3,7 +3,11 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 
+// This example currently displays English, but shows where French translations would go.
 define({
-    "COMMAND_NAME"        : "Ma nouvelle commande",
-    "ALERT_MESSAGE"       : "Il s'agit d'un message d'alerte échantillon"
+    "COMMAND_NAME"      : "French: My New Command",
+    "ALERT_MESSAGE"     : "French: This is a sample alert message",
+    "DIALOG_TITLE"		: "French: Localized Dialog Example",
+    "DIALOG_TEXT"		: "French: This is an example of localized text in Brackets",
+    "DIALOG_OK"			: "OK"
 });
