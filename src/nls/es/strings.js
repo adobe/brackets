643a73fbea99abaee85930b58a06d2e63746e71b
Chema <jbalsas@gmail.com>
2012-09-07 21:14:22 +0200

    - Replace Brackets with {APP_NAME} - Added new strings - Substituted characters not allowed previous to CEF3 Brackets

--- src/nls/es/strings.js
+++ src/nls/es/strings.js
@@ -49,61 +49,77 @@ define({
     "ERROR_RELOADING_FILE"              : "Ha ocurrido un error al intentar recargar el archivo <span class='dialog-filename'>{0}</span>. {1}",
     "ERROR_SAVING_FILE_TITLE"           : "Error guardando archivo",
     "ERROR_SAVING_FILE"                 : "Ha ocurrido un error al intentar guardar el archivo <span class='dialog-filename'>{0}</span>. {1}",
-    "INVALID_FILENAME_TITLE"            : "Nombre de archivo inv\u00E1lido",
+    "INVALID_FILENAME_TITLE"            : "Nombre de archivo inválido",
     "INVALID_FILENAME_MESSAGE"          : "Los nombres de archivo no pueden contener los siguientes caracteres: /?*:;{}<>\\|",
     "FILE_ALREADY_EXISTS"               : "El archivo <span class='dialog-filename'>{0}</span> ya existe.",
     "ERROR_CREATING_FILE_TITLE"         : "Error creando archivo",
     "ERROR_CREATING_FILE"               : "Ha ocurrido un error al intentar crear el archivo <span class='dialog-filename'>{0}</span>. {1}",
 
     // Application error strings
-    "ERROR_BRACKETS_IN_BROWSER_TITLE"   : "Vaya\u2026 parece que Brackets todav\u00EDa no funciona en navegadores.",
-    "ERROR_BRACKETS_IN_BROWSER"         : "Brackets est\u00E1 desarrollado en HTML, pero por ahora funciona como una aplicaci\u00F3n de escritorio para que puedas editar archivos en local. Por favor, utiliza la aplicaci\u00F3n del repositorio <b>github.com/adobe/brackets-app</b> para ejecutar Brackets.",
+    "ERROR_IN_BROWSER_TITLE"            : "Vaya... parece que {APP_NAME} todavía no funciona en navegadores.",
+    "ERROR_IN_BROWSER"                  : "{APP_NAME} está desarrollado en HTML, pero por ahora funciona como una aplicación de escritorio para que puedas editar archivos en local. Por favor, utiliza la aplicación del repositorio <b>github.com/adobe/brackets-app</b> para ejecutar {APP_NAME}.",
 
     // FileIndexManager error string
     "ERROR_MAX_FILES_TITLE"             : "Error indexando archivos",
-    "ERROR_MAX_FILES"                   : "Se ha alcanzado el n\u00FAmero m\u00E1ximo de archivos indexables. Puede que algunas de las acciones que utilizan archivos del \u00EDndice no funcionen correctamente.",
+    "ERROR_MAX_FILES"                   : "Se ha alcanzado el número máximo de archivos indexables. Puede que algunas de las acciones que utilizan archivos del índice no funcionen correctamente.",
     
     // CSSManager error strings
     "ERROR_PARSE_TITLE"                 : "Error parseando fichero(s) CSS:",
 
     // Live Development error strings
     "ERROR_LAUNCHING_BROWSER_TITLE"     : "Error iniciando navegador",
-    "ERROR_CANT_FIND_CHROME"            : "No se pudo encontrar el navegador Google Chrome. Por favor, aseg\u00FArate de que est\u00E9 instalado correctamente.",
+    "ERROR_CANT_FIND_CHROME"            : "No se pudo encontrar el navegador Google Chrome. Por favor, asegúrate de que esté instalado correctamente.",
     "ERROR_LAUNCHING_BROWSER"           : "Ha ocurrido un error al iniciar el navegador. (error {0})",
     
-    "LIVE_DEVELOPMENT_ERROR_TITLE"      : "Error en desarrollo en directo",
-    "LIVE_DEVELOPMENT_ERROR_MESSAGE"    : "No se pudo establecer una conexi\u00F3n en vivo con Chrome. Para poder desarrollar en vivo, Chrome debe ser iniciado depuraci\u00F3n remota habilitada.<br /><br />¿Quieres reiniciar Chrome habilitar la depuraci\u00F3n remota?",
-    "LIVE_DEV_NEED_HTML_MESSAGE"        : "Abre un archivo HTML para poder ejecutar la funcionalidad de desarrollo en directo.",
+    "LIVE_DEVELOPMENT_ERROR_TITLE"      : "Error en Desarrollo en Vivo",
+    "LIVE_DEVELOPMENT_RELAUNCH_TITLE"   : "Conectando con el navegador",
+    "LIVE_DEVELOPMENT_ERROR_MESSAGE"    : "Para poder iniciar el modo de Desarrollo en Vivo, Chrome debe ser iniciado habilitando la depuración remota.<br /><br />¿Quieres reiniciar Chrome y habilitar la depuración remota?",
+    "LIVE_DEV_NEED_HTML_MESSAGE"        : "Abre un archivo HTML para poder iniciar el modo de Desarrollo en Vivo.",
+    "LIVE_DEVELOPMENT_INFO_TITLE"       : "Bienvenido a Desarrollo en Vivo!",
+    "LIVE_DEVELOPMENT_INFO_MESSAGE"     : "Desarrollo en Vivo conecta {APP_NAME} con tu navegador. Lanza una vista previa de tu archivo HTML en el navegador y la actualiza a medida que modificas tu código.<br /><br />En esta versión preliminar de {APP_NAME}, Desarollo en Vivo sólo funciona para cambios de <strong>archivos CSS</strong> y únicamente con <strong>Google Chrome</strong>. ¡Pronto estará disponible también para HTML y JavaScript!<br /><br />(No volverás a ver este mensaje.)",
     
-    "LIVE_DEV_STATUS_TIP_NOT_CONNECTED" : "Desarrollo en directo",
-    "LIVE_DEV_STATUS_TIP_PROGRESS1"     : "Desarrollo en directo: Conectando...",
-    "LIVE_DEV_STATUS_TIP_PROGRESS2"     : "Desarrollo en directo: Inicializando...",
-    "LIVE_DEV_STATUS_TIP_CONNECTED"     : "Terminar desarrollo en directo",
+    "LIVE_DEV_STATUS_TIP_NOT_CONNECTED" : "Desarrollo en Vivo",
+    "LIVE_DEV_STATUS_TIP_PROGRESS1"     : "Desarrollo en Vivo: Conectando...",
+    "LIVE_DEV_STATUS_TIP_PROGRESS2"     : "Desarrollo en Vivo: Inicializando...",
+    "LIVE_DEV_STATUS_TIP_CONNECTED"     : "Terminar Desarrollo en Vivo",
     
     "SAVE_CLOSE_TITLE"                  : "Guardar cambios",
     "SAVE_CLOSE_MESSAGE"                : "¿Quieres guardar los cambios existentes en el documento <span class='dialog-filename'>{0}</span>?",
     "SAVE_CLOSE_MULTI_MESSAGE"          : "¿Quieres guardar tus cambios en los siguientes documentos?",
     "EXT_MODIFIED_TITLE"                : "Cambios externos",
-    "EXT_MODIFIED_MESSAGE"              : "<span class='dialog-filename'>{0}</span> ha sido modificado, pero tambi\u00E9n tiene cambios en Brackets.<br /><br />¿Qu\u00E9 versi\u00F3n quieres conservar?",
-    "EXT_DELETED_MESSAGE"               : "<span class='dialog-filename'>{0}</span> ha sido eliminado, pero tiene cambios sin guardar en Brackets.<br /><br />¿Quieres conservar tus cambios?",
+    "EXT_MODIFIED_MESSAGE"              : "<span class='dialog-filename'>{0}</span> ha sido modificado, pero también tiene cambios en {APP_NAME}.<br /><br />¿Qué versión quieres conservar?",
+    "EXT_DELETED_MESSAGE"               : "<span class='dialog-filename'>{0}</span> ha sido eliminado, pero tiene cambios sin guardar en {APP_NAME}.<br /><br />¿Quieres conservar tus cambios?",
     
     // Find, Replace, Find in Files
-    "SEARCH_REGEXP_INFO"                : "Utiliza /re/ para b\u00FAsquedas con expresiones regulares",
+    "SEARCH_REGEXP_INFO"                : "Utiliza /re/ para búsquedas con expresiones regulares",
     "WITH"                              : "Con",
-    "BUTTON_YES"                        : "S\u00ED",
+    "BUTTON_YES"                        : "Sí",
     "BUTTON_NO"                         : "No",
     "BUTTON_STOP"                       : "Parar",
 
     "OPEN_FILE"                         : "Abrir archivo",
+    "CHOOSE_FOLDER"                     : "Elige una carpeta",
 
-    "RELEASE_NOTES"                     : "Notas de versi\u00F3n",
-    "NO_UPDATE_TITLE"                   : "¡Est\u00E1s actualizado!",
-    "NO_UPDATE_MESSAGE"                 : "Est\u00E1s utilizando la \u00FAltima versi\u00F3n de Brackets.",
+    "RELEASE_NOTES"                     : "Notas de versión",
+    "NO_UPDATE_TITLE"                   : "¡Estás actualizado!",
+    "NO_UPDATE_MESSAGE"                 : "Estás utilizando la última versión de {APP_NAME}.",
+    
+    "FIND_IN_FILES_TITLE"               : "- {0} {1} in {2} {3}",
+    "FIND_IN_FILES_FILE"                : "archivo",
+    "FIND_IN_FILES_FILES"               : "archivos",
+    "FIND_IN_FILES_MATCH"               : "coincidencia",
+    "FIND_IN_FILES_MATCHES"             : "coincidencias",
+    "FIND_IN_FILES_MAX"                 : " (mostrando las premeras {0} coincidencias)",
+    "FIND_IN_FILES_FILE_PATH"           : "Archivo: <b>{0}</b>",
+    "FIND_IN_FILES_LINE"                : "línea:&nbsp;{0}",
 
+    "ERROR_FETCHING_UPDATE_INFO_TITLE"  : "Error obteniendo información sobre actualizaciones",
+    "ERROR_FETCHING_UPDATE_INFO_MSG"    : "Ocurrió un problema al obtener la información sobre las últimas actualizaciones desde el servidor. Por favor, asegúrate de estar conectado a internet y vuelve a intentarlo.",
+    
     // Switch language
     "LANGUAGE_TITLE"                    : "Cambiar idioma",
     "LANGUAGE_MESSAGE"                  : "Por favor, elige el idioma deseado de la siguiente lista:",
-    "LANGUAGE_SUBMIT"                   : "Reiniciar Brackets",
+    "LANGUAGE_SUBMIT"                   : "Reiniciar {APP_NAME}",
     "LANGUAGE_CANCEL"                   : "Cancelar",
 
     /**
@@ -120,7 +136,7 @@ define({
     "FILE_MENU"                           : "Archivo",
     "CMD_FILE_NEW"                        : "Nuevo",
     "CMD_FILE_OPEN"                       : "Abrir\u2026",
-    "CMD_ADD_TO_WORKING_SET"              : "A\u00F1adir a espacio de trabajo",
+    "CMD_ADD_TO_WORKING_SET"              : "Añadir a espacio de trabajo",
     "CMD_OPEN_FOLDER"                     : "Abrir carpeta\u2026",
     "CMD_FILE_CLOSE"                      : "Cerrar",
     "CMD_FILE_CLOSE_ALL"                  : "Cerrar todo",
@@ -137,27 +153,27 @@ define({
     "CMD_FIND_NEXT"                       : "Buscar siguiente",
     "CMD_FIND_PREVIOUS"                   : "Buscar anterior",
     "CMD_REPLACE"                         : "Reemplazar",
-    "CMD_INDENT"                          : "Aumentar indentaci\u00F3n",
-    "CMD_UNINDENT"                        : "Disminuir indentaci\u00F3n",
+    "CMD_INDENT"                          : "Aumentar indentación",
+    "CMD_UNINDENT"                        : "Disminuir indentación",
     "CMD_DUPLICATE"                       : "Duplicar",
-    "CMD_COMMENT"                         : "Comentar/Descomentar l\u00EDneas",
-    "CMD_LINE_UP"                         : "Subir l\u00EDnea(s)",
-    "CMD_LINE_DOWN"                       : "Bajar l\u00EDnea(s)",
+    "CMD_COMMENT"                         : "Comentar/Descomentar líneas",
+    "CMD_LINE_UP"                         : "Subir línea(s)",
+    "CMD_LINE_DOWN"                       : "Bajar línea(s)",
      
     // View menu commands
     "VIEW_MENU"                           : "Ver",
-    "CMD_HIDE_SIDEBAR"                    : "Ocultar men\u00FA lateral",
-    "CMD_SHOW_SIDEBAR"                    : "Mostrar men\u00FA lateral",
-    "CMD_INCREASE_FONT_SIZE"              : "Aumentar tama\u00F1o de fuente",
-    "CMD_DECREASE_FONT_SIZE"              : "Disminuir tama\u00F1o de fuente",
-    "CMD_RESTORE_FONT_SIZE"               : "Restablecer tama\u00F1o de fuente",
+    "CMD_HIDE_SIDEBAR"                    : "Ocultar menú lateral",
+    "CMD_SHOW_SIDEBAR"                    : "Mostrar menú lateral",
+    "CMD_INCREASE_FONT_SIZE"              : "Aumentar tamaño de fuente",
+    "CMD_DECREASE_FONT_SIZE"              : "Disminuir tamaño de fuente",
+    "CMD_RESTORE_FONT_SIZE"               : "Restablecer tamaño de fuente",
 
     // Navigate menu Commands
     "NAVIGATE_MENU"                       : "Navegar",
-    "CMD_QUICK_OPEN"                      : "Apertura r\u00E1pida",
-    "CMD_GOTO_LINE"                       : "Ir a l\u00EDnea",
-    "CMD_GOTO_DEFINITION"                 : "Ir a definici\u00F3n",
-    "CMD_TOGGLE_QUICK_EDIT"               : "Edici\u00F3n r\u00E1pida",
+    "CMD_QUICK_OPEN"                      : "Apertura rápida",
+    "CMD_GOTO_LINE"                       : "Ir a línea",
+    "CMD_GOTO_DEFINITION"                 : "Ir a definición",
+    "CMD_TOGGLE_QUICK_EDIT"               : "Edición rápida",
     "CMD_QUICK_EDIT_PREV_MATCH"           : "Coincidencia anterior",
     "CMD_QUICK_EDIT_NEXT_MATCH"           : "Coincidencia siguiente",
     "CMD_NEXT_DOC"                        : "Documento siguiente",
@@ -165,28 +181,30 @@ define({
     
     // Debug menu commands
     "DEBUG_MENU"                          : "Desarrollo",
-    "CMD_REFRESH_WINDOW"                  : "Reiniciar Brackets",
+    "CMD_REFRESH_WINDOW"                  : "Reiniciar {APP_NAME}",
     "CMD_SHOW_DEV_TOOLS"                  : "Mostrar herramientas para desarrolladores",
     "CMD_RUN_UNIT_TESTS"                  : "Ejecutar tests",
     "CMD_JSLINT"                          : "Habilitar JSLint",
-    "CMD_SHOW_PERF_DATA"                  : "Mostrar informaci\u00F3n de rendimiento",
-    "CMD_NEW_BRACKETS_WINDOW"             : "Nueva ventana de Brackets",
-    "CMD_USE_TAB_CHARS"                   : "Usar tabulaciones",
+    "CMD_SHOW_PERF_DATA"                  : "Mostrar información de rendimiento",
+    "CMD_NEW_BRACKETS_WINDOW"             : "Nueva ventana de {APP_NAME}",
     "CMD_SHOW_EXTENSIONS_FOLDER"          : "Abrir carpeta de extensiones",
+    "CMD_USE_TAB_CHARS"                   : "Usar tabulaciones",
     "CMD_SWITCH_LANGUAGE"                 : "Cambiar idioma",
     "CMD_CHECK_FOR_UPDATE"                : "Buscar actualizaciones",
 
     // Help menu commands
-    "CMD_ABOUT"                           : "Acerca de\u2026",
+    "HELP_MENU"                           : "Ayuda",
+    "CMD_ABOUT"                           : "Acerca de...",
+    "CMD_FORUM"                           : "Foro de {APP_NAME}",
 
     // Special commands invoked by the native shell
     "CMD_CLOSE_WINDOW"                    : "Cerrar ventana",
     "CMD_ABORT_QUIT"                      : "Cancelar salida",
 
     // Strings for main-view.html
-    "EXPERIMENTAL_BUILD"                   : "Versi\u00F3n experimental",
+    "EXPERIMENTAL_BUILD"                   : "Versión experimental",
     "JSLINT_ERRORS"                        : "Errores de JSLint",
-    "SEARCH_RESULTS"                       : "Resultados de b\u00FAsqueda",
+    "SEARCH_RESULTS"                       : "Resultados de búsqueda",
     "OK"                                   : "Aceptar",
     "DONT_SAVE"                            : "No guardar",
     "SAVE"                                 : "Guardar",
@@ -195,12 +213,14 @@ define({
     "KEEP_CHANGES_IN_EDITOR"               : "Conservar los cambios del editor",
     "CLOSE_DONT_SAVE"                      : "Cerrar (No guardar)",
     "RELAUNCH_CHROME"                      : "Reiniciar Chrome",
-    "ABOUT"                                : "Acerca de",
-    "BRACKETS"                             : "Brackets",
+    "ABOUT"                                : "Acerca de...",
+    "APP_NAME"                             : "{APP_NAME}",
     "CLOSE"                                : "Cerrar",
-    "ABOUT_TEXT_LINE1"                     : "sprint 13 versi\u00F3n experimental ",
-    "UPDATE_NOTIFICATION_TOOLTIP"          : "¡Hay una nueva versi\u00F3n de Brackets disponible! Haz click aqu\u00ED para m\u00E1s detalles.",
-    "UPDATE_AVAILABLE_TITLE"               : "Actualizaci\u00F3n disponible",
-    "UPDATE_MESSAGE"                       : "Oye, hay una nueva versi\u00F3n de Brackets disponible. \u00C9stas son algunas de las nuevas caracter\u00EDsticas:",
-    "GET_IT_NOW"                           : "¡Cons\u00EDguelo ahora!"
+    "ABOUT_TEXT_LINE1"                     : "sprint 14 versión experimental ",
+    "ABOUT_TEXT_LINE3"                     : "Los avisos, términos y condiciones pertenecientes a software de terceros se encuentran en <span class=\"non-clickble-link\">http://www.adobe.com/go/thirdparty/</span> y se incluyen aquí como referencia.",
+    "ABOUT_TEXT_LINE4"                     : "Puedes encontrar la documentación y código fuente en <span class=\"non-clickble-link\">https://github.com/adobe/brackets/</span>",
+    "UPDATE_NOTIFICATION_TOOLTIP"          : "¡Hay una nueva versión de {APP_NAME} disponible! Haz click aquí para más detalles.",
+    "UPDATE_AVAILABLE_TITLE"               : "Actualización disponible",
+    "UPDATE_MESSAGE"                       : "Oye, hay una nueva versión de {APP_NAME} disponible. Éstas son algunas de las nuevas características:",
+    "GET_IT_NOW"                           : "¡Consíguelo ahora!"
 });
