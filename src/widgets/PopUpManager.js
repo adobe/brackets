bd175a4a64af3f4e46695c8468f59b40500ee0df
Jacques Couzteau <couzteau@bitfaeule.net>
2012-09-12 14:00:34 -0700

    add global window to satisfy JSLint

--- src/widgets/PopUpManager.js
+++ src/widgets/PopUpManager.js
@@ -23,7 +23,7 @@
 
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets */
+/*global define, window, $, brackets */
 
 /**
  * Utilities for managing pop-ups.
