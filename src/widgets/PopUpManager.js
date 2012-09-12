064e58c680d3c4655fd463146be2803c7fd4da54
Jacques Couzteau <couzteau@bitfaeule.net>
2012-09-12 13:57:41 -0700

    add global $ t satisfy JSLint

--- src/widgets/PopUpManager.js
+++ src/widgets/PopUpManager.js
@@ -23,7 +23,7 @@
 
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, brackets */
+/*global define, $, brackets */
 
 /**
  * Utilities for managing pop-ups.
