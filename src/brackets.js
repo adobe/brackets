01d599d6c075ec256d8025f0d9484bab6e3b6c0a
Jason San Jose <jasonsj@adobe.com>
2012-09-17 16:50:06 -0700

    package.json name can't take special chars

--- src/brackets.js
+++ src/brackets.js
@@ -270,7 +270,7 @@ define(function (require, exports, module) {
     // Localize MainViewHTML and inject into <BODY> tag
     var templateVars    = $.extend({
         ABOUT_ICON          : brackets.config.about_icon,
-        APP_NAME_ABOUT_BOX  : brackets.metadata.name,
+        APP_NAME_ABOUT_BOX  : brackets.config.app_name_about,
         VERSION             : brackets.metadata.version
     }, Strings);
     
