8fc980c214cce5b84f40bfe4694b3e7ac7f59973
wALF Utility <walf@adobe.com>
2012-09-18 09:41:27 -0700

    Updated by ALF automation.

--- src/nls/fr/strings.js
+++ src/nls/fr/strings.js
@@ -199,7 +199,7 @@ define({
 
     // Help menu commands
 	"HELP_MENU": "Aide",
-	"CMD_ABOUT": "[P] !é=About {APP_TITLE}=!",
+	"CMD_ABOUT": "A propos de {APP_TITLE}",
 	"CMD_FORUM": "Forum {APP_NAME}",
 
     // Special commands invoked by the native shell
