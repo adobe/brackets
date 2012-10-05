0cd3bf8fdf633f0da06e0ea871c369baf5f1de90
Jonathan Diehl <jonathan.diehl@gmail.com>
2012-10-05 12:42:49 +0200

    bugfix: navigator.language is not set in brackets-shell -> fall back to brackets.app.language

--- src/brackets.js
+++ src/brackets.js
@@ -34,7 +34,7 @@ require.config({
     // NOTE: When we change to navigator.language here, we also should change to
     // navigator.language in ExtensionLoader (when making require contexts for each
     // extension).
-    locale: window.localStorage.getItem("locale") || navigator.language
+    locale: window.localStorage.getItem("locale") || navigator.language || brackets.app.language
 });
 
 /**
