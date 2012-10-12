7ef4f2fc3c9315bd17f3c5ec2cfe83cf0b1a449b
wALF Utility <walf@adobe.com>
2012-10-11 18:17:09 -0700

    Updated by ALF automation.

--- src/nls/fr/strings.js
+++ src/nls/fr/strings.js
@@ -143,6 +143,8 @@ define({
 	"STATUSBAR_CURSOR_POSITION": "Line {0}, Column {1}",
 	"STATUSBAR_INDENT_TOOLTIP_SPACES": "Click to switch indentation to spaces",
 	"STATUSBAR_INDENT_TOOLTIP_TABS": "Click to switch indentation to tabs",
+	"STATUSBAR_INDENT_SIZE_TOOLTIP_SPACES": "Click to change number of spaces used when indenting",
+	"STATUSBAR_INDENT_SIZE_TOOLTIP_TABS": "Click to change tab character width",
 	"STATUSBAR_SPACES": "Espaces",
 	"STATUSBAR_TAB_SIZE": "Tab Size",
 	"STATUSBAR_LINE_COUNT": "{0} Lines",
