fb8d61c299449b791db96ebb28ac71aac385b203
Kraig Walker <kraig_walker@me.com>
2012-10-24 23:04:34 +0100

    "Lines" are now singular "Line"

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -180,10 +180,10 @@ define({
     "CMD_INDENT"                          : "Indent",
     "CMD_UNINDENT"                        : "Unindent",
     "CMD_DUPLICATE"                       : "Duplicate",
-    "CMD_DELETE_LINES"                    : "Delete Lines",
+    "CMD_DELETE_LINES"                    : "Delete Line",
     "CMD_COMMENT"                         : "Comment/Uncomment Lines",
-    "CMD_LINE_UP"                         : "Move Lines Up",
-    "CMD_LINE_DOWN"                       : "Move Lines Down",
+    "CMD_LINE_UP"                         : "Move Line Up",
+    "CMD_LINE_DOWN"                       : "Move Line Down",
      
     // View menu commands
     "VIEW_MENU"                           : "View",
