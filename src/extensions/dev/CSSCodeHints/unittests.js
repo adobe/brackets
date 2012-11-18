eea3debc2a02c8babc625fd3aa1d0a12fee1e785
Andre Zoufahl <zoufahl@zib.de>
2012-11-18 12:20:33 -0800

    Added more unit tests handling general css hint features.

--- src/extensions/dev/CSSCodeHints/unittests.js
+++ src/extensions/dev/CSSCodeHints/unittests.js
@@ -56,6 +56,80 @@ define(function (require, exports, module) {
             expect(query).toBeTruthy();
             expect(query.queryStr).toBeNull();
         }        
+
+        
+        // Expect hintList to contain attribute names, starting with given value (if unspecified, expects the default unfilered list)
+        function verifyAttrHints(hintList, expectedFirstHint) {
+            expect(hintList.indexOf("div")).toBe(-1);   // make sure tag names aren't sneaking in there
+            
+            expect(hintList[0]).toBe(expectedFirstHint);
+        }        
+
+        describe("CSS attributes in general (selection of correct attribute based on input)", function () {
+            var defaultContent = ".selector {\n" +
+                                 " \n" +
+                                 " b\n" +
+                                 " bord\n" +
+                                 " border-\n" +
+                                 " border-colo\n" +
+                                 "} \n";
+                    
+            beforeEach(function () {
+                // create dummy Document for the Editor
+                testDocument = SpecRunnerUtils.createMockDocument(defaultContent);
+                
+                // create Editor instance (containing a CodeMirror instance)
+                $("body").append("<div id='editor'/>");
+                testEditor = new Editor(testDocument, true, "css", $("#editor").get(0), {});
+            });
+            
+            
+            
+            it("should list all hints right after curly bracket", function () {
+                testEditor.setCursorPos({ line: 0, ch: 11 });    // inside h1, after {
+                
+                var hintList = expectHints(CSSCodeHints.attrHintProvider);
+                verifyAttrHints(hintList, "align-content");  // filtered on "empty string"
+            });
+            
+            it("should list all hints in new line", function () {
+                testEditor.setCursorPos({ line: 1, ch: 1 });
+                
+                var hintList = expectHints(CSSCodeHints.attrHintProvider);
+                verifyAttrHints(hintList, "align-content");  // filtered on "empty string"
+            });
+
+            it("should list all hints starting with 'b' in new line", function () {
+                testEditor.setCursorPos({ line: 2, ch: 2 });
+                
+                var hintList = expectHints(CSSCodeHints.attrHintProvider);
+                verifyAttrHints(hintList, "backface-visibility");  // filtered on "b"
+            });            
+
+            it("should list all hints starting with 'bord' ", function () {
+                testEditor.setCursorPos({ line: 3, ch: 5 });
+                
+                var hintList = expectHints(CSSCodeHints.attrHintProvider);
+                verifyAttrHints(hintList, "border");  // filtered on "bord"
+            });
+
+            it("should list all hints starting with 'border-' ", function () {
+                testEditor.setCursorPos({ line: 4, ch: 8 });
+                
+                var hintList = expectHints(CSSCodeHints.attrHintProvider);
+                verifyAttrHints(hintList, "border-collapse");  // filtered on "border-"
+            });
+
+            it("should list only hint border-color", function () {
+                testEditor.setCursorPos({ line: 5, ch: 12 });
+                
+                var hintList = expectHints(CSSCodeHints.attrHintProvider);
+                verifyAttrHints(hintList, "border-color");  // filtered on "border-color"  
+                expect(hintList.length).toBe(1);
+            });
+            
+        });        
+        
         
         describe("CSS attribute hint provider in .css files", function () {
             
@@ -74,5 +148,42 @@ define(function (require, exports, module) {
                 expectNoHints(CSSCodeHints.attrHintProvider);
             });
         });
+        
+        describe("CSS attribute hint provider inside mixed htmlfiles", function () {
+            var defaultContent = "<html> \n"+
+                                 "<head></head> \n"+
+                                 "<body> <style> \n"+
+                                 " body { \n"+
+                                 "    background-color: red; \n"+
+                                 " \n"+ 
+                                 "} \n"+
+                                 "</style></body></html>";
+                    
+            beforeEach(function () {
+                // create dummy Document for the Editor
+                testDocument = SpecRunnerUtils.createMockDocument(defaultContent);
+                
+                // create Editor instance (containing a CodeMirror instance)
+                $("body").append("<div id='editor'/>");
+                testEditor = new Editor(testDocument, true, "hmtlmixed", $("#editor").get(0), {});
+            });
+            
+            it("should list hints right after curly bracket", function () {
+                testEditor.setCursorPos({ line: 3, ch: 7 });    // inside h1, after {
+                expectHints(CSSCodeHints.attrHintProvider);
+            }); 
+  
+            it("should NOT list hints right before curly bracket", function () {
+                testEditor.setCursorPos({ line: 3, ch: 6 });    // inside h1, after {
+                expectNoHints(CSSCodeHints.attrHintProvider);
+            });             
+
+            it("should NOT inside head-tag", function () {
+                testEditor.setCursorPos({ line: 1, ch: 6 });    // between <head> and </head> {
+                expectNoHints(CSSCodeHints.attrHintProvider);
+            }); 
+            
+        });
+        
     });
 });
