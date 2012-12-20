9a9148d176f2ee16437827c5529073d7f6dbcc07
Tomás Malbrán <malbrantomas@gmail.com>
2012-12-19 22:42:37 -0300

    New Key bindings and keep cursor on screen

--- src/view/ViewCommandHandlers.js
+++ src/view/ViewCommandHandlers.js
@@ -133,10 +133,33 @@ define(function (require, exports, module) {
      * @param {number} -1 to scroll one line up; 1 to scroll one line down.
      */
     function _scrollLine(direction) {
-        var editor     = EditorManager.getCurrentFullEditor();
-        var scrollPos  = editor.getScrollPos();
-        var textHeight = editor.getTextHeight();
+        var editor        = EditorManager.getCurrentFullEditor(),
+            scrollPos     = editor.getScrollPos(),
+            totalHeight   = $("#editor-holder").height(), // editor.getTotalHeight();
+            textHeight    = editor.getTextHeight(),
+            visibleLines  = Math.floor(totalHeight / textHeight),
+            firstLine     = Math.floor(scrollPos.y / textHeight),
+            lastLine      = firstLine + visibleLines - 1,
+            cursorPos     = editor.getCursorPos(),
+            hasSelecction = editor.hasSelection();
         
+        // If there is no selection move the cursor so that is always visible
+        if (!hasSelecction) {
+            // Move the cursor to the first visible line
+            if (direction > 0 && cursorPos.line < firstLine) {
+                editor.setCursorPos({line: firstLine + 1, ch: cursorPos.ch});
+            
+            // Move the cursor to the last visible line
+            } else if (direction < 0 && cursorPos.line > lastLine) {
+                editor.setCursorPos({line: lastLine - 1, ch: cursorPos.ch});
+            
+            // Move the cursor up or down using CodeMirror function
+            } else if ((direction > 0 && cursorPos.line === firstLine) || (direction < 0 && cursorPos.line === lastLine)) {
+                editor._codeMirror.moveV(direction, "line");
+            }
+        }
+        
+        // Scroll the editor
         editor.setScrollPos(scrollPos.x, scrollPos.y + (textHeight * direction));
     }
     
