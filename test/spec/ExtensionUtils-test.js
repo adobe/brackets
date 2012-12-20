bdcee4f94a505f4dae6968af8dc906cba4349664
Jason San Jose <jasonsj@adobe.com>
2012-12-19 18:07:37 -0800

    Use jQuery load event to detect stylesheet loaded

--- test/spec/ExtensionUtils-test.js
+++ test/spec/ExtensionUtils-test.js
@@ -52,33 +52,11 @@ define(function (require, exports, module) {
 
             function loadStyleSheet(doc, path) {
 
-                var styleSheetsCount, lastStyleSheetIndex;
-
-                expect(doc).toBeTruthy();
-                expect(doc.styleSheets).toBeTruthy();
-
                 // attach style sheet
                 runs(function () {
-                    styleSheetsCount = doc.styleSheets.length;
                     var promise = ExtensionUtils.loadStyleSheet(module, path);
                     waitsForDone(promise, "loadStyleSheet: " + path);
                 });
-
-                // even though promise has resolved, stylesheet may still not be loaded,
-                // so wait until styleSheets array length has been incremented.
-                waitsFor(function () {
-                    return (doc.styleSheets.length > styleSheetsCount);
-                }, "loadStyleSheet() increments array timeout", 1000);
-
-                // after styleSheets array is incremented, wait for cssRules object to be defined.
-                // note that this works for Chrome, but not sure about other browsers.
-                // someday there may be an event to listen for...
-                runs(function () {
-                    lastStyleSheetIndex = doc.styleSheets.length - 1;
-                });
-                waitsFor(function () {
-                    return (doc.styleSheets[lastStyleSheetIndex].cssRules);
-                }, "loadStyleSheet() cssRules defined timeout", 1000);
             }
 
             // putting everything CSS related in 1 test so it runs faster
@@ -98,18 +76,14 @@ define(function (require, exports, module) {
                     // second.css is imported in basic.css
                     var fontWeight = $projectTitle.css("font-weight");
                     expect(fontWeight).toEqual("500");
-                });
-
-                // attach another style sheet in a sub-directory with space in name.
-                runs(function () {
-                    loadStyleSheet(testWindow.document, "ExtensionUtils-test-files/sub dir/third.css");
-                });
-
-                runs(function () {
-                    // third.css
-                    var $projectTitle = testWindow.$("#project-title");
+                    
+                    // third.css is imported in second.css
                     var fontVariant = $projectTitle.css("font-variant");
                     expect(fontVariant).toEqual("small-caps");
+                    
+                    // fourth.css is imported in basic.css
+                    var fontFamily = $projectTitle.css("font-family");
+                    expect(fontFamily).toEqual("serif");
                 });
             });
             
