4fcf4ecb00ff1d2ff087924a27a1ec3018434880
Andre Zoufahl <zoufahl@zib.de>
2013-01-05 22:18:51 +0100

    further implemented hasHints, getHints, (todo: insertHints) - still not stable/ready to use code

--- src/extensions/dev/CSSCodeHints/main.js
+++ src/extensions/dev/CSSCodeHints/main.js
@@ -19,27 +19,45 @@ define(function (require, exports, module) {
         this.cssMode = "";
     }
     
-    CssAttrHints.prototype.hasHints = function (editor, cursor) {
+    CssAttrHints.prototype.hasHints = function (editor, implicitChar) {
+        this.editor = editor;
         var query       = {queryStr: null},
+            cursor      = this.editor.getCursorPos(),
             pos         = $.extend({}, cursor),
             ctx         = TokenUtils.getInitialContext(editor._codeMirror, pos),
             styleblocks = HTMLUtils.findStyleBlocks(editor),
             selector    = CSSUtils.findSelectorAtDocumentPos(editor, cursor),
             csscontext  = false;
-          
-        /* first: check overall context - is this a css-valid context */
+
+        this.info = CSSUtils.getInfoAtPos(editor, cursor);
+        console.log(this.info);
+        
+        if (implicitChar === null) {
+            if (this.info.context === "prop.value" || this.info.context === "prop.name") {
+                return true;
+            }
+        } else {
+            return (this.alphabet.indexOf(implicitChar) !== -1);
+        }
+        
+        return false;
+        
+        
+        
+/*        
+        // first: check overall context - is this a css-valid context /
         if (editor.getModeForSelection() === "css") {
             // we know we are dealing with a .css file or the cursor is inside a <style/> tag, but we don't know if we are actually
             // inside a set of rules { } or between two or in a selector
             // we only need to check if we hit a { and return true, or } and return false, if we hit a ; it's okay, but not sufficent
             do {
                 if (ctx.token.string === "{") {
-                    /* first relevant symbol, everything fine */
+                    // first relevant symbol, everything fine
                     csscontext = true;
                     ctx = TokenUtils.getInitialContext(editor._codeMirror, cursor);
                     break;
                 } else if (ctx.token.string === "}") {
-                    /* cursor is outside of set of rules */
+                    // cursor is outside of set of rules
                     csscontext = false;
                     break;
                 }
@@ -47,9 +65,8 @@ define(function (require, exports, module) {
         }
         
         
-        /* third: determine queryStr based on characters around cursor if actually in csscontext */
+        // third: determine queryStr based on characters around cursor if actually in csscontext 
         if (csscontext && selector !== "") {
-            console.log(CSSUtils.getInfoAtPos(editor, cursor));
             query.queryStr = ctx.token.string;
             if (query.queryStr !== null) {
                 query.queryStr = query.queryStr.trim();
@@ -63,31 +80,79 @@ define(function (require, exports, module) {
                 }
             }
             
-            /* notes: we need some contextinformation around cursor, 2-3 tokens to determine if we need to show attrs or values */
-            this.cssMode = "attr";
+            // notes: we need some contextinformation around cursor, 2-3 tokens to determine if we need to show attrs or values
+            this.cssMode = "prop-name";
             if (query.queryStr === "{" || query.queryStr === ";") {
-                /* cssattribute context */
+                // cssattribute context
                 query.queryStr = "";
             } else if (query.queryStr === ":") {
-                /* cssattrvalue context */
-                /* move token 1 step back to get attibutename */
+                // cssattrvalue context
+                // move token 1 step back to get attibutename
                 query.queryStr = "";
                 query.attrName = query.prevStr;
-                this.cssMode = "value";
+                this.cssMode = "prop-value";
 
             } else if (query.prevStr === ":") {
                 TokenUtils.moveSkippingWhitespace(TokenUtils.movePrevToken, ctx);
                 query.attrName = ctx.token.string;
-                this.cssMode = "value";
+                this.cssMode = "prop-value";
             }
         }
-        return query;
+        return false;
+*/
+        
     };
     
-    CssAttrHints.prototype.search = function (query) {
+    CssAttrHints.prototype.getHints = function (implicitChar) {
+        this.info = CSSUtils.getInfoAtPos(this.editor, this.editor.getCursorPos());
+
+        var needle = this.info.name,
+            context = this.info.context,
+            result;
+        
+        var list = null;
+        if (context === "prop.value") {
+            if (!attributes[needle]) {
+                return null;
+            } else {
+                result = $.map(attributes[needle].values, function (pvalue, pindex) {
+                    if (pvalue.indexOf(needle) === 0) {
+                        return pvalue;
+                    }
+                });
+                
+                return {
+                    hints: result,
+                    match: needle,
+                    selectInitial: true
+                };
+            }
+            
+            
+        } else if (context === "prop.name") {
+            result = $.map(attributes, function (pvalues, pname) {
+                if (pname.indexOf(needle) === 0) {
+                    return pname;
+                }
+            }).sort();
+            
+            return {
+                hints: result,
+                match: needle,
+                selectInitial: true
+            };
+        
+        }
+        
+        return null;
+        
+        
+        
+/*        var query;
         var result = [],
             filter = query.queryStr,
             attrName = query.attrName;
+        
         
         if (attrName) {
             if (!attributes[attrName]) {
@@ -111,27 +176,31 @@ define(function (require, exports, module) {
                 });
             }
         }
-        return result;
+        return result;*/
     };
     
-    CssAttrHints.prototype.handleSelect = function (completion, editor, cursor) {
+    CssAttrHints.prototype.insertHint = function (hint) {
+        return false;
+    };
+    
+    CssAttrHints.prototype.handleSelect2 = function (completion, editor, cursor) {
         var ctx  = TokenUtils.getInitialContext(editor._codeMirror, cursor),
             closure = "";
 /*
         TokenUtils.moveSkippingWhitespace(TokenUtils.moveNextToken, ctx);
         console.log(ctx.token.string);
         TokenUtils.moveSkippingWhitespace(TokenUtils.movePrevToken, ctx);
-*/      
-        if (ctx.token !== null) {            
-            if (this.cssMode === "value") {
+*/
+        if (ctx.token !== null) {
+            if (this.cssMode === "prop-value") {
                 closure = ";";
-            } else if (this.cssMode === "attr") {
+            } else if (this.cssMode === "prop-name") {
                 closure = ": ";
             }
             
-            if (len == completion.length) {
+/*            if (len === completion.length) {
                 closure = "";
-            }
+            }*/
             
             /* if token is special char, 'len' will be wrong, since a wrong token is used, set token directly to be the empty string */
             if (ctx.token.string === "{" || ctx.token.string === ":" || ctx.token.string === ";") {
@@ -156,24 +225,24 @@ define(function (require, exports, module) {
      * Check whether to select the first item in the list by default
      * @return {boolean} return true to highlight the first item.
      */
-    CssAttrHints.prototype.wantInitialSelection = function () {
+/*    CssAttrHints.prototype.wantInitialSelection = function () {
         return true;
-    };    
+    }; */
     
     /**
      * Check whether to show hints on a specific key.
      * @param {string} key -- the character for the key user just presses.
      * @return {boolean} return true/false to indicate whether hinting should be triggered by this key.
      */
-    CssAttrHints.prototype.shouldShowHintsOnKey = function (key) {
-        return (this.alphabet.indexOf(key) !== -1);
+//    CssAttrHints.prototype.shouldShowHintsOnKey = function (key) {
+//        return (this.alphabet.indexOf(key) !== -1);
         // return (key === "{" || key === ";"); /* only popup after brackets, else this will always trigger */
         // return (key === " " || key === "{" );
-    };
+//    };
     
     
                 
-    CssAttrHints.prototype._cursorInRange = function (cursor, start, end) {
+/*    CssAttrHints.prototype._cursorInRange = function (cursor, start, end) {
         var afterStart = false,
             beforeEnd  = false;
         
@@ -185,7 +254,7 @@ define(function (require, exports, module) {
         }
         
         return (afterStart && beforeEnd);
-    };
+    };*/
                 
                 
                 
