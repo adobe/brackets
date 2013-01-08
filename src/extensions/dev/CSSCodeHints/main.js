719e739784e85293a43419648d456d66202a6641
Andre Zoufahl <zoufahl@zib.de>
2013-01-09 00:38:07 +0100

    added defaultcomments to functions

--- src/extensions/dev/CSSCodeHints/main.js
+++ src/extensions/dev/CSSCodeHints/main.js
@@ -8,12 +8,32 @@ define(function (require, exports, module) {
         CSSUtils            = brackets.getModule("language/CSSUtils"),
         CSSAttributes       = require("text!CSSAttributes.json"),
         attributes          = JSON.parse(CSSAttributes);
-
+    
+    /**
+     * @constructor
+     */
     function CssAttrHints() {
         this.primaryTriggerKeys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-";
         this.secondaryTriggerKeys = " :;";
     }
-    
+
+    /**
+     * Determines whether HTML tag hints are available in the current editor
+     * context.
+     * 
+     * @param {Editor} editor 
+     * A non-null editor object for the active window.
+     *
+     * @param {String} implicitChar 
+     * Either null, if the hinting request was explicit, or a single character
+     * that represents the last insertion and that indicates an implicit
+     * hinting request.
+     *
+     * @return {Boolean} 
+     * Determines whether the current provider is able to provide hints for
+     * the given editor context and, in case implicitChar is non- null,
+     * whether it is appropriate to do so.
+     */
     CssAttrHints.prototype.hasHints = function (editor, implicitChar) {
         this.editor = editor;
         var cursor = this.editor.getCursorPos();
@@ -32,7 +52,20 @@ define(function (require, exports, module) {
         
         return false;
     };
-    
+       
+    /**
+     * Returns a list of availble HTML tag hints if possible for the current
+     * editor context. 
+     *
+     * @return {Object<hints: Array<(String + jQuery.Obj)>, match: String, 
+     *      selectInitial: Boolean>}
+     * Null if the provider wishes to end the hinting session. Otherwise, a
+     * response object that provides 1. a sorted array hints that consists 
+     * of strings; 2. a string match that is used by the manager to emphasize
+     * matching substrings when rendering the hint list; and 3. a boolean that
+     * indicates whether the first result, if one exists, should be selected
+     * by default in the hint list window.
+     */
     CssAttrHints.prototype.getHints = function (implicitChar) {
         this.info = CSSUtils.getInfoAtPos(this.editor, this.editor.getCursorPos());
 
@@ -69,8 +102,6 @@ define(function (require, exports, module) {
                     selectInitial: selectInitial
                 };
             }
-            
-            
         } else if (context === CSSUtils.PROP_NAME) {
             result = $.map(attributes, function (pvalues, pname) {
                 if (pname.indexOf(needle) === 0) {
@@ -83,12 +114,20 @@ define(function (require, exports, module) {
                 match: needle,
                 selectInitial: selectInitial
             };
-        
         }
-        
         return null;
     };
     
+    /**
+     * Inserts a given HTML tag hint into the current editor context. 
+     * 
+     * @param {String} hint 
+     * The hint to be inserted into the editor context.
+     * 
+     * @return {Boolean} 
+     * Indicates whether the manager should follow hint insertion with an
+     * additional explicit hint request.
+     */
     CssAttrHints.prototype.insertHint = function (hint) {
         var offset = this.info.offset,
             cursor = this.editor.getCursorPos(),
