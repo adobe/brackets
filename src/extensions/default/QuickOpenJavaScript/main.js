6cb2a7449f2d8fc5e9a6c9ed26e712c26b7e24c4
Kevin Dangoor <kid@blazingthings.com>
2013-01-15 12:04:49 -0500

    proof of concept simple caching for #2496.
    Notably, this also stops generating the function list on every search call.

--- src/extensions/default/QuickOpenJavaScript/main.js
+++ src/extensions/default/QuickOpenJavaScript/main.js
@@ -52,20 +52,13 @@ define(function (require, exports, module) {
         this.functionName = functionName;
     }
 
-    /**
-     * Contains a list of information about functions for a single document. This array is populated
-     * by createFunctionList()
-     * @type {?Array.<FileLocation>}
-     */
-    var functionList = null;
-
-
-    function done() {
-        functionList = null;
-    }
-
-    
     function createFunctionList() {
+        /**
+         * Contains a list of information about functions for a single document. This array is populated
+         * by createFunctionList()
+         * @type {?Array.<FileLocation>}
+         */
+        var functionList = null;
         var doc = DocumentManager.getCurrentDocument();
         if (!doc) {
             return;
@@ -80,7 +73,7 @@ define(function (require, exports, module) {
             var chTo = chFrom + funcEntry.name.length;
             functionList.push(new FileLocation(null, funcEntry.lineStart, chFrom, chTo, funcEntry.name));
         });
-
+        return functionList;
     }
 
     
@@ -89,13 +82,17 @@ define(function (require, exports, module) {
      * @param {string} query what the user is searching for
      * @returns {Array.<SearchResult>} sorted and filtered results that match the query
      */
-    function search(query) {
-        createFunctionList();
+    function search(query, matcher) {
+        var functionList = matcher.functionList;
+        if (!functionList) {
+            functionList = createFunctionList();
+            matcher.functionList = functionList;
+        }
         query = query.slice(query.indexOf("@") + 1, query.length);
         
         // Filter and rank how good each match is
         var filteredList = $.map(functionList, function (fileLocation) {
-            var searchResult = QuickOpen.stringMatch(fileLocation.functionName, query);
+            var searchResult = matcher.match(fileLocation.functionName, query);
             if (searchResult) {
                 searchResult.fileLocation = fileLocation;
             }
@@ -147,8 +144,8 @@ define(function (require, exports, module) {
         {
             name: "JavaScript functions",
             fileTypes: ["js"],
-            done: done,
             search: search,
+            done: function () {},
             match: match,
             itemFocus: itemFocus,
             itemSelect: itemSelect,
--- src/utils/StringMatch.js
+++ src/utils/StringMatch.js
@@ -594,7 +594,7 @@ define(function (require, exports, module) {
      * @param {string} query  The query string to find in string
      * @return {{ranges:Array.<{text:string, matched:boolean, includesLastSegment:boolean}>, matchGoodness:int, scoreDebug: Object}} matched ranges and score
      */
-    function stringMatch(str, query) {
+    function stringMatch(str, query, special) {
         var result;
 
         // No query? Short circuit the normal work done and just
@@ -615,7 +615,9 @@ define(function (require, exports, module) {
         
         // Locate the special characters and then use orderedCompare to do the real
         // work.
-        var special = findSpecialCharacters(str);
+        if (!special) {
+            special = findSpecialCharacters(str);
+        }
         var lastSegmentStart = special.specials[special.lastSegmentSpecialsIndex];
         var matchList = _wholeStringSearch(query, str, special.specials,
                               special.lastSegmentSpecialsIndex);
@@ -684,6 +686,36 @@ define(function (require, exports, module) {
         multiFieldSort(searchResults, { matchGoodness: 0, label: 1 });
     }
     
+    function StringMatcher() {
+        this.lastQuery = null;
+        this.specialsCache = {};
+        this.noMatchCache = {};
+    }
+    
+    StringMatcher.prototype.match = function (str, query) {
+        if (this.lastQuery !== null && (this.lastQuery !== query.substring(0, this.lastQuery.length))) {
+            this.noMatchCache = {};
+        }
+        
+        this.lastQuery = query;
+        
+        if (this.noMatchCache[str]) {
+            return undefined;
+        }
+        
+        var special = this.specialsCache[str];
+        if (special === undefined) {
+            special = findSpecialCharacters(str);
+            this.specialsCache[str] = special;
+        }
+        
+        var result = stringMatch(str, query, special);
+        if (!result) {
+            this.noMatchCache[str] = true;
+        }
+        return result;
+    };
+    
     exports._findSpecialCharacters  = findSpecialCharacters;
     exports._wholeStringSearch      = _wholeStringSearch;
     exports._lastSegmentSearch      = _lastSegmentSearch;
@@ -698,4 +730,5 @@ define(function (require, exports, module) {
     exports.stringMatch             = stringMatch;
     exports.basicMatchSort          = basicMatchSort;
     exports.multiFieldSort          = multiFieldSort;
+    exports.StringMatcher           = StringMatcher;
 });
