25462512046d78ea8be38a9be45483bf83a251ae
RaymondLim <rayblim@gmail.com>
2013-01-26 09:14:28 -0800

    Fixing multiple issues in CSS code hints extension (in getHints and insertHint APIs)

--- src/extensions/default/CSSCodeHints/main.js
+++ src/extensions/default/CSSCodeHints/main.js
@@ -4,8 +4,10 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var CodeHintManager     = brackets.getModule("editor/CodeHintManager"),
+    var AppInit             = brackets.getModule("utils/AppInit"),
+        CodeHintManager     = brackets.getModule("editor/CodeHintManager"),
         CSSUtils            = brackets.getModule("language/CSSUtils"),
+        TokenUtils          = brackets.getModule("utils/TokenUtils"),
         CSSProperties       = require("text!CSSProperties.json"),
         properties          = JSON.parse(CSSProperties);
     
@@ -88,25 +90,27 @@ define(function (require, exports, module) {
         if (context === CSSUtils.PROP_VALUE) {
             if (!properties[needle]) {
                 return null;
-            } else {
-                
-                if (this.info.values.length > 0) {
-                    valueNeedle = this.info.values[this.info.values.length - 1].trim();
-                }
-                
-                result = $.map(properties[needle].values, function (pvalue, pindex) {
-                    if (pvalue.indexOf(valueNeedle) === 0) {
-                        return pvalue;
-                    }
-                }).sort();
-                
-                return {
-                    hints: result,
-                    match: valueNeedle,
-                    selectInitial: selectInitial
-                };
             }
+            
+            // Cursor is in an existing property value or partially typed value
+            if (!this.info.isNewItem && this.info.index !== -1) {
+                valueNeedle = this.info.values[this.info.index].trim();
+                valueNeedle = valueNeedle.substr(0, this.info.offset);
+            }
+            
+            result = $.map(properties[needle].values, function (pvalue, pindex) {
+                if (pvalue.indexOf(valueNeedle) === 0) {
+                    return pvalue;
+                }
+            }).sort();
+            
+            return {
+                hints: result,
+                match: valueNeedle,
+                selectInitial: selectInitial
+            };
         } else if (context === CSSUtils.PROP_NAME) {
+            needle = needle.substr(0, this.info.offset);
             result = $.map(properties, function (pvalues, pname) {
                 if (pname.indexOf(needle) === 0) {
                     return pname;
@@ -135,32 +139,58 @@ define(function (require, exports, module) {
     CssPropHints.prototype.insertHint = function (hint) {
         var offset = this.info.offset,
             cursor = this.editor.getCursorPos(),
-            closure = "",
             start = {line: -1, ch: -1},
             end = {line: -1, ch: -1},
-            keepHints = true;
+            keepHints = false,
+            ctx;
         
-        if (this.info.context === CSSUtils.PROP_NAME) {
-            closure = ":";
-        } else if (this.info.context !== CSSUtils.PROP_VALUE) {
+        if (this.info.context !== CSSUtils.PROP_NAME && this.info.context !== CSSUtils.PROP_VALUE) {
             return false;
         }
         
-        hint = hint + closure;
-        
         start.line = end.line = cursor.line;
         start.ch = cursor.ch - offset;
-        end.ch = start.ch + hint.length;
+
+        if (this.info.context === CSSUtils.PROP_NAME) {
+            if (this.info.name.length === 0) {
+                // It's a new insertion, so append a colon and set keepHints
+                // to show property value hints.
+                hint += ":";
+                keepHints = true;
+                end.ch = start.ch;
+            } else {
+                // It's a replacement of an existing one or just typed in properety.
+                // So we need to check whether there is an existing colon following 
+                // the current property name. If a colon already exists, then we just
+                // replace the property name and won't adjust the cursor position. 
+                // And therefore we won't show code hints for property values either.
+                end.ch = start.ch + this.info.name.length;
+                ctx = TokenUtils.getInitialContext(this.editor._codeMirror, cursor);
+                if (!TokenUtils.moveSkippingWhitespace(TokenUtils.moveNextToken, ctx) ||
+                        ctx.token.string !== ":") {
+                    hint += ":";
+                    keepHints = true;
+                    end.ch += 1;    // Add one for the colon that we're appending.
+                }
+            }
+        } else if (!this.info.isNewItem && this.info.index !== -1) {
+            // Replacing an existing property value or partially typed value
+            end.ch = start.ch + this.info.values[this.info.index].length;
+        } else {
+            // Inserting a new property value
+            end.ch = start.ch;
+        }
         
         this.editor.document.replaceRange(hint, start, end);
         
         return keepHints;
     };
     
-    var cssPropHints = new CssPropHints();
-    CodeHintManager.registerHintProvider(cssPropHints, ["css"], 0);
-    
-    // For unit testing
-    exports.cssPropHintProvider = cssPropHints;
-    
+    AppInit.appReady(function () {
+        var cssPropHints = new CssPropHints();
+        CodeHintManager.registerHintProvider(cssPropHints, ["css"], 0);
+        
+        // For unit testing
+        exports.cssPropHintProvider = cssPropHints;
+    });
 });
