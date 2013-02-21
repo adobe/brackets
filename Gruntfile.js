2f959d0f51779997dd1023569d29a69da490058a
Jason San Jose <jasonsj@adobe.com>
2013-02-21 11:57:54 -0800

    missing semicolon

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function (grunt) {
 
     // task: set-sprint
     // Update sprint number in package.json and rewrite src/config.json
-    grunt.renameTask('set-sprint', 'set-sprint-alias')
+    grunt.renameTask('set-sprint', 'set-sprint-alias');
     grunt.registerTask('set-sprint', ['set-sprint-alias', 'write-config']);
 
     // Default task.
