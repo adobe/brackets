8ea8deb7ad9d5bb5b44e3f522cec53510f6e01a3
Albert Xing <albert.xing@alumni.ubc.ca>
2013-04-08 00:07:40 -0700

    Implement recommendations
    Align `var` declarations, change from `.CodeMirror` to `#editor-holder`,
    implement code portability.

--- src/extensions/default/JSLint/main.js
+++ src/extensions/default/JSLint/main.js
@@ -186,6 +186,8 @@ define(function (require, exports, module) {
             setGotoEnabled(false);
         }
         
+        EditorManager.resizeEditor();
+
         // Set default action to false
         var mustShow = false;
 
@@ -196,21 +198,21 @@ define(function (require, exports, module) {
             // Gather info to determine whether to scroll after editor resizes
             var scrollInfo = editor._codeMirror.getScrollInfo(),
                 currScroll = scrollInfo.top,
-                height = scrollInfo.clientHeight,
+                height     = scrollInfo.clientHeight,
                 textHeight = editor.getTextHeight(),
-                cursorTop = editor._codeMirror.cursorCoords().top;
+                cursorTop  = editor._codeMirror.cursorCoords().top;
 
-            var bottom = cursorTop - $(".CodeMirror").offset().top + textHeight - height;
+            var bottom = cursorTop - $("#editor-holder").offset().top + textHeight - height;
+
+            console.log(bottom);
 
             // Determine whether panel would block text at cursor
             // If so, set variable to determine action after
             // editor is resized
-            if (bottom >= -$("#jslint-results").height() && bottom <= 5) {
+            if (bottom <= $("#jslint-results").height() && bottom >= 5) {
                 mustShow = true;
             }
         }
-
-        EditorManager.resizeEditor();
 
         // Scroll cursor back into view only if cursor is hidden by panel
         if (mustShow && _gotoEnabled) {
