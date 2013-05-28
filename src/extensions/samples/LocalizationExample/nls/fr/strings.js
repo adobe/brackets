d2a9fb657755f0392fab41419bd727a14cd1739e
Florian M Valence <fantlo@gmail.com>
2013-05-28 15:32:41 +0300

    Translate in French

--- src/extensions/samples/LocalizationExample/nls/fr/strings.js
+++ src/extensions/samples/LocalizationExample/nls/fr/strings.js
@@ -26,11 +26,11 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 
-// This example currently displays English, but shows where French translations would go.
+
 define({
-    "COMMAND_NAME"      : "French: My New Command",
-    "ALERT_MESSAGE"     : "French: This is a sample alert message",
-    "DIALOG_TITLE"      : "French: Localized Dialog Example",
-    "DIALOG_TEXT"       : "French: This is an example of localized text in Brackets",
+    "COMMAND_NAME"      : "Ma nouvelle commande",
+    "ALERT_MESSAGE"     : "Ceci est une alerte d'exemple",
+    "DIALOG_TITLE"      : "Exemple de boite de dialogue traduite",
+    "DIALOG_TEXT"       : "Ceci est un exemple de texte traduit dans Brackets",
     "DIALOG_OK"         : "OK"
 });
