1dd2474fd8d514b033d90be5b59c04b31d8aa30f
Jochen Hagenstroem <jhagenst@adobe.com>
2013-06-13 15:36:23 -0700

    removing jslint regexp setting, as we moved to pathutils instead of using regex

--- src/document/DocumentCommandHandlers.js
+++ src/document/DocumentCommandHandlers.js
@@ -22,7 +22,7 @@
  */
 
 
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, regexp: true */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, PathUtils, window */
 
 define(function (require, exports, module) {
