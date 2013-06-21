d6b53c410f020a3eb6cd769463388084f9243e4b
Jeff Booher <jsbooher@adobe.com>
2013-06-20 23:33:14 -0700

    undo brackets-concat perf test changes

--- test/perf/OpenFile-perf-files/brackets-concat.js
+++ test/perf/OpenFile-perf-files/brackets-concat.js
@@ -20084,7 +20084,7 @@ define('utils/NativeApp',['require','exports','module','utils/Async'],function (
      * Opens a URL in the system default browser
      */
     function openURLInDefaultBrowser(url) {
-        brackets.app.openURLInDefaultBrowser(url);
+        brackets.app.openURLInDefaultBrowser(function (err) {}, url);
     }
     
 
