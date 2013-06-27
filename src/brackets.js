a29b21061fecbc084d9a54fd12210dee6f854e5f
Glenn Ruehle <gruehle@gmail.com>
2013-06-27 16:55:10 -0700

    Re-enable HTML menus.

--- src/brackets.js
+++ src/brackets.js
@@ -271,7 +271,7 @@ define(function (require, exports, module) {
         }
 
         // Enable/Disable HTML Menus
-        if (brackets.platform !== "linux") {
+        if (brackets.platform !== "linux" && !brackets.inBrowser) {
             $("body").addClass("has-appshell-menus");
         }
         
