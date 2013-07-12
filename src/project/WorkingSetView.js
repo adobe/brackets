879949b1abda6174358507df0dde451304e21ca9
Bryan Chin <bchin@adobe.com>
2013-07-12 18:07:54 -0500

    per pull request review, conditionalizing more of the list removal code so that the working set list isn't changed at all

--- src/project/WorkingSetView.js
+++ src/project/WorkingSetView.js
@@ -514,20 +514,20 @@ define(function (require, exports, module) {
      * @param {FileEntry, boolean=} file, flag to suppress redraw if true
      */
     function _handleFileRemoved(file, suppressRedraw) {
-        var $listItem = _findListItemFromFile(file);
-        if ($listItem) {
-            // Make the next file in the list show the close icon, 
-            // without having to move the mouse, if there is a next file.
-            var $nextListItem = $listItem.next();
-            if ($nextListItem && $nextListItem.length > 0) {
-                var canClose = ($listItem.find(".can-close").length === 1);
-                var isDirty = isOpenAndDirty($nextListItem.data(_FILE_KEY));
-                _updateFileStatusIcon($nextListItem, isDirty, canClose);
-            }
-            $listItem.remove();
-        }
-        
         if (!suppressRedraw) {
+            var $listItem = _findListItemFromFile(file);
+            if ($listItem) {
+                // Make the next file in the list show the close icon, 
+                // without having to move the mouse, if there is a next file.
+                var $nextListItem = $listItem.next();
+                if ($nextListItem && $nextListItem.length > 0) {
+                    var canClose = ($listItem.find(".can-close").length === 1);
+                    var isDirty = isOpenAndDirty($nextListItem.data(_FILE_KEY));
+                    _updateFileStatusIcon($nextListItem, isDirty, canClose);
+                }
+                $listItem.remove();
+            }
+            
             _redraw();
         }
     }
