a4b7d46e87da5f9bf888eaacd78dbffcbb7b68cd
Maksim Lin <maks@manichord.com>
2013-07-16 09:03:47 +1000

    fix jshint error and add license header

--- src/xorigin.js
+++ src/xorigin.js
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global brackets: true */
 (function () {
@@ -19,9 +42,9 @@
     }
     
     // Abort if running in the shell, running on a server or not running in a supported and affected browser
-    if (typeof (brackets) !== "undefined"
-            || document.location.href.substr(0, 7) !== "file://"
-            || !testCrossOriginError) {
+    if (typeof (brackets) !== "undefined" || 
+            document.location.href.substr(0, 7) !== "file://" ||
+            !testCrossOriginError) {
         return;
     }
     
