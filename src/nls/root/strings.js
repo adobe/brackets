5ee7dea735993597e72008ad78362157510c2cad
Peter Flynn <pflynn@adobe.com>
2013-07-18 15:43:42 -0700

    Make folder tree refresh menu item less ambiguous (could be interpreted as refreshing the single file that was right-clicked).

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -189,7 +189,7 @@ define({
     "CMD_FILE_DELETE"                     : "Delete",
     "CMD_INSTALL_EXTENSION"               : "Install Extension\u2026",
     "CMD_EXTENSION_MANAGER"               : "Extension Manager\u2026",
-    "CMD_FILE_REFRESH"                    : "Refresh",
+    "CMD_FILE_REFRESH"                    : "Refresh File Tree",
     "CMD_QUIT"                            : "Quit",
     // Used in native File menu on Windows
     "CMD_EXIT"                            : "Exit",
