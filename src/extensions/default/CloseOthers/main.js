2e360bc1058e74ee3da5a6ace6381b3e6910fcca
sathyamoorthi <sathyamoorthi10@gmail.com>
2013-07-18 13:14:15 +0530

    Coding standard update based initial review.

--- src/extensions/default/CloseOthers/main.js
+++ src/extensions/default/CloseOthers/main.js
@@ -6,66 +6,49 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var Menus = brackets.getModule("command/Menus");
-    var CommandManager = brackets.getModule("command/CommandManager");
-    var Commands = brackets.getModule("command/Commands");
-    var working_set_cmenu = Menus.getContextMenu(Menus.ContextMenuIds.WORKING_SET_MENU);
-    var dm = brackets.getModule("document/DocumentManager");
-    var settings = JSON.parse(require("text!settings.json"));
+    var Menus             = brackets.getModule("command/Menus"),
+        CommandManager    = brackets.getModule("command/CommandManager"),
+        Commands          = brackets.getModule("command/Commands"),
+        dm                = brackets.getModule("document/DocumentManager"),
+        settings          = JSON.parse(require("text!settings.json")),
+        working_set_cmenu = Menus.getContextMenu(Menus.ContextMenuIds.WORKING_SET_MENU);
 
     function handleClose(mode) {
 
-        var currDmFullPath = dm.getCurrentDocument().file.fullPath;
-        var workingSet = dm.getWorkingSet().slice(0), flag = false;
+        var targetIndex = dm.findInWorkingSet(dm.getCurrentDocument().file.fullPath),
+            fileEntry   = dm.getWorkingSet().slice(0);
 
         CommandManager.execute(Commands.FILE_CLOSE_ALL, {promptOnly: true}).done(function () {
             
+            var start = (mode === "close_below") ? (targetIndex + 1) : 0,
+                end   = (mode === "close_above") ? (targetIndex) : (fileEntry.length),
+                i;
+
             if (mode === "close_others") {
+                fileEntry.splice(targetIndex, 1);
+            }
 
-                workingSet.forEach(function (value) {
-                    if (value.fullPath !== currDmFullPath) {
-                        dm.removeFromWorkingSet(value);
-                    }
-                });
-
-            } else if (mode === "close_below") {
-
-                workingSet.forEach(function (value) {
-                    if (flag) {
-                        dm.removeFromWorkingSet(value);
-                    } else if (value.fullPath === currDmFullPath) {
-                        flag = true;
-                    }
-                });
-
-            } else if (mode === "close_above") {
-
-                workingSet.forEach(function (value) {
-                    if (value.fullPath === currDmFullPath) {
-                        flag = true;
-                    } else if (!flag) {
-                        dm.removeFromWorkingSet(value);
-                    }
-                });
+            for (i = start; i < end; i++) {
+                dm.removeFromWorkingSet(fileEntry[i]);
             }
         });
     }
 
-    if (settings.close_below === "true") {
+    if (settings.close_below) {
         CommandManager.register("Close Others Below", "file.close_below", function () {
             handleClose("close_below");
         });
         working_set_cmenu.addMenuItem("file.close_below", "", Menus.AFTER, Commands.FILE_CLOSE);
     }
 
-    if (settings.close_others === "true") {
+    if (settings.close_others) {
         CommandManager.register("Close Others", "file.close_others", function () {
             handleClose("close_others");
         });
         working_set_cmenu.addMenuItem("file.close_others", "", Menus.AFTER, Commands.FILE_CLOSE);
     }
 
-    if (settings.close_above === "true") {
+    if (settings.close_above) {
         CommandManager.register("Close Others Above", "file.close_above", function () {
             handleClose("close_above");
         });
