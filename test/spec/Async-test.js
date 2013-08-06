9da2c7c5f0cafc824040ebf06a6c8654dc45eaed
Ian Wehrman <wehrman@adobe.com>
2013-08-06 11:07:27 -0700

    Restructure Async tests

--- test/spec/Async-test.js
+++ test/spec/Async-test.js
@@ -27,7 +27,8 @@
 define(function (require, exports, module) {
     "use strict";
     
-    var Async = require("utils/Async");
+    var Async = require("utils/Async"),
+        PromiseQueue = Async.PromiseQueue;
     
     describe("Async", function () {
         
@@ -268,144 +269,141 @@ define(function (require, exports, module) {
             });
                         
         });
-        
-    });
     
-    var PromiseQueue = require("utils/Async").PromiseQueue;
+        describe("Async PromiseQueue", function () {
+            var queue, calledFns;
+            
+            beforeEach(function () {
+                queue = new PromiseQueue();
+                calledFns = {};
+            });
+            
+            function makeFn(id, resolveNow) {
+                var result = new $.Deferred();
+                return {
+                    fn: function () {
+                        calledFns[id] = true;
+                        if (resolveNow) {
+                            result.resolve();
+                        }
+                        return result.promise();
+                    },
+                    deferred: result
+                };
+            }
+            
+            it("should immediately execute a function when the queue is empty", function () {
+                var fnInfo = makeFn("one");
+                
+                queue.add(fnInfo.fn);
+                expect(calledFns.one).toBe(true);
+                
+                fnInfo.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+            });
+            
+            it("should wait to execute the second added function", function () {
+                var fnInfo1 = makeFn("one"),
+                    fnInfo2 = makeFn("two");
+                
+                queue.add(fnInfo1.fn);
+                expect(calledFns.one).toBe(true);
+                
+                queue.add(fnInfo2.fn);
+                expect(calledFns.two).toBeUndefined();
+                
+                fnInfo1.deferred.resolve();
+                expect(calledFns.two).toBe(true);
+                
+                fnInfo2.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+            });
+            
+            it("should properly handle the case where the first function completes synchronously", function () {
+                var fnInfo1 = makeFn("one", true),
+                    fnInfo2 = makeFn("two");
+                
+                queue.add(fnInfo1.fn);
+                expect(calledFns.one).toBe(true);
+                
+                queue.add(fnInfo2.fn);
+                expect(calledFns.two).toBe(true);
+                
+                fnInfo2.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+            });
+            
+            it("should sequentially execute a second and third function if they're both added while the first is executing", function () {
+                var fnInfo1 = makeFn("one"),
+                    fnInfo2 = makeFn("two"),
+                    fnInfo3 = makeFn("three");
+                
+                queue.add(fnInfo1.fn);
+                expect(calledFns.one).toBe(true);
+                
+                queue.add(fnInfo2.fn);
+                queue.add(fnInfo3.fn);
+                expect(calledFns.two).toBeUndefined();
+                expect(calledFns.three).toBeUndefined();
+                
+                fnInfo1.deferred.resolve();
+                expect(calledFns.two).toBe(true);
+                expect(calledFns.three).toBeUndefined();
+                
+                fnInfo2.deferred.resolve();
+                expect(calledFns.three).toBe(true);
+                
+                fnInfo3.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+            });
     
-    describe("Async PromiseQueue", function () {
-        var queue, calledFns;
-        
-        beforeEach(function () {
-            queue = new PromiseQueue();
-            calledFns = {};
-        });
-        
-        function makeFn(id, resolveNow) {
-            var result = new $.Deferred();
-            return {
-                fn: function () {
-                    calledFns[id] = true;
-                    if (resolveNow) {
-                        result.resolve();
-                    }
-                    return result.promise();
-                },
-                deferred: result
-            };
-        }
-        
-        it("should immediately execute a function when the queue is empty", function () {
-            var fnInfo = makeFn("one");
+            it("should sequentially execute a second and third function if the third is added while the second is executing", function () {
+                var fnInfo1 = makeFn("one"),
+                    fnInfo2 = makeFn("two"),
+                    fnInfo3 = makeFn("three");
+                
+                queue.add(fnInfo1.fn);
+                expect(calledFns.one).toBe(true);
+                
+                queue.add(fnInfo2.fn);
+                expect(calledFns.two).toBeUndefined();
+    
+                fnInfo1.deferred.resolve();
+                expect(calledFns.two).toBe(true);
+    
+                queue.add(fnInfo3.fn);
+                expect(calledFns.three).toBeUndefined();
+                
+                fnInfo2.deferred.resolve();
+                expect(calledFns.three).toBe(true);
+                
+                fnInfo3.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+            });
             
-            queue.add(fnInfo.fn);
-            expect(calledFns.one).toBe(true);
-            
-            fnInfo.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
-        });
-        
-        it("should wait to execute the second added function", function () {
-            var fnInfo1 = makeFn("one"),
-                fnInfo2 = makeFn("two");
-            
-            queue.add(fnInfo1.fn);
-            expect(calledFns.one).toBe(true);
-            
-            queue.add(fnInfo2.fn);
-            expect(calledFns.two).toBeUndefined();
-            
-            fnInfo1.deferred.resolve();
-            expect(calledFns.two).toBe(true);
-            
-            fnInfo2.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
-        });
-        
-        it("should properly handle the case where the first function completes synchronously", function () {
-            var fnInfo1 = makeFn("one", true),
-                fnInfo2 = makeFn("two");
-            
-            queue.add(fnInfo1.fn);
-            expect(calledFns.one).toBe(true);
-            
-            queue.add(fnInfo2.fn);
-            expect(calledFns.two).toBe(true);
-            
-            fnInfo2.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
-        });
-        
-        it("should sequentially execute a second and third function if they're both added while the first is executing", function () {
-            var fnInfo1 = makeFn("one"),
-                fnInfo2 = makeFn("two"),
-                fnInfo3 = makeFn("three");
-            
-            queue.add(fnInfo1.fn);
-            expect(calledFns.one).toBe(true);
-            
-            queue.add(fnInfo2.fn);
-            queue.add(fnInfo3.fn);
-            expect(calledFns.two).toBeUndefined();
-            expect(calledFns.three).toBeUndefined();
-            
-            fnInfo1.deferred.resolve();
-            expect(calledFns.two).toBe(true);
-            expect(calledFns.three).toBeUndefined();
-            
-            fnInfo2.deferred.resolve();
-            expect(calledFns.three).toBe(true);
-            
-            fnInfo3.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
-        });
-
-        it("should sequentially execute a second and third function if the third is added while the second is executing", function () {
-            var fnInfo1 = makeFn("one"),
-                fnInfo2 = makeFn("two"),
-                fnInfo3 = makeFn("three");
-            
-            queue.add(fnInfo1.fn);
-            expect(calledFns.one).toBe(true);
-            
-            queue.add(fnInfo2.fn);
-            expect(calledFns.two).toBeUndefined();
-
-            fnInfo1.deferred.resolve();
-            expect(calledFns.two).toBe(true);
-
-            queue.add(fnInfo3.fn);
-            expect(calledFns.three).toBeUndefined();
-            
-            fnInfo2.deferred.resolve();
-            expect(calledFns.three).toBe(true);
-            
-            fnInfo3.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
-        });
-        
-        it("should execute a second function when added to the empty queue after the first function has completed", function () {
-            var fnInfo1 = makeFn("one"),
-                fnInfo2 = makeFn("two");
-            
-            queue.add(fnInfo1.fn);
-            expect(calledFns.one).toBe(true);
-            
-            fnInfo1.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
-            
-            queue.add(fnInfo2.fn);
-            expect(calledFns.two).toBe(true);
-
-            fnInfo2.deferred.resolve();
-            expect(queue._queue.length).toBe(0);
-            expect(queue._curPromise).toBe(null);
+            it("should execute a second function when added to the empty queue after the first function has completed", function () {
+                var fnInfo1 = makeFn("one"),
+                    fnInfo2 = makeFn("two");
+                
+                queue.add(fnInfo1.fn);
+                expect(calledFns.one).toBe(true);
+                
+                fnInfo1.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+                
+                queue.add(fnInfo2.fn);
+                expect(calledFns.two).toBe(true);
+    
+                fnInfo2.deferred.resolve();
+                expect(queue._queue.length).toBe(0);
+                expect(queue._curPromise).toBe(null);
+            });
         });
     });
 });
