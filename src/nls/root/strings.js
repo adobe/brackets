79cc6e845a61b54fa4ea2156b80a134e0b85bced
Lawrence Hsu <larzshinobi@gmail.com>
2013-08-08 12:58:33 -0700

    Find in Files search results UI tweak.

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -136,8 +136,8 @@ define({
     "FIND_IN_FILES_PAGING"              : "{0}&mdash;{1}",
     "FIND_IN_FILES_LESS"                : " <a href='#' class='find-less'>Less</a>",
     "FIND_IN_FILES_MORE"                : " <a href='#' class='find-more'>More</a>",
-    "FIND_IN_FILES_FILE_PATH"           : "File: <span class='dialog-filename'>{0}</span>",
-    "FIND_IN_FILES_LINE"                : "line: {0}",
+    "FIND_IN_FILES_FILE_PATH"           : "<span class='dialog-filename'>{0}</span>",
+    "FIND_IN_FILES_LINE"                : "{0}",
 
     "ERROR_FETCHING_UPDATE_INFO_TITLE"  : "Error getting update info",
     "ERROR_FETCHING_UPDATE_INFO_MSG"    : "There was a problem getting the latest update information from the server. Please make sure you are connected to the internet and try again.",
