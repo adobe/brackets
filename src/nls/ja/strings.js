b9ca32f5adfbd319d5e7f1d660564a1fd0b7363d
wALF Utility <walf@adobe.com>
2013-08-26 20:26:45 -0700

    Updated by ALF automation.

--- src/nls/ja/strings.js
+++ src/nls/ja/strings.js
@@ -117,7 +117,7 @@ define({
 	"WITH": "置換後の文字列",
 	"BUTTON_YES": "はい",
 	"BUTTON_NO": "いいえ",
-	"BUTTON_ALL": "すべて",
+	"BUTTON_REPLACE_ALL": "!能=[7126959] All\u2026_=!",
 	"BUTTON_STOP": "終了",
 	"BUTTON_REPLACE": "置換",
 
