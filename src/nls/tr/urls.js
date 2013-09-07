d05eb5ec99170fe2b9c91d5dac072b9237d7b42e
Muhammet Ali Bekiroglu <s49132@beuth-hochschule.de>
2013-09-07 16:59:57 +0200

    Added Locates to nls/tr/strings.js

--- src/nls/tr/urls.js
+++ src/nls/tr/urls.js
@@ -26,6 +26,7 @@
 
 define({
     // Relative to the samples folder
+    "GETTING_STARTED"           : "tr/Ilk basamaklar",
     "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty_tr/",
     "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.tr"
 });
