df2c99ddd65f5bfa26c67391328cc2058e483f13
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2013-09-12 13:40:54 +0430

    Fix translation to Persian

--- src/nls/fa-ir/strings.js
+++ src/nls/fa-ir/strings.js
@@ -1,15 +1,37 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
 
-//********************* Translated by: Mohammad.Yaghobi
-//********************* info@pgrammer.com --- m.yaghobi.abc@gmail.com
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
 
 define({
     
-    /**
+    /*
      * Errors
      */
 
     // General file io error strings
-	"APP_NAME"		                    : "براکتس",
+    "APP_NAME"                          : "براکتس",
     "GENERIC_ERROR"                     : "(خطا {0})",
     "NOT_FOUND_ERR"                     : "پرونده پیدا نشد.",
     "NOT_READABLE_ERR"                  : "فایل قابل خواندن نیست.",
@@ -411,7 +433,7 @@ define({
     "LOCALE_IT"                                 : "Italian",
     "LOCALE_JA"                                 : "Japanese",
     "LOCALE_NB"                                 : "Norwegian",
-	"LOCALE_fa_ir"                              : "Persian-پارسی",
+    "LOCALE_fa_ir"                              : "Persian-پارسی",
     "LOCALE_PL"                                 : "Polish",
     "LOCALE_PT_BR"                              : "Portuguese, Brazil",
     "LOCALE_PT_PT"                              : "Portuguese",
