fe3b1d084d70955c85b0fdf8853c3f7f52eb5270
Jason San Jose <jasonsj@adobe.com>
2013-09-12 20:06:07 -0700

    replace node fs-extra remove() with remoteTempDirectory()

--- test/spec/ExtensionInstallation-test.js
+++ test/spec/ExtensionInstallation-test.js
@@ -132,7 +132,7 @@ define(function (require, exports, module) {
         afterEach(function () {
             ExtensionLoader.getUserExtensionPath = realGetUserExtensionPath;
             ExtensionLoader.loadExtension = realLoadExtension;
-            var promise = SpecRunnerUtils.remove(mockGetUserExtensionPath());
+            var promise = SpecRunnerUtils.removeTempDirectory();
             waitsForDone(promise, "Mock Extension Removal", 2000);
         });
         
