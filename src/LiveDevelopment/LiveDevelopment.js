93c5a38ebb612cb90219ec90762a3ba11b99c4be
Ingo Richter <ingo.richter@gmail.com>
2013-10-03 12:07:53 -0700

    Implementation for this story https://trello.com/c/gbBtpARq/709-1-define-default-html-file-for-live-development
    Implemented fix for https://github.com/adobe/brackets/issues/2033
    
    Added a lot of test cases to cover the new functionality.

--- src/LiveDevelopment/LiveDevelopment.js
+++ src/LiveDevelopment/LiveDevelopment.js
@@ -75,6 +75,8 @@ define(function LiveDevelopment(require, exports, module) {
     var STATUS_SYNC_ERROR     = exports.STATUS_SYNC_ERROR     =  5;
 
     var Async                = require("utils/Async"),
+        CollectionUtils      = require("utils/CollectionUtils"),
+        FileIndexManager     = require("project/FileIndexManager"),
         Dialogs              = require("widgets/Dialogs"),
         DefaultDialogs       = require("widgets/DefaultDialogs"),
         DocumentManager      = require("document/DocumentManager"),
@@ -566,7 +568,7 @@ define(function LiveDevelopment(require, exports, module) {
             // After (1) the interstitial page loads, (2) then browser navigation
             // to the base URL is completed, and (3) the agents finish loading
             // gather related documents and finally set status to STATUS_ACTIVE.
-            var doc = _getCurrentDocument();
+            var doc = _getCurrentDocument();  // TODO: probably wrong...
 
             if (doc) {
                 var status = STATUS_ACTIVE,
@@ -607,6 +609,115 @@ define(function LiveDevelopment(require, exports, module) {
 
         // resolve/reject the open() promise after agents complete
         result.then(_openDeferred.resolve, _openDeferred.reject);
+
+        return result.promise();
+    }
+
+    /**
+     * @private
+     * Determine an index file that can be used to start Live Development.
+     * This function will inspect all files in a project to find the closest index file
+     * available for currently opened document. We are searching for these files:
+     *  - index.html
+     *  - index.htm
+     * 
+     * If the project is configured with a custom base url for live developmment, then
+     * the list of possible index files is extended to contain these index files too:
+     *  - index.php
+     *  - index.php3
+     *  - index.php4
+     *  - index.php5
+     *  - index.phtm
+     *  - index.phtml
+     *  - index.cfm
+     *  - index.cfml
+     *  - index.asp
+     *  - index.aspx
+     *  - index.jsp
+     *  - index.jspx
+     *  - index.shm
+     *  - index.shml
+     * 
+     * If a file was found, the promise will be resolved with the full path to this file. If no file
+     * was found in the whole project tree, the promise will be resolved with null.
+     * 
+     * @return {jQuery.Promise} A promise that is resolved with a full path
+     * to a file if one could been determined, or null if there was no suitable index
+     * file.
+     */
+    function _getInitialDocFromCurrent() {
+        var doc = _getCurrentDocument(),
+            refPath,
+            i;
+
+        // TODO: FileUtils.getParentFolder()
+        function getParentFolder(path) {
+            return path.substring(0, path.lastIndexOf('/', path.length - 2) + 1);
+        }
+
+        // Is the currently opened document already a file we can use for Live Development?
+        if (doc) {
+            refPath = doc.file.fullPath;
+            if (FileUtils.isStaticHtmlFileExt(refPath) || FileUtils.isServerHtmlFileExt(refPath)) {
+                return new $.Deferred().resolve(doc);
+            }
+        }
+
+        var result = new $.Deferred();
+
+        var baseUrl = ProjectManager.getBaseUrl(),
+            hasOwnServerForLiveDevelopment = (baseUrl && baseUrl.length);
+
+        // TODO: It would be better to reuse the list of file extensions from FileUtils
+        var staticIndexFiles = ["html", "htm"].map(function (extension) { return "index." + extension; });
+        var dynamicIndexFiles = ["php", "php3", "php4", "php5", "phtm", "phtml", "cfm", "cfml", "asp", "aspx", "jsp", "jspx", "shtm", "shtml"].map(function (extension) { return "index." + extension; });
+
+        FileIndexManager.getFileInfoList("all").done(function (allFiles) {
+            if (refPath) {
+                var projectRoot = ProjectManager.getProjectRoot().fullPath,
+                    containingFolder = FileUtils.getDirectoryPath(refPath),
+                    indexFileFound = false,
+                    stillInProjectTree = true;
+
+                var filterIndexFile = function (fileInfo) {
+                    if (fileInfo.fullPath.indexOf(containingFolder) === 0) {
+                        if (hasOwnServerForLiveDevelopment) {
+                            if ((dynamicIndexFiles.indexOf(fileInfo.name) !== -1) ||
+                                    (staticIndexFiles.indexOf(fileInfo.name) !== -1)) {
+                                return true;
+                            }
+                        } else if (staticIndexFiles.indexOf(fileInfo.name) !== -1) {
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
+                };
+
+                while (!indexFileFound && stillInProjectTree) {
+                    i = CollectionUtils.indexOf(allFiles, filterIndexFile);
+
+                    // We found no good match
+                    if (i === -1) {
+                        // traverse the directory tree up one level
+                        containingFolder = getParentFolder(containingFolder);
+                        // Are we still inside the project?
+                        if (containingFolder.indexOf(projectRoot) === -1) {
+                            stillInProjectTree = false;
+                        }
+                    } else {
+                        indexFileFound = true;
+                    }
+                }
+
+                if (i !== -1) {
+                    DocumentManager.getDocumentForPath(allFiles[i].fullPath).then(result.resolve, result.resolve);
+                    return;
+                }
+            }
+
+            result.resolve(null);
+        });
 
         return result.promise();
     }
@@ -819,17 +930,18 @@ define(function LiveDevelopment(require, exports, module) {
             // parallel.
             loadAgents();
 
-            var doc = _getCurrentDocument();
-            if (doc) {
-                // Navigate from interstitial to the document
-                // Fires a frameNavigated event
-                Inspector.Page.navigate(doc.url);
-            } else {
-                // Unlikely that we would get to this state where
-                // a connection is in process but there is no current
-                // document
-                close();
-            }
+            _getInitialDocFromCurrent().done(function (doc) {
+                if (doc) {
+                    // Navigate from interstitial to the document
+                    // Fires a frameNavigated event
+                    Inspector.Page.navigate(doc.url);
+                } else {
+                    // Unlikely that we would get to this state where
+                    // a connection is in process but there is no current
+                    // document
+                    close();
+                }
+            });
         });
     }
     
@@ -974,12 +1086,13 @@ define(function LiveDevelopment(require, exports, module) {
     
     // helper function that actually does the launch once we are sure we have
     // a doc and the server for that doc is up and running.
-    function _doLaunchAfterServerReady(doc) {
+    function _doLaunchAfterServerReady(initialDoc) {
         // update status
         _setStatus(STATUS_CONNECTING);
         
         // create live document
-        _liveDocument = _createDocument(doc, EditorManager.getCurrentFullEditor());
+        initialDoc._ensureMasterEditor();
+        _liveDocument = _createDocument(initialDoc, initialDoc._masterEditor);
 
         // start listening for requests
         _server.add(_liveDocument);
@@ -1037,18 +1150,23 @@ define(function LiveDevelopment(require, exports, module) {
     /** Open the Connection and go live */
     function open() {
         _openDeferred = new $.Deferred();
-
-        var doc = _getCurrentDocument(),
-            prepareServerPromise = (doc && _prepareServer(doc)) || new $.Deferred().reject();
         
-        // wait for server (StaticServer, Base URL or file:)
-        prepareServerPromise.done(function (result) {
-            _doLaunchAfterServerReady(doc);
-        });
-      
-        prepareServerPromise.fail(function () {
-            _showWrongDocError();
-            _openDeferred.reject();
+        // TODO: need to run _onDocumentChange() after load if doc != currentDocument here? Maybe not, since activeEditorChange
+        // doesn't trigger it, while inline editors can still cause edits in doc other than currentDoc...
+        _getInitialDocFromCurrent().done(function (doc) {
+            var prepareServerPromise = (doc && _prepareServer(doc)) || new $.Deferred().reject();
+
+            DocumentManager.addToWorkingSet(doc.file);
+
+            // wait for server (StaticServer, Base URL or file:)
+            prepareServerPromise
+                .done(function () {
+                    _doLaunchAfterServerReady(doc);
+                })
+                .fail(function () {
+                    _showWrongDocError();
+                    _openDeferred.reject();
+                });
         });
 
         return _openDeferred.promise();
@@ -1181,8 +1299,9 @@ define(function LiveDevelopment(require, exports, module) {
     }
 
     // For unit testing
-    exports.launcherUrl         = launcherUrl;
-    exports._getServer          = _getServer;
+    exports.launcherUrl               = launcherUrl;
+    exports._getServer                = _getServer;
+    exports._getInitialDocFromCurrent = _getInitialDocFromCurrent;
 
     // Export public functions
     exports.agents              = agents;
