d8ff6b3b40efa7c4db5f9b66a4718f2ae262f9ad
Kevin Dangoor <kid@blazingthings.com>
2013-10-08 11:21:03 -0400

    Added an integration test for project preferences.

--- test/UnitTestSuite.js
+++ test/UnitTestSuite.js
@@ -60,7 +60,7 @@ define(function (require, exports, module) {
     require("spec/NativeMenu-test");
     require("spec/NodeConnection-test");
     require("spec/PreferencesBase-test");
-//    require("spec/PreferencesManager-test");
+    require("spec/PreferencesManager-test");
     require("spec/ProjectManager-test");
     require("spec/QuickOpen-test");
     require("spec/RemoteFunctions-test");
