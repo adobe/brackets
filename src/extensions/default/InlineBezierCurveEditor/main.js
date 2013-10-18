6edb92fec5c5772adbb6b0b10b61a4e5aec76d14
Marcel Gerber <mg.hain@gmx.de>
2013-10-18 15:10:00 +0200

    Fix JSLint error

--- src/extensions/default/InlineBezierCurveEditor/main.js
+++ src/extensions/default/InlineBezierCurveEditor/main.js
@@ -41,7 +41,7 @@
  */
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true,  regexp: true, indent: 4, maxerr: 50 */
-/*global define, brackets, $, window */
+/*global define, brackets, $, window, Mustache */
 
 define(function (require, exports, module) {
     "use strict";
