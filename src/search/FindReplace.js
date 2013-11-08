3d3a3d84c23cc71b63e1c3472426b53028907388
Marcel Gerber <SAPlayer@users.noreply.github.com>
2013-11-08 06:37:30 +0100

    Changing unnecessary $$, removing comment

--- src/search/FindReplace.js
+++ src/search/FindReplace.js
@@ -126,9 +126,7 @@ define(function (require, exports, module) {
                 return whole;
             }
         });
-        // we need to escape (\$) these chars in the regexp to avoid to get these parsed as
-        // normal regexp anchors
-        replaceWith = replaceWith.replace(/\$\$/g, "$$");
+        replaceWith = replaceWith.replace(/\$\$/g, "$");
         return replaceWith;
     }
 
