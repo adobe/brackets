842656b6085603455554dc37fccaddfe26a7f8f9
Ian Wehrman <wehrman@adobe.com>
2013-11-27 11:12:06 -0800

    Stub out the _fileSystemChange function for @jasonsanjose

--- src/project/ProjectManager.js
+++ src/project/ProjectManager.js
@@ -1686,9 +1686,12 @@ define(function (require, exports, module) {
      * @private 
      * Respond to a FileSystem change event.
      */
-    _fileSystemChange = function (event, item) {
-        // TODO: Refresh file tree too - once watchers are precise enough to notify only
-        // when real changes occur, instead of on every window focus!
+    _fileSystemChange = function (event, entry, added, removed) {
+        if (entry) {
+            // Use the added and removed sets to update the project tree here.
+        } else {
+            refreshFileTree();
+        }
         
         FileSyncManager.syncOpenDocuments();
     };
