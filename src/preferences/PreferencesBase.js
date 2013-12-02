23c34c354f82cfd5b6eb19ac085f6519934ef827
Jason San Jose <jasonsj@adobe.com>
2013-12-02 10:22:59 -0800

    fix indentation

--- src/preferences/PreferencesBase.js
+++ src/preferences/PreferencesBase.js
@@ -472,7 +472,8 @@ define(function (require, exports, module) {
     PreferencesManager.prototype = new MergedMap();
     
     _.extend(PreferencesManager.prototype, {
-        definePreference: function (id, type, initial, options) {            options = options || {};
+        definePreference: function (id, type, initial, options) {
+            options = options || {};
             if (this._knownPrefs.hasOwnProperty(id)) {
                 throw new Error("Preference " + id + " was redefined");
             }
