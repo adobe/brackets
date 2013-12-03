1ddf3932b9b96a39d027aec541c2a912e80b8a8c
Ian Wehrman <wehrman@adobe.com>
2013-12-02 16:43:06 -0800

    Always filter getAllFiles using shouldShow in case the project is not watched

--- src/project/ProjectManager.js
+++ src/project/ProjectManager.js
@@ -1639,10 +1639,13 @@ define(function (require, exports, module) {
 
         
         function visitor(entry) {
-            if (entry.isFile && !isBinaryFile(entry.name)) {
-                result.push(entry);
+            if (shouldShow(entry)) {
+                if (entry.isFile && !isBinaryFile(entry.name)) {
+                    result.push(entry);
+                }
+                return true;
             }
-            return true;
+            return false;
         }
         
         // First gather all files in project proper
