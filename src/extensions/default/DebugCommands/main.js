f86246f81ba68206b03901ef5280b1664ce09388
Lance Campbell <lkcampbell@gmail.com>
2014-01-12 18:17:02 -0800

    Roll back incorrect changes on translation strings

--- src/extensions/default/DebugCommands/main.js
+++ src/extensions/default/DebugCommands/main.js
@@ -23,7 +23,7 @@
 
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window, WebSocket, Mustache */
+/*global define, $, brackets, window, Mustache */
 
 define(function (require, exports, module) {
     "use strict";
