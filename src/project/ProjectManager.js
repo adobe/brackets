9d9167d3793158775c58dbbb1da30535eecfc8e4
Couzteau <couzteau@bitfaeule.net>
2014-01-13 23:15:44 -0800

    Enable project manager to differentiate on documentSelectionFocusChange between the case where it needs to open the file before redrawing the tree or just go ahead and redraw because the custom viewer for the file entry  is already open.

--- src/project/ProjectManager.js
+++ src/project/ProjectManager.js
@@ -542,23 +542,32 @@ define(function (require, exports, module) {
                 "select_node.jstree",
                 function (event, data) {
                     var entry = data.rslt.obj.data("entry");
+                    function redraw() {
+                        // update when tree display state changes
+                        _redraw(true);
+                        _lastSelected = data.rslt.obj;
+                    }
                     if (entry) {
                         if (entry.isFile) {
-                            var openResult = FileViewController.openAndSelectDocument(entry.fullPath, FileViewController.PROJECT_MANAGER);
-                        
-                            openResult.done(function () {
-                                // update when tree display state changes
-                                _redraw(true);
-                                _lastSelected = data.rslt.obj;
-                            }).fail(function () {
-                                if (_lastSelected) {
-                                    // revert this new selection and restore previous selection
-                                    _forceSelection(data.rslt.obj, _lastSelected);
-                                } else {
-                                    _projectTree.jstree("deselect_all");
-                                    _lastSelected = null;
-                                }
-                            });
+                            if (EditorManager.getCurrentlyViewedPath() !== entry.fullPath) {
+                                // avoid dupe open /bug #6001
+                                // if the entry points to a file that is already in view
+                                // we don't want to send the open command again.
+                                var openResult = FileViewController.openAndSelectDocument(entry.fullPath, FileViewController.PROJECT_MANAGER);
+                                openResult.done(redraw())
+                                    .fail(function () {
+                                        if (_lastSelected) {
+                                            // revert this new selection and restore previous selection
+                                            _forceSelection(data.rslt.obj, _lastSelected);
+                                        } else {
+                                            _projectTree.jstree("deselect_all");
+                                            _lastSelected = null;
+                                        }
+                                    });
+                            } else {
+                                redraw();
+                            }
+                           
                         } else {
                             FileViewController.setFileViewFocus(FileViewController.PROJECT_MANAGER);
                             // show selection marker on folders
