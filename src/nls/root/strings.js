26086ad7cf7beab218ea3b0b9360bd68afdab331
Randy Edmunds <redmunds@adobe.com>
2014-01-22 08:52:25 -0800

    support for timing function quick edit

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -171,6 +171,7 @@ define({
     "ERROR_CSSQUICKEDIT_CLASSNOTFOUND"      : "CSS Quick Edit: place cursor in class name",
     "ERROR_CSSQUICKEDIT_IDNOTFOUND"         : "CSS Quick Edit: place cursor in id name",
     "ERROR_CSSQUICKEDIT_UNSUPPORTEDATTR"    : "CSS Quick Edit: place cursor in tag name, class name, or id name",
+    "ERROR_TIMINGQUICKEDIT_INVALIDSYNTAX"   : "CSS Timing Function Quick Edit: invalid syntax",
 
     // Quick Docs
     "ERROR_QUICK_DOCS_PROVIDER_NOT_FOUND"   : "No Quick Docs provider found for current cursor position",
