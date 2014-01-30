ec6b2784a6d0d9e4453fa65432b094f956cf749d
unknown <ingo.richter@adobe.com>
2014-01-30 14:41:11 -0800

    - replace all backslashes in result file path to forward slashes to make new filesystem implementation happy

--- tasks/test.js
+++ tasks/test.js
@@ -39,11 +39,11 @@ module.exports = function (grunt) {
             spec            = grunt.option("spec") || "all",
             suite           = grunt.option("suite") || "all",
             results         = grunt.option("results") || process.cwd() + "/results.xml",
-            resultsPath     = common.resolve(results),
+            resultsPath     = common.resolve(results).replace(/\\/g, "/"),
             specRunnerPath  = common.resolve("test/SpecRunner.html"),
             args            = " --startup-path=\"" + specRunnerPath + "?suite=" + encodeURIComponent(suite) + "&spec=" + encodeURIComponent(spec) + "&resultsPath=" + encodeURIComponent(resultsPath) + "\"";
 
-        if (platform === "mac") {
+	if (platform === "mac") {
             cmd = "open \"" + cmd + "\" -W --args " + args;
         } else {
             cmd += args;
