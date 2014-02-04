3cc6c89f9eb3324dac3a1de970d819da4762d1ef
Jason San Jose <jasonsj@adobe.com>
2014-02-04 14:09:58 -0800

    temporary logging for travis build

--- tasks/build.js
+++ tasks/build.js
@@ -162,6 +162,8 @@ module.exports = function (grunt) {
             
             res.on("end", function () {
                 var json = JSON.parse(body);
+                grunt.log.writeln("DEBUG api.github.com");
+                grunt.log.writeln(body);
                 grunt.option("user", json.user.login);
                 grunt.task.run("cla-check");
                 done();
