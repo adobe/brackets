8c4f4080d282183b9d8e54cd93469a91232c80c8
Simon Wex <simon@simonwex.com>
2014-02-06 19:58:18 -0800

    Temporarily adding server for hosting on heroku

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/src'));
 
-app.listen(4000);
+app.use(express.logger());
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function(){
+  console.log(
+    "Express server listening on port:",
+    app.get('port'),
+    "( Likely http://localhost:" + app.get('port'),
+    ")"
+  );
+});
+
