5f6e15441e1cfe668153bbf50a8969905f77e08c
Jeff Booher <jsbooher@adobe.com>
2014-03-07 15:40:43 -0800

    revert xp to native node file watching impl

--- src/filesystem/impls/appshell/AppshellFileSystem.js
+++ src/filesystem/impls/appshell/AppshellFileSystem.js
@@ -563,7 +563,8 @@ define(function (require, exports, module) {
      *
      * @type {boolean}
      */
-    exports.recursiveWatch = appshell.platform === "mac" || appshell.platform === "win";
+    exports.recursiveWatch = (appshell.platform === "mac" ||
+        (appshell.platform === "win" && navigator.userAgent.indexOf("Windows NT 6.") >= 0));
     
     /**
      * Indicates whether or not the filesystem should expect and normalize UNC
