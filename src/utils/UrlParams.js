dd2a2aea9916bc4195858484fd98f7342c6f3820
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2014-03-18 10:15:15 -0400

    Replace String.trimRight() with regex in order to support IE10+

--- src/utils/UrlParams.js
+++ src/utils/UrlParams.js
@@ -55,7 +55,7 @@ define(function (require, exports, module) {
             queryString = url.substring(url.indexOf("?") + 1);
         }
         
-        queryString = queryString.trimRight();
+        queryString = queryString.replace(/\s*$/, "");
         
         if (queryString) {
             urlParams = queryString.split("&");
