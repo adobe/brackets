0ec2e1b976ff4b7a1445b3016a3cb1db15437645
Randy Edmunds <redmunds@adobe.com>
2014-03-21 07:57:05 -0700

    remove global xit

--- src/extensions/default/UrlCodeHints/unittests.js
+++ src/extensions/default/UrlCodeHints/unittests.js
@@ -23,7 +23,7 @@
 
 
 /*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, describe, it, xit, expect, beforeEach, afterEach, beforeFirst, afterLast, waitsFor, runs, $, brackets, waitsForDone */
+/*global define, describe, it, expect, beforeEach, afterEach, beforeFirst, afterLast, waitsFor, runs, $, brackets, waitsForDone */
 
 define(function (require, exports, module) {
     "use strict";
