dc0981d1f59f1e84eb78833579a575f6a5bbd3ee
Kevin Dangoor <kid@blazingthings.com>
2014-03-25 09:31:23 -0400

    Remove non-functional triangle

--- src/project/FileTreeView.jsx
+++ src/project/FileTreeView.jsx
@@ -30,19 +30,14 @@ define(function (require, exports, module) {
                 );
             }
             
-            var fileClasses = "",
-                triangle;
+            var fileClasses = "";
             if (this.props.selected) {
                 fileClasses = "jstree-clicked jstree-hovered sidebar-selection";
-                triangle=(
-                    <div className="sidebar-selection-triangle triangle-visible"/>
-                );
             }
             return (
                 <li className="jstree-leaf" onClick={this.handleClick}>
                     <ins className="jstree-icon">&nbsp;</ins>
                     <a href="#" className={fileClasses}>{name}{extension}</a>
-                    {triangle}
                 </li>
             );
         }
