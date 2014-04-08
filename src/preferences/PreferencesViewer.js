58fba6e51df7699348d7d11ffede7492b239f990
Martin Zagora <zaggino@gmail.com>
2014-04-08 11:38:06 +1000

    simple dialog for object and array preferences

--- src/preferences/PreferencesViewer.js
+++ src/preferences/PreferencesViewer.js
@@ -32,17 +32,107 @@
 define(function (require, exports, module) {
     "use strict";
 
-    var _                             = require("thirdparty/lodash"),
-        CommandManager                = require("command/CommandManager"),
-        Commands                      = require("command/Commands"),
-        EditorManager                 = require("editor/EditorManager"),
-        PreferencesManager            = require("preferences/PreferencesManager"),
-        Strings                       = require("strings"),
-        SettingsDialogTemplate        = require("text!htmlContent/preferences-viewer.html"),
-        SettingsDialogTemplateParts   = require("text!htmlContent/preferences-viewer-parts.html");
+    var _                               = require("thirdparty/lodash"),
+        CommandManager                  = require("command/CommandManager"),
+        Commands                        = require("command/Commands"),
+        Dialogs                         = require("widgets/Dialogs"),
+        DefaultDialogs                  = require("widgets/DefaultDialogs"),
+        EditorManager                   = require("editor/EditorManager"),
+        PreferencesManager              = require("preferences/PreferencesManager"),
+        Strings                         = require("strings"),
+        PreferencesViewerTemplate       = require("text!htmlContent/preferences-viewer.html"),
+        PreferencesViewerPartsTemplate  = require("text!htmlContent/preferences-viewer-parts.html"),
+        PreferencesViewerDialogTemplate = require("text!htmlContent/preferences-viewer-dialog.html");
 
     var extensions,
         mainPrefs;
+
+    // convienience method to display a name even for those who don't have it defined
+    function _getPreferenceName(pref, includeExtInfo) {
+        var name = pref.name || (pref.idPrefix ? pref.id.substring(pref.idPrefix.length) : pref.id);
+        if (includeExtInfo && pref.idPrefix) {
+            name = pref.idPrefix.slice(0, -1) + " \u2014 " + name;
+        }
+        return name;
+    }
+
+    function _toggleValid($el, validBool, message) {
+        $el.toggleClass("validation-error", !validBool);
+        if (validBool) {
+            $el.removeAttr("title");
+        } else {
+            $el.attr("title", message);
+        }
+    }
+
+    function _validateElement($el, pref) {
+        var val = $el.val(),
+            valid = true;
+
+        if (pref.validator) {
+            valid = pref.validator(val);
+        }
+
+        var msg = "Value invalid!";
+        if (pref.description) {
+            msg += " (" + pref.description + ")";
+        }
+        if (typeof valid !== "boolean") {
+            msg = valid;
+            valid = false;
+        }
+
+        _toggleValid($el, valid, msg);
+        return valid;
+    }
+
+    function openDialog(pref) {
+        var compiledTemplate = Mustache.render(PreferencesViewerDialogTemplate, {
+            title: _getPreferenceName(pref, true),
+            template: pref.template,
+            Strings: Strings
+        });
+
+        var dialog = Dialogs.showModalDialogUsingTemplate(compiledTemplate),
+            $dialog = dialog.getElement(),
+            $textarea = $dialog.find("textarea"),
+            $okBtn = $dialog.find("button[data-button-id='ok']");
+
+        $okBtn.on("click", function (event) {
+            if (!pref.template) {
+
+                var newVal = $textarea.val();
+                try {
+                    JSON.parse(newVal);
+                } catch (e) {
+                    _toggleValid($textarea, false, e);
+                    event.stopPropagation();
+                    return;
+                }
+                var valid = _validateElement($textarea, pref);
+                if (!valid) {
+                    event.stopPropagation();
+                    return;
+                }
+
+            } else {
+                // TODO:
+            }
+
+        });
+
+        dialog.done(function (buttonId) {
+            if (buttonId === "ok") {
+                // TODO: validated successfully
+            }
+        });
+
+        if (!pref.template) {
+            $textarea.val(JSON.stringify(PreferencesManager.get(pref.id), null, 4));
+        } else {
+            // TODO:
+        }
+    }
 
     function initVars() {
         // get all extensions that called getExtensionPrefs
@@ -50,33 +140,38 @@ define(function (require, exports, module) {
 
         // setup an array for brackets preferences
         mainPrefs = {preferences: [], groups: {}};
-        function pushToPrefObj(what, which) {
-            var group = what.group;
+        function pushToPrefObj(pref, prefObj) {
+            var group = pref.group;
             if (group) {
-                if (!which.groups[group]) { which.groups[group] = []; }
-                which.groups[group].push(what);
+                if (!prefObj.groups[group]) { prefObj.groups[group] = []; }
+                prefObj.groups[group].push(pref);
             } else {
-                which.preferences.push(what);
+                prefObj.preferences.push(pref);
             }
         }
 
         // iterate all defined preferences
-        PreferencesManager.getKnownPreferences().forEach(function (key) {
-            var pref = PreferencesManager.getPreference(key);
-            // find if this preference starts with an extension id
-            var ext = _.find(extensions, function (ext) {
-                return key.indexOf(ext.id + ".") === 0;
-            });
+        PreferencesManager.getKnownPreferences().forEach(function (id) {
+
+            // we should take care not to modify anything on pref
+            var pref = PreferencesManager.getPreference(id);
+
+            var ext;
+            if (pref.idPrefix) {
+                // find if this preference starts with an extension id
+                ext = _.find(extensions, function (ext) {
+                    return id.indexOf(ext.id + ".") === 0;
+                });
+            }
+
             // assign preference to appropriate extension or global
-            pref.fullKey = key;
             if (!ext) {
-                pref.key = key;
                 pushToPrefObj(pref, mainPrefs);
             } else {
                 ext.prefsObj = ext.prefsObj || {preferences: [], groups: {}};
-                pref.key = key.substring(ext.id.length + 1);
                 pushToPrefObj(pref, ext.prefsObj);
             }
+
         });
 
         // prepare for Mustache
@@ -125,29 +220,14 @@ define(function (require, exports, module) {
         // set values and attach validators
         $view.find("*[name]").each(function () {
             var $this = $(this),
-                key = $this.attr("name"),
-                value = PreferencesManager.get(key);
+                id = $this.attr("name"),
+                value = PreferencesManager.get(id);
 
-            var pref = PreferencesManager.getPreference(key);
+            var pref = PreferencesManager.getPreference(id);
             $this.off("change.validator").removeClass("validation-error");
             if (pref.validator) {
                 $this.on("change.validator", function () {
-                    var $this = $(this),
-                        val = $this.val(),
-                        valid = pref.validator(val);
-
-                    var msg = "Value invalid!";
-                    if (typeof valid !== "boolean") {
-                        msg = valid;
-                        valid = false;
-                    }
-
-                    $this.toggleClass("validation-error", !valid);
-                    if (valid) {
-                        $this.removeAttr("title");
-                    } else {
-                        $this.attr("title", msg);
-                    }
+                    _validateElement($(this), pref);
                 });
             }
 
@@ -158,17 +238,27 @@ define(function (require, exports, module) {
             }
         });
 
-        $view.find("button[data-button-id='save']").on("click", function (id) {
+        $view.on("click", "button.object-editor", function (e) {
+            e.preventDefault();
+
+            var id = $(e.target).attr("name"),
+                pref = PreferencesManager.getPreference(id);
+            openDialog(pref);
+        });
+
+        $view.on("click", "button[data-button-id='save']", function (e) {
+            e.preventDefault();
             alert("save!");
         });
 
-        $view.find("button[data-button-id='cancel']").on("click", function (id) {
-            EditorManager._closeCustomViewer();
+        $view.on("click", "button[data-button-id='cancel']", function (e) {
+            e.preventDefault();
+            CommandManager.execute(Commands.NAVIGATE_PREV_DOC);
         });
     }
 
     function renderView() {
-        var parts = SettingsDialogTemplateParts.split("======").reduce(function (obj, part) {
+        var parts = PreferencesViewerPartsTemplate.split("======").reduce(function (obj, part) {
             if (part.trim().length === 0) {
                 return obj;
             }
@@ -189,14 +279,14 @@ define(function (require, exports, module) {
                 var template = parts[this.type];
                 if (!template) { template = parts.other; }
                 return Mustache.render(template, {
-                    key: this.fullKey,
-                    name: this.name || this.key,
+                    id: this.id,
+                    name: _getPreferenceName(this),
                     description: this.description || ""
                 });
             }
         };
 
-        var compiledTemplate = Mustache.render(SettingsDialogTemplate, templateVars);
+        var compiledTemplate = Mustache.render(PreferencesViewerTemplate, templateVars);
         var $view = $(compiledTemplate);
         attachEvents($view);
         return $view;
