d94c38cbdb34cf1737d5729eff2056a126ac380e
bodhiBit <TGiessingP@gmail.com>
2014-06-24 22:55:06 +0200

    Removed link to missing Danish translation of Adobe thirdparty EULA

--- src/nls/da/urls.js
+++ src/nls/da/urls.js
@@ -27,6 +27,5 @@
 define({
     // Relative to the samples folder
     "GETTING_STARTED"           : "da/Kom godt i gang",
-    "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty_da/",
     "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.da"
 });
