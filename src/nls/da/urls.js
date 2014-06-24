cfea318ad4d5e1747cf893a423fc32de5a3e11e5
bodhiBit <TGiessingP@gmail.com>
2014-06-24 14:58:23 +0200

    Corrections and improvements suggested by http://newz.dk/forum/tagwall/review-af-dansk-oversaettelse-af-brackets-134128#2

--- src/nls/da/urls.js
+++ src/nls/da/urls.js
@@ -26,7 +26,7 @@
 
 define({
     // Relative to the samples folder
-    "GETTING_STARTED"           : "da/Kom Godt Igang",
+    "GETTING_STARTED"           : "da/Kom godt i gang",
     "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty_da/",
     "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.da"
 });
