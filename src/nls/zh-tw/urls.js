cd2118697a5b058502d70354243aaaea7f01cbea
Pei-Tang Huang <tangtheone@gmail.com>
2014-07-09 23:49:01 +0800

    Copied "Getting Started" from en locale.

--- src/nls/zh-tw/urls.js
+++ src/nls/zh-tw/urls.js
@@ -26,7 +26,7 @@
 
 define({
     // Relative to the samples folder
-    "GETTING_STARTED"           : "root/Getting Started",
+    "GETTING_STARTED"           : "zh-tw/入門",
     "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty/",
     "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.zh_TW"
 });
