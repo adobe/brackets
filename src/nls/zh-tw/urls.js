23b3ba56043fed9a53a3ec6ed6e622063d7ca5e5
Pei-Tang Huang <tangtheone@gmail.com>
2014-07-09 23:42:59 +0800

    Update CC BY 3.0 link to zh-TW localized version.

--- src/nls/zh-tw/urls.js
+++ src/nls/zh-tw/urls.js
@@ -28,5 +28,5 @@ define({
     // Relative to the samples folder
     "GETTING_STARTED"           : "root/Getting Started",
     "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty/",
-    "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/"
+    "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.zh_TW"
 });
