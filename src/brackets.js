892a1c8e0f6b0df54e1443a04ee139ba74a64bd6
Jeff Booher <jsbooher@adobe.com>
2014-07-21 10:52:39 -0700

    lost during merge

--- src/brackets.js
+++ src/brackets.js
@@ -407,14 +407,14 @@ define(function (require, exports, module) {
         $("html").on("mousedown", ".no-focus", function (e) {
             // Text fields should always be focusable.
             var $target = $(e.target),
-                isTextField =
+                isFormElement =
                     $target.is("input[type=text]") ||
                     $target.is("input[type=number]") ||
                     $target.is("input[type=password]") ||
                     $target.is("input:not([type])") || // input with no type attribute defaults to text
                     $target.is("textarea");
     
-            if (!isTextField) {
+            if (!isFormElement) {
                 e.preventDefault();
             }
         });
