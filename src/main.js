6d0e684455f6ff8a91b38e6f5e3c7755731eb396
Randy Edmunds <redmunds@adobe.com>
2014-07-29 12:11:53 -0700

    brackets polyfill support

--- src/main.js
+++ src/main.js
@@ -59,6 +59,22 @@ require.config({
     locale: window.localStorage.getItem("locale") || (typeof (brackets) !== "undefined" ? brackets.app.language : navigator.language)
 });
 
+require.config({
+    // Remember: only use shim config for non-AMD scripts,
+    // scripts that do not already call define(). The shim
+    // config will not work correctly if used on AMD scripts,
+    // in particular, the exports and init config will not
+    // be triggered, and the deps config will be confusing
+    // for those cases.
+    shim: {
+        'brackets': {
+            // These script dependencies should be loaded before loading
+            // brackets.js
+            deps: ['utils/Compatibility']
+        }
+    }
+});
+
 define(function (require, exports, module) {
     "use strict";
     
