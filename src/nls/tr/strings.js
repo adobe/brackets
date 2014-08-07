31186bda471d459020a75592936028335517e7be
Yasin Kuyu <yasinkuyu@gmail.com>
2014-08-08 01:03:33 +0300

    Update strings.js

--- src/nls/tr/strings.js
+++ src/nls/tr/strings.js
@@ -1,30 +1,31 @@
 /*
  * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 
 define({
+
     /**
      * Errors
      */
@@ -35,8 +36,17 @@ define({
     "NOT_READABLE_ERR"                  : "Dosya okunamadı.",
     "NO_MODIFICATION_ALLOWED_ERR"       : "Klasörde değişiklik yapılamıyor.",
     "NO_MODIFICATION_ALLOWED_ERR_FILE"  : "Dosya değişikliği için izniniz yok.",
+    "CONTENTS_MODIFIED_ERR"             : "Dosya {APP_NAME} dışında modifiye edilmiştir.",
+    "UNSUPPORTED_ENCODING_ERR"          : "{APP_NAME} şu anda sadece UTF-8 ile kodlanmış metin dosyaları destekler.",
     "FILE_EXISTS_ERR"                   : "Dosya bulunmaktadır.",
-
+    "FILE"                              : "dosya",
+    "DIRECTORY"                         : "klasör",
+    "DIRECTORY_NAMES_LEDE"              : "Klasör adları",
+    "FILENAMES_LEDE"                    : "Dosyaadları",
+    "FILENAME"                          : "dosyadı",
+    "DIRECTORY_NAME"                    : "klasör adı",
+    
+    
     // Project error strings
     "ERROR_LOADING_PROJECT"             : "Proje yüklenirken hata",
     "OPEN_DIALOG_ERROR"                 : "Dosya dialogu gösterilirken hata meydana geldi. (hata {0})",
@@ -46,20 +56,27 @@ define({
     // File open/save error string
     "ERROR_OPENING_FILE_TITLE"          : "Dosya açılırken hata",
     "ERROR_OPENING_FILE"                : "<span class='dialog-filename'>{0}</span> dosyası açılırken hata meydana geldi. {1}",
+    "ERROR_OPENING_FILES"               : "An error occurred when trying to open the following files:",
     "ERROR_RELOADING_FILE_TITLE"        : "Değişiklikler hafızadan okunurken hata oluştu.",
     "ERROR_RELOADING_FILE"              : "<span class='dialog-filename'>{0}</span> dosyası yenilenirken hata meydana geldi. {1}",
     "ERROR_SAVING_FILE_TITLE"           : "Dosya kaydedilirken hata",
     "ERROR_SAVING_FILE"                 : "<span class='dialog-filename'>{0}</span> dosyası kaydedilirken hata meydana geldi. {1}",
     "ERROR_RENAMING_FILE_TITLE"         : "Dosya ismi değiştirilirken hata",
     "ERROR_RENAMING_FILE"               : "<span class='dialog-filename'>{0}</span> dosyasının ismi değiştirilirken hata meydana geldi. {1}",
+    "ERROR_DELETING_FILE_TITLE"         : "Error Deleting File",
+    "ERROR_DELETING_FILE"               : "An error occurred when trying to delete the file <span class='dialog-filename'>{0}</span>. {1}",
     "INVALID_FILENAME_TITLE"            : "Hatalı dosya ismi",
     "INVALID_FILENAME_MESSAGE"          : "Dosya isimleri yandaki karakterleri bulunduramaz: {0}",
-    "FILE_ALREADY_EXISTS"               : "<span class='dialog-filename'>{0}</span> dosyası zaten bulunmakta",
+    "ENTRY_WITH_SAME_NAME_EXISTS"       : "A file or directory with the name <span class='dialog-filename'>{0}</span> already exists.",
     "ERROR_CREATING_FILE_TITLE"         : "Dosya yaratılırken hata",
     "ERROR_CREATING_FILE"               : "<span class='dialog-filename'>{0}</span> dosyası yaratılırken hata meydana geldi. {1}",
 
+    // Application preferences corrupt error strings
+    "ERROR_PREFS_CORRUPT_TITLE"         : "Tercihleri okuma hatası",
+    "ERROR_PREFS_CORRUPT"               : "JSON tercih dosyası geçerli değil. Dosya biçimini düzelttiğinde açılacaktır. {APP_NAME} değişikliklerin geçerli olabilmesi için uygulamayı yeniden başlatın.",
+
     // Application error strings
-    "ERROR_IN_BROWSER_TITLE"            : "Ups! {APP_NAME} programı şuan tarayıcıda açılmadı.",
+    "ERROR_IN_BROWSER_TITLE"            : "Hoop! {APP_NAME} programı şuan tarayıcıda açılmadı.",
     "ERROR_IN_BROWSER"                  : "{APP_NAME} HTML olarak hazırlandı, ancak şuan için masaüstünde çalışabilmekte. Bu nedenle makinanızda bulunan dosyalarda değişiklik için kullanabilirsiniz. {APP_NAME} programını çalıştırabilmek için lütfen <b>github.com/adobe/brackets-shell</b> adresindeki 'application shell'i kullanınız.",
 
     // FileIndexManager error string
@@ -74,8 +91,10 @@ define({
     "LIVE_DEVELOPMENT_ERROR_TITLE"      : "Canlı Önizleme Hatası",
     "LIVE_DEVELOPMENT_RELAUNCH_TITLE"   : "Tarayıcıya bağlanılıyor",
     "LIVE_DEVELOPMENT_ERROR_MESSAGE"    : "Canlı önizleme özelliğini kullanabilmek için uzaktan hata ayıklayıcı ile Chrome'un tekrardan açılması gerekiyor.<br /><br />Chrome'u uzaktan hata ayıklayıcı ile tekrardan açılmasını ister misiniz?",
+    "LIVE_DEV_LOADING_ERROR_MESSAGE"    : "Unable to load Live Preview page",
     "LIVE_DEV_NEED_HTML_MESSAGE"        : "Canlı önizlemeyi çalıştırabilmeniz için html dosyası açmanız gerekiyor",
     "LIVE_DEV_NEED_BASEURL_MESSAGE"     : "Canlı Önizlemeyi server-side dosyalarınız ile açmak istiyorsanız, lütfen bu proje için kullanılabilir bir link belirtiniz.",
+    "LIVE_DEV_SERVER_NOT_READY_MESSAGE" : "Canlı önizleme için HTTP sunucusu başlatılamıyor. Lütfen tekrar deneyiniz.",
     "LIVE_DEVELOPMENT_INFO_TITLE"       : "Canlı Önizlemeye Hoşgeldiniz!",
     "LIVE_DEVELOPMENT_INFO_MESSAGE"     : "Canlı önizleme için {APP_NAME} programı tarayıcınıza bağlanıyor ve sizin HTML dosyanızın örnek görüntüsünü tarayıcınızda açıyor. Sonrasında ise değişiklik yaptığınız sayfayı anında tarayıcıda yenileyerek gösteriyor.<br /><br />{APP_NAME} programının bu versiyonunda, Canlı önizleme özelliği sadece <strong>CSS dosyaları</strong> değişikliğine izin veriyor ve sadece <strong>Google Chrome</strong> üzerinde çalışıyor. Yakında HTML ve Javascript değişikliğini de ekleyeceğiz!<br /><br />(Bu mesaj sadece tek sefer gösterilecektir.)",
     "LIVE_DEVELOPMENT_TROUBLESHOOTING"  : "Detaylı bilgi için lütfen <a href='{0}' title='{0}'>Troubleshooting Live Development connection errors</a> sayfasına bakınız.",
@@ -85,40 +104,89 @@ define({
     "LIVE_DEV_STATUS_TIP_PROGRESS2"     : "Canlı Önizleme: Başlıyor\u2026",
     "LIVE_DEV_STATUS_TIP_CONNECTED"     : "Canlı Önizleme'den Çık",
     "LIVE_DEV_STATUS_TIP_OUT_OF_SYNC"   : "Canlı Önizleme: Kapatmak için tıklayın (Yenilemek için dosyayı kaydedin)",
+    "LIVE_DEV_STATUS_TIP_SYNC_ERROR"    : "Live Preview (not updating due to syntax error)",
+
+    "LIVE_DEV_DETACHED_REPLACED_WITH_DEVTOOLS" : "Geliştirici araçları açıldığı için canlı önizleme iptal edildi",
+    "LIVE_DEV_DETACHED_TARGET_CLOSED"          : "Tarayıcı sayfası kapatıldğı için canlı önizleme iptal edildi",
+    "LIVE_DEV_NAVIGATED_AWAY"                  : "Projede yer almayan bir dosya açıldığı için canlı önizleme iptal edildi",
+    "LIVE_DEV_CLOSED_UNKNOWN_REASON"           : "Bilinmeyen bir denenden dolayı canlı önizleme iptal edildi ({0})",
     
     "SAVE_CLOSE_TITLE"                  : "Değişiklikleri kaydet",
     "SAVE_CLOSE_MESSAGE"                : "<span class='dialog-filename'>{0}</span> dosyasında yaptığınız değişiklikleri kaydetmek istiyor musunuz?",
     "SAVE_CLOSE_MULTI_MESSAGE"          : "Dosyalardaki değişiklikleri kaydetmek istiyor musunuz?",
     "EXT_MODIFIED_TITLE"                : "Harici değişiklikler",
+    "CONFIRM_FOLDER_DELETE_TITLE"       : "Silmeyi Onayla",
+    "CONFIRM_FOLDER_DELETE"             : "<span class='dialog-filename'>{0}</span> klasörü silmek istediğinizden emin misiniz??",
+    "FILE_DELETED_TITLE"                : "Dosya Silindi",
+    "EXT_MODIFIED_WARNING"              : "<span class='dialog-filename'>{0}</span> değiştirildi.<br /><br />Bu değişikliklerin üzerine yazmak istiyor musunuz?",
     "EXT_MODIFIED_MESSAGE"              : "<span class='dialog-filename'>{0}</span> dosyası dışarıdan değiştirildi ama {APP_NAME} programında kaydetmediğiniz değişiklikler bulunmakta.<br /><br />Hangi versiyonun kalmasını istiyorsunuz?",
-    "EXT_DELETED_MESSAGE"               : "<span class='dialog-filename'>{0}</span> dosyası hafızadan silind ama {APP_NAME} programında kaydetmediğiniz değişiklikler bulunmakta.<br /><br />Değişikliklerin kalmasını istiyor musunuz?",
+    "EXT_DELETED_MESSAGE"               : "<span class='dialog-filename'>{0}</span> dosyası hafızadan silindi ama {APP_NAME} programında kaydetmediğiniz değişiklikler bulunmakta.<br /><br />Değişikliklerin kalmasını istiyor musunuz?",
     
-    // Find, Replace, Find in Files
-    "SEARCH_REGEXP_INFO"                : "Regex arama yapmak için /re/ sentaksını kullanın",
-    "WITH"                              : "İle",
+    // Generic dialog/button labels
+    "DONE"                              : "Bitti",
+    "OK"                                : "TAMAM",
+    "CANCEL"                            : "Vazgeç",
+    "DONT_SAVE"                         : "Kaydetme",
+    "SAVE"                              : "Kaydet",
+    "SAVE_AS"                           : "Farklı kaydet\u2026",
+    "SAVE_AND_OVERWRITE"                : "Üstüne yaz",
+    "DELETE"                            : "Sil",
     "BUTTON_YES"                        : "Evet",
     "BUTTON_NO"                         : "Hayır",
-    "BUTTON_STOP"                       : "Durdur",
+    
+    // Find, Replace, Find in Files
+    "FIND_RESULT_COUNT"                 : "{0} sonuçlar",
+    "FIND_RESULT_COUNT_SINGLE"          : "1 sonuç",
+    "FIND_NO_RESULTS"                   : "Sonuç Yok",
+    "FIND_QUERY_PLACEHOLDER"            : "Bul\u2026",
+    "REPLACE_PLACEHOLDER"               : "ile değiştir\u2026",
+    "BUTTON_REPLACE_ALL"                : "Toplu\u2026",
+    "BUTTON_REPLACE_ALL_IN_FILES"       : "Değiştir\u2026",
+    "BUTTON_REPLACE"                    : "Değiştir",
+    "BUTTON_NEXT"                       : "\u25B6",
+    "BUTTON_PREV"                       : "\u25C0",
+    "BUTTON_NEXT_HINT"                  : "Sonraki",
+    "BUTTON_PREV_HINT"                  : "Önceki",
+    "BUTTON_CASESENSITIVE_HINT"         : "Büyük/Küçük Harf Duyarlı",
+    "BUTTON_REGEXP_HINT"                : "Düzenli İfade(Regex)",
+    "REPLACE_WITHOUT_UNDO_WARNING_TITLE": "Geri al",
+    "REPLACE_WITHOUT_UNDO_WARNING"      : "Because more than {0} files need to be changed, {APP_NAME} will modify unopened files on disk.<br />You won't be able to undo replacements in those files.",
+    "BUTTON_REPLACE_WITHOUT_UNDO"       : "Geri al",
 
     "OPEN_FILE"                         : "Dosya Aç",
+    "SAVE_FILE_AS"                      : "Dosyayı kaydet",
     "CHOOSE_FOLDER"                     : "Klasör Seç",
 
+    "RELEASE_NOTES"                     : "Sürüm Notları",
+    "NO_UPDATE_TITLE"                   : "Güncel",
+    "NO_UPDATE_MESSAGE"                 : "{APP_NAME}'ın en son sürümünü çalıştırıyorsunuz.",
+
+    // Find and Replace
+    "FIND_REPLACE_TITLE_LABEL"          : "Değiştir",
+    "FIND_REPLACE_TITLE_WITH"           : "İle",
+    "FIND_TITLE_LABEL"                  : "Bulundu",
+    "FIND_TITLE_SUMMARY"                : " &mdash; {0} {1} {2} içinde {3}",
+
+    // Find in Files
+    "FIND_NUM_FILES"                    : "{0} {1}",
+    "FIND_IN_FILES_SCOPED"              : "<span class='dialog-filename'>{0}</span> dosyası içinde",
+    "FIND_IN_FILES_NO_SCOPE"            : "in project",
+    "FIND_IN_FILES_ZERO_FILES"          : "Filtre hariç tüm dosyalar {0}",
+    "FIND_IN_FILES_FILE"                : "dosya",
+    "FIND_IN_FILES_FILES"               : "dosyalar",
+    "FIND_IN_FILES_MATCH"               : "bulunan",
+    "FIND_IN_FILES_MATCHES"             : "bulunanlar",
+    "FIND_IN_FILES_MORE_THAN"           : "Daha fazla ",
+    "FIND_IN_FILES_PAGING"              : "{0}&mdash;{1}",
+    "FIND_IN_FILES_FILE_PATH"           : "<span class='dialog-filename'>{0}</span> {2} <span class='dialog-path'>{1}</span>", // We should use normal dashes on Windows instead of em dash eventually
+    "FIND_IN_FILES_EXPAND_COLLAPSE"     : "Tümünü genişletmek/daraltmak için Ctrl/Cmd tıklayın",
+    "REPLACE_IN_FILES_ERRORS_TITLE"     : "Hataları değiştir",
+    "REPLACE_IN_FILES_ERRORS"           : "TheAşağıdaki dosyalar aramadan sonra değişti.",
+    
     "RELEASE_NOTES"                     : "Yeni Versiyon Bilgileri",
     "NO_UPDATE_TITLE"                   : "En son versiyon!",
     "NO_UPDATE_MESSAGE"                 : "{APP_NAME} programının en son versiyonunu kullanıyorsunuz.",
     
-    "FIND_IN_FILES_TITLE"               : "- {2} {3} içindeki {0} {1}",
-    "FIND_IN_FILES_SCOPED"              : "<span class='dialog-filename'>{0}</span> dosyası içinde",
-    "FIND_IN_FILES_NO_SCOPE"            : "proje içinde",
-    "FIND_IN_FILES_FILE"                : "dosya",
-    "FIND_IN_FILES_FILES"               : "dosyalar",
-    "FIND_IN_FILES_MATCH"               : "eşleşen",
-    "FIND_IN_FILES_MATCHES"             : "eşleşenler",
-    "FIND_IN_FILES_MORE_THAN"           : "Daha fazla ",
-    "FIND_IN_FILES_MAX"                 : " (ilk {0} eşlemeyi gösteriyor)",
-    "FIND_IN_FILES_FILE_PATH"           : "Dosya: <b>{0}</b>",
-    "FIND_IN_FILES_LINE"                : "Satır:&nbsp;{0}",
-
     "ERROR_FETCHING_UPDATE_INFO_TITLE"  : "Versiyon bilgisi alınırken hata",
     "ERROR_FETCHING_UPDATE_INFO_MSG"    : "Sunucudan yeni versiyon bilgisi alınırken hata oluştu. Lütfen internete bağlı olduğunuzdan emin olun ve tekrar deneyin.",
     
