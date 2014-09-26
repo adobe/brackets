25e7f3f3688b1f87c297ee40a9fdf9d339932689
Randy Edmunds <redmunds@adobe.com>
2014-09-26 15:01:23 -0700

    set focus in new pane only if there is a file in original pane

--- src/view/MainViewManager.js
+++ src/view/MainViewManager.js
@@ -1056,8 +1056,8 @@ define(function (require, exports, module) {
         
         $(exports).triggerHandler("paneLayoutChange", [_orientation]);
         
-        // if new pane was created, make it the active pane
-        if (newPane) {
+        // if new pane was created, and original pane is not empty, make new pane the active pane
+        if (newPane && getWorkingSetSize(firstPane.id) > 0) {
             setActivePaneId(newPane.id);
         }
     }
