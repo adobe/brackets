d1eef447e880989bc5fbd1c8d5c1a11192363b8e
valtlait <valtlait@users.noreply.github.com>
2014-10-01 18:15:58 +0300

    Update strings.js

--- src/nls/fi/strings.js
+++ src/nls/fi/strings.js
@@ -434,7 +434,7 @@ define({
     "GET_IT_NOW"                           : "Hae nyt!",
     "PROJECT_SETTINGS_TITLE"               : "Projektin asetukset kohteelle: {0}",
     "PROJECT_SETTING_BASE_URL"             : "Esikatselun URL-osoite",
-    "PROJECT_SETTING_BASE_URL_HINT"        : "Käytä paikallista palvelinta antamalla url, kuten http://localhost:8000/",
+    "PROJECT_SETTING_BASE_URL_HINT"        : "Käytä paikal. palvelinta antamalla url, kuten http://localhost:8000/",
     "BASEURL_ERROR_INVALID_PROTOCOL"       : "Esikatselu ei tue {0}-protokollaa. Käytä joko http: tai https: .",
     "BASEURL_ERROR_SEARCH_DISALLOWED"      : "URL-osoite ei voi sisältää hakuparametreja, kuten ”{0}”.",
     "BASEURL_ERROR_HASH_DISALLOWED"        : "URL-osoite ei voi sisältää ristikkomerkkejä, kuten ”{0}”.",
