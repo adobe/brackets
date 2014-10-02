23a861384b50d830d4145ad0514d7033b6ad455e
Jeff Booher <jsbooher@adobe.com>
2014-10-02 14:57:22 -0700

    add fix for deleting, from Finder/Explorer, temporary views of images

--- src/project/ProjectManager.js
+++ src/project/ProjectManager.js
@@ -1088,6 +1088,17 @@ define(function (require, exports, module) {
         FileSyncManager.syncOpenDocuments();
 
         model.handleFSEvent(entry, added, removed);
+        
+        // @TODO: DocumentManager should implement its own fsChange  handler
+        //          we can clean up the calls to DocumentManager.notifyPathDeleted
+        //          and privatize DocumentManager.notifyPathDeleted as well
+        //        We can also remove the _fileSystemRename handler below and move
+        //          it to DocumentManager
+        if (removed) {
+            removed.forEach(function(file) {
+                DocumentManager.notifyPathDeleted(file.fullPath);
+            });
+        }
     };
 
     /**
