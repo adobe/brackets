2caf4f2e5745a87d482246b2aa57cdd5aab1e13d
encadyma <encadyma@gmail.com>
2014-11-16 21:09:00 -0800

    Changed string NOT_READABLE_ERR, NOT_FOUND_ERR from 'file' to 'file/directory' in ./src/nls/root/strings.js

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -32,8 +32,8 @@ define({
 
     // General file io error strings
     "GENERIC_ERROR"                     : "(error {0})",
-    "NOT_FOUND_ERR"                     : "The file could not be found.",
-    "NOT_READABLE_ERR"                  : "The file could not be read.",
+    "NOT_FOUND_ERR"                     : "The file/directory could not be found.",
+    "NOT_READABLE_ERR"                  : "The file/directory could not be read.",
     "EXCEEDS_MAX_FILE_SIZE"             : "Files larger than {0} MB cannot be opened in {APP_NAME}.",
     "NO_MODIFICATION_ALLOWED_ERR"       : "The target directory cannot be modified.",
     "NO_MODIFICATION_ALLOWED_ERR_FILE"  : "The permissions do not allow you to make modifications.",
