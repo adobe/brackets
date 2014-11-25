edad1e044c72962d8c0230e2879e941f2917f1b4
Marcel Gerber <mg.hain@gmx.de>
2014-11-25 08:58:17 +0100

    Support Quick Docs (Web Platform Docs) in HTML

--- src/extensions/default/WebPlatformDocs/main.js
+++ src/extensions/default/WebPlatformDocs/main.js
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
- *  
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
 
 
@@ -29,98 +29,131 @@ define(function (require, exports, module) {
     "use strict";
 
     // Core modules
-    var EditorManager        = brackets.getModule("editor/EditorManager"),
-        FileSystem           = brackets.getModule("filesystem/FileSystem"),
-        FileUtils            = brackets.getModule("file/FileUtils"),
-        ExtensionUtils       = brackets.getModule("utils/ExtensionUtils"),
-        CSSUtils             = brackets.getModule("language/CSSUtils");
-    
+    var _               = brackets.getModule("thirdparty/lodash"),
+        EditorManager   = brackets.getModule("editor/EditorManager"),
+        FileSystem      = brackets.getModule("filesystem/FileSystem"),
+        FileUtils       = brackets.getModule("file/FileUtils"),
+        CSSUtils        = brackets.getModule("language/CSSUtils"),
+        HTMLUtils       = brackets.getModule("language/HTMLUtils"),
+        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils");
+
     // Extension modules
     var InlineDocsViewer = require("InlineDocsViewer");
-    
-    
-    /** @type {?$.Promise} */
-    var _cssDocsPromise = null;
-    
+
+
+    /*
+     * Caches docs promises
+     */
+    var promiseCache = {};
+
     /**
      * Lazily loads JSON docs files. Returns a Promise the is resolved with the parsed Object, or
      * rejected if the file is missing/corrupt.
+     * @param {string} fileName JSON file to load
      * @return {!$.Promise}
      */
-    function getCSSDocs() {
-        if (!_cssDocsPromise) {
+    function getDocs(fileName) {
+        if (!promiseCache[fileName]) {
             var result = new $.Deferred();
-            
-            var path = ExtensionUtils.getModulePath(module, "css.json"),
+            var path = ExtensionUtils.getModulePath(module, fileName),
                 file = FileSystem.getFileForPath(path);
-            
+
             FileUtils.readAsText(file)
                 .done(function (text) {
                     var jsonData;
                     try {
                         jsonData = JSON.parse(text);
                     } catch (ex) {
-                        console.error("Malformed CSS documentation database: ", ex);
+                        console.error("Malformed documentation database: ", path, ex);
                         result.reject();
                     }
                     result.resolve(jsonData);  // ignored if we already reject()ed above
                 })
                 .fail(function (err) {
-                    console.error("Unable to load CSS documentation database: ", err);
+                    console.error("Unable to load documentation database: ", path,  err);
                     result.reject();
                 });
-            
-            _cssDocsPromise = result.promise();
+
+            promiseCache[fileName] = result.promise();
         }
-        
-        return _cssDocsPromise;
+
+        return promiseCache[fileName];
     }
-    
-    
+
+
     /**
-     * Inline docs provider. Currently looks up docs based on CSS properties only.
+     * Inline docs provider.
      *
      * @param {!Editor} editor
      * @param {!{line:Number, ch:Number}} pos
      * @return {?$.Promise} resolved with an InlineWidget; null if we're not going to provide anything
      */
     function inlineProvider(hostEditor, pos) {
-        var langId = hostEditor.getLanguageForSelection().getId();
-        // Only provide docs when cursor is in CSS content
-        if (langId !== "css" && langId !== "scss" && langId !== "less") {
+        var jsonFile, propInfo,
+            propQueue = [], // priority queue of propNames to try
+            langId = hostEditor.getLanguageForSelection().getId(),
+            supportedLangs = ["css", "scss", "less", "html"],
+            langIndex = langId ? supportedLangs.indexOf(langId) : -1; // fail if langId is falsy
+
+        // Only provide docs when cursor is in supported language
+        if (langIndex < 0) {
             return null;
         }
-        
+
         // Only provide docs if the selection is within a single line
         var sel = hostEditor.getSelection();
         if (sel.start.line !== sel.end.line) {
             return null;
         }
-        
-        // Explicitly use selection start rather than pos, which is usually selection end
-        var cssInfo = CSSUtils.getInfoAtPos(hostEditor, sel.start);
-        
-        // Are we at a propety name (or in a value where name is discernible?)
-        if (cssInfo && cssInfo.name) {
-            var cssPropName = cssInfo.name,
-                result = new $.Deferred();
-            
+
+        if (langIndex <= 2) { // CSS-like language
+            jsonFile = "css.json";
+            propInfo = CSSUtils.getInfoAtPos(hostEditor, sel.start);
+            if (propInfo.name) {
+                propQueue.push("css/properties/" + propInfo.name);
+                // remove possible vendor prefixes
+                propQueue.push("css/properties/" + propInfo.name.replace(/^-(webkit|moz|ms|o)-/, ""));
+            }
+        } else { // HTML
+            jsonFile = "html.json";
+            propInfo = HTMLUtils.getTagInfo(hostEditor, sel.start);
+            if (propInfo.position.tokenType === HTMLUtils.ATTR_NAME && propInfo.attr && propInfo.attr.name) {
+                // we're on an HTML attribute (and not on its value)
+                propQueue.push("html/attributes/" + propInfo.attr.name.toLowerCase());
+            }
+            if (propInfo.tagName) { // we're somehow on an HTML tag (no matter where exactly)
+                propInfo = propInfo.tagName.toLowerCase();
+                if (/^h[1-6]$/.test(propInfo)) { // h1..h6 are expressed as "hn" in Web Platform Docs
+                    propInfo = "hn";
+                }
+                propQueue.push("html/elements/" + propInfo);
+            }
+        }
+
+        // Are we on a supported property? (no matter if info is available for the property)
+        if (propQueue.length) {
+            var result = new $.Deferred();
+
             // Load JSON file if not done yet
-            getCSSDocs()
-                .done(function (cssDocs) {
+            getDocs(jsonFile)
+                .done(function (docs) {
+                    docs = docs.PROPERTIES;
                     // Construct inline widget (if we have docs for this property)
-                    var cssPropDetails = cssDocs.PROPERTIES["css/properties/" + cssPropName];
-                    if (!cssPropDetails) {
-                        cssPropName = cssPropName.replace(/^-(webkit|moz|ms|o)-/, ""); // remove possible vendor prefixes
-                        if (cssPropName) {
-                            cssPropDetails = cssDocs.PROPERTIES["css/properties/" + cssPropName];
-                        }
+
+                    var displayName, propDetails,
+                        propName = _.find(propQueue, function (propName) { // find the first property where info is available
+                            return docs.hasOwnProperty(propName);
+                        });
+
+                    if (propName) {
+                        // strip off all prefixes from the propName
+                        displayName = propName.substr(propName.lastIndexOf("/") + 1);
+                        propDetails = docs[propName];
                     }
-                    if (cssPropDetails) {
-                        var inlineWidget = new InlineDocsViewer(cssPropName, cssPropDetails);
+                    if (propDetails) {
+                        var inlineWidget = new InlineDocsViewer(displayName, propDetails);
                         inlineWidget.load(hostEditor);
                         result.resolve(inlineWidget);
-                        
                     } else {
                         result.reject();
                     }
@@ -128,17 +161,17 @@ define(function (require, exports, module) {
                 .fail(function () {
                     result.reject();
                 });
-            
+
             return result.promise();
-            
+
         } else {
             return null;
         }
     }
-    
+
     // Register as inline docs provider
     EditorManager.registerInlineDocsProvider(inlineProvider);
 
-    exports._getCSSDocs      = getCSSDocs;
+    exports._getDocs         = getDocs;
     exports._inlineProvider  = inlineProvider;
 });
