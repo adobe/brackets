a22a0be63cdfe28e690c6835d3eab191c65d572c
Jason San Jose <jasonsj@adobe.com>
2015-01-20 14:16:33 +0530

    fix jshint error

--- src/view/ViewCommandHandlers.js
+++ src/view/ViewCommandHandlers.js
@@ -22,7 +22,7 @@
  */
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, window, $, brackets */
+/*global define, $, brackets */
 
 /**
  * The ViewCommandHandlers object dispatches the following event(s):
