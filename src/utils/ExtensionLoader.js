ffd67949fc9055422604bee7d344fe31a76abd66
Kenny Nguyen <kenimorri@gmail.com>
2015-01-26 14:01:15 -0500

    added loader for HTMLHinter submodule

--- src/utils/ExtensionLoader.js
+++ src/utils/ExtensionLoader.js
@@ -413,7 +413,8 @@ define(function (require, exports, module) {
 
                     // Custom extensions we want loaded by default
                     // NOTE: Maps to a folder inside /src/extensions/default/
-                    "makedrive-sync-icon"
+                    "makedrive-sync-icon",
+                    "HTMLHinter"
 
                     // "ExampleExtension",
                 ];
