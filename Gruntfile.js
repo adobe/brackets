aec54a62b68261cc17d3ee8d9e6b17569b748f27
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2015-03-03 12:27:57 -0500

    Split grunt copy task into copy and uglify to minify JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,42 @@ module.exports = function (grunt) {
                 }]
             }
         },
+        uglify: {
+            options: {
+                mangle: false,
+                compress: false
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: [
+                        /* static files */
+                        'nls/{,*/}*.js',
+                        'xorigin.js',
+                        'dependencies.js',
+                        'thirdparty/requirejs/require.js',
+                        'LiveDevelopment/MultiBrowserImpl/transports/**/*.js',
+                        'LiveDevelopment/MultiBrowserImpl/launchers/**/*.js',
+
+                        /* extensions and CodeMirror modes */
+                        '!extensions/default/*/unittests.js',
+                        'extensions/default/*/**/*.js',
+                        '!**/unittest-files/**',
+                        '!extensions/default/JavaScriptCodeHints/thirdparty/*/test/**/*',
+                        '!extensions/default/**/node_modules/**/*',
+                        'thirdparty/CodeMirror2/addon/{,*/}*.js',
+                        'thirdparty/CodeMirror2/keymap/{,*/}*.js',
+                        'thirdparty/CodeMirror2/lib/{,*/}*.js',
+                        'thirdparty/CodeMirror2/mode/{,*/}*.js',
+                        'thirdparty/CodeMirror2/theme/{,*/}*.js',
+                        'thirdparty/i18n/*.js',
+                        'thirdparty/text/*.js'
+                    ],
+                    dest: 'dist/'
+                }]
+            }
+        },
         copy: {
             dist: {
                 files: [
@@ -344,6 +380,9 @@ module.exports = function (grunt) {
         'usemin',
         'build-config'
     ]);
+
+    // task: build dist/ for browser
+    grunt.registerTask('build-browser', ['build', 'uglify']);
 
     // Default task.
     grunt.registerTask('default', ['test']);
