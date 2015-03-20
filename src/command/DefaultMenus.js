888ec3094e1a50141861a9a3525269c8c7ddd4a4
Prashant <prkumars@adobe.com>
2015-03-20 17:08:31 +0530

    Removing confusing menu items "Find in Selected File/Folder" and "Replace in Selected File/Folder". This fixes #10754

--- src/command/DefaultMenus.js
+++ src/command/DefaultMenus.js
@@ -110,11 +110,9 @@ define(function (require, exports, module) {
         menu.addMenuItem(Commands.CMD_SKIP_CURRENT_MATCH);
         menu.addMenuDivider();
         menu.addMenuItem(Commands.CMD_FIND_IN_FILES);
-        menu.addMenuItem(Commands.CMD_FIND_IN_SELECTED);
         menu.addMenuDivider();
         menu.addMenuItem(Commands.CMD_REPLACE);
         menu.addMenuItem(Commands.CMD_REPLACE_IN_FILES);
-        menu.addMenuItem(Commands.CMD_REPLACE_IN_SELECTED);
         
         /*
          * View menu
