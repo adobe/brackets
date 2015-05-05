8b01819505d6b5349a0664566a26be562391bad3
Martin Zagora <zaggino@gmail.com>
2015-05-05 22:42:41 +1000

    moveToTrash done

--- app/appshell/index.js
+++ app/appshell/index.js
@@ -1,6 +1,19 @@
 /*jshint globalstrict:true, node:true*/
 
+"use strict";
+
+var _ = require("lodash");
+var fs = require("fs-extra");
+var trash = require("trash");
+
+var fsAdditions = {
+    moveToTrash: function (path, callback) {
+        // trash expects an array of files which is inconsistent with fs-extra apis
+        trash(Array.isArray(path) ? path : [path], callback);
+    }
+};
+
 module.exports = {
     app: require("./app"),
-    fs: require("./fs")
+    fs: _.mixin({}, fs, fsAdditions)
 };
