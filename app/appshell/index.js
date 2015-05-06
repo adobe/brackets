a1c4c9e725fad9a4f41a12da9ddec907f85c8d45
Martin Zagora <zaggino@gmail.com>
2015-05-06 11:44:06 +1000

    implement readTextFile

--- app/appshell/index.js
+++ app/appshell/index.js
@@ -4,12 +4,19 @@
 
 var _ = require("lodash");
 var fs = require("fs-extra");
+var isbinaryfile = require("isbinaryfile");
 var trash = require("trash");
 var utils = require("../utils");
 var remote = require("remote");
 var dialog = remote.require("dialog");
 
 var fsAdditions = {
+    isBinaryFile: function (filename, callback) {
+        isbinaryfile(filename , callback);
+    },
+    isBinaryFileSync: function (filename) {
+        return isbinaryfile(filename);
+    },
     isNetworkDrive: function (path, callback) {
         // TODO: implement
         process.nextTick(function () {
@@ -19,6 +26,28 @@ var fsAdditions = {
     moveToTrash: function (path, callback) {
         // trash expects an array of files which is inconsistent with fs-extra apis
         trash(Array.isArray(path) ? path : [path], callback);
+    },
+    readTextFile: function (filename, encoding, callback) {
+        isbinaryfile(filename, function(err, isBinary) {
+            if (err) {
+                return callback(err);
+            }
+            if (isBinary) {
+                err = new Error("ECHARSET: file is a binary file");
+                err.code = "ECHARSET";
+                return callback(err);
+            }
+            fs.readFile(filename, encoding, callback);
+        });
+    },
+    readTextFileSync: function (filename, encoding) {
+        var isBinary = isbinaryfile(filename);
+        if (isBinary) {
+            var err = new Error("ECHARSET: file is a binary file");
+            err.code = "ECHARSET";
+            throw err;
+        }
+        return fs.readFileSync(filename, encoding);
     },
     showOpenDialog: function (allowMultipleSelection, chooseDirectory, title, initialPath, fileTypes, callback) {
         var properties = [];
