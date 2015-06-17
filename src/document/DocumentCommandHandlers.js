9154e7456a2e0b1c5d011fbe9021aa5517bc346c
nethip <prashant@adobe.com>
2015-06-17 16:20:39 +0530

    This has the first cut which now opens default preferences file in the left pane and user preferences file brackets.json in the right pane. Also moved the code from PreferencesManager to DebugCommands extension.
    Formatting object change
    
    Added code to format nested object preferences. Also refactored lot of code
    
    Code refactoring
    
    Adding a preference to enable/disable opening preferences in split view.
    
    Improved object formatting
    
    Fixed some unit testing defects
    
    Code cleanup and also added tests temporarily for unit tests
    
    "Fixed many unit testing defects as well as added code to close any existing instances of defaultPreferences.json before deleting the existing instance of defaultPreferences.json."
    
    "Removing extra spaces across the changes made on this branch"
    
    "More removals of white spaces"
    
    "Changed the editor options from boolean to object"
    
    "Continuation of the older commit."
    
    Fixed a unit testing defect caused by using the same editorOptions variable.
    
    "Changed the default values of some preferences default from true to false"
    
    "Fixed an issue which pops up when searching for default preferences. Changed typeof(var) == "undefined" var === undefined. And changed defaultSettings.json to defaultPreferences.json
    
    "Change the header comment as per @ryanstewart's suggestion. Also made a code change suggested by @sprintr"
    
    Incorporated some code review comments
    
    Fixing a bad commit.
    
    Added a promise, so that the caller of this command can now react to done or failure message.
    
    Incorporated lot of code review comments suggested by @MarcelGerber.
    
    Fixing the JSHint issue that failed travis build.
    
    New preferences for opening preferences along with default preferences in split view
    
    Changed for() to forEach() at two places and we are now generating the keys in a sorted order. Also added a new preference to give an opportunity to people to either display the user preferences on the right side or the left.
    
    Fixing a unit test issue while opening default preferences. This crept in with the preference change that allows users to specifiy the pane in which the user preferences need to be open. Also added default values to closeTag attribute.
    
    Some string changes along with preference name change got from code review comments from @mackenza and @rroshan1
    
    Adding documentation for additional parameters. Thanks to @rroshan1.

--- src/document/DocumentCommandHandlers.js
+++ src/document/DocumentCommandHandlers.js
@@ -449,7 +449,7 @@ define(function (require, exports, module) {
             paneId = (commandData && commandData.paneId) || MainViewManager.ACTIVE_PANE,
             result = new $.Deferred();
         
-        _doOpenWithOptionalPath(fileInfo.path, silent, paneId, commandData)
+        _doOpenWithOptionalPath(fileInfo.path, silent, paneId, commandData && commandData.options)
             .done(function (file) {
                 if (!commandData || !commandData.options || !commandData.options.noPaneActivate) {
                     MainViewManager.setActivePaneId(paneId);
