7ca494adecfb1e2fbf4b646e2f13d0bcce63c489
Praful Kumar Vaishnav <vaishnav@adobe.com>
2015-06-19 17:03:06 +0530

    Child Node Process

---
+++ src/search/node/ChildFIF.js
@@ -0,0 +1,304 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4,
+maxerr: 50, node: true */
+/*global */
+(function () {
+    "use strict";
+    
+    var fs = require('fs');
+    var projectCache = {};
+    var files;
+    var MAX_DISPLAY_LENGTH = 200,
+        MAX_TOTAL_RESULTS = 100;
+    
+    var results = {},
+        numMatches = 0,
+        foundMaximum = false,
+        exceedsMaximum = false;
+    
+/*    fs.appendFile('C://Users//vaishnav//Desktop//nodeLog.txt', "Entered Child Process", function (err) {});*/
+    function offsetToLineNum(textOrLines, offset) {
+        if (Array.isArray(textOrLines)) {
+            var lines = textOrLines,
+                total = 0,
+                line;
+            for (line = 0; line < lines.length; line++) {
+                if (total < offset) {
+                    // add 1 per line since /n were removed by splitting, but they needed to 
+                    // contribute to the total offset count
+                    total += lines[line].length + 1;
+                } else if (total === offset) {
+                    return line;
+                } else {
+                    return line - 1;
+                }
+            }
+
+            // if offset is NOT over the total then offset is in the last line
+            if (offset <= total) {
+                return line - 1;
+            } else {
+                return undefined;
+            }
+        } else {
+            return textOrLines.substr(0, offset).split("\n").length - 1;
+        }
+    }
+    
+    function _getSearchMatches(contents, queryExpr) {
+        if (!contents) {
+            return;
+        }
+        // Quick exit if not found or if we hit the limit
+        if (foundMaximum || contents.search(queryExpr) === -1) {
+            return [];
+        }
+
+        var match, lineNum, line, ch, totalMatchLength, matchedLines, numMatchedLines, lastLineLength, endCh,
+            padding, leftPadding, rightPadding, highlightOffset, highlightEndCh,
+            lines   = contents.split("\n"),
+            matches = [];
+
+        while ((match = queryExpr.exec(contents)) !== null) {
+            lineNum          = offsetToLineNum(lines, match.index);
+            line             = lines[lineNum];
+            ch               = match.index - contents.lastIndexOf("\n", match.index) - 1;  // 0-based index
+            matchedLines     = match[0].split("\n");
+            numMatchedLines  = matchedLines.length;
+            totalMatchLength = match[0].length;
+            lastLineLength   = matchedLines[matchedLines.length - 1].length;
+            endCh            = (numMatchedLines === 1 ? ch + totalMatchLength : lastLineLength);
+            highlightEndCh   = (numMatchedLines === 1 ? endCh : line.length);
+            highlightOffset  = 0;
+
+            if (highlightEndCh <= MAX_DISPLAY_LENGTH) {
+                // Don't store more than 200 chars per line
+                line = line.substr(0, Math.min(MAX_DISPLAY_LENGTH, line.length));
+            } else if (totalMatchLength > MAX_DISPLAY_LENGTH) {
+                // impossible to display the whole match
+                line = line.substr(ch, ch + MAX_DISPLAY_LENGTH);
+                highlightOffset = ch;
+            } else {
+                // Try to have both beginning and end of match displayed
+                padding = MAX_DISPLAY_LENGTH - totalMatchLength;
+                rightPadding = Math.floor(Math.min(padding / 2, line.length - highlightEndCh));
+                leftPadding = Math.ceil(padding - rightPadding);
+                highlightOffset = ch - leftPadding;
+                line = line.substring(highlightOffset, highlightEndCh + rightPadding);
+            }
+
+            matches.push({
+                start:       {line: lineNum, ch: ch},
+                end:         {line: lineNum + numMatchedLines - 1, ch: endCh},
+
+                highlightOffset: highlightOffset,
+
+                // Note that the following offsets from the beginning of the file are *not* updated if the search
+                // results change. These are currently only used for multi-file replacement, and we always
+                // abort the replace (by shutting the results panel) if we detect any result changes, so we don't
+                // need to keep them up to date. Eventually, we should either get rid of the need for these (by
+                // doing everything in terms of line/ch offsets, though that will require re-splitting files when
+                // doing a replace) or properly update them.
+                startOffset: match.index,
+                endOffset:   match.index + totalMatchLength,
+
+                line:        line,
+                result:      match,
+                isChecked:   true
+            });
+
+            // We have the max hits in just this 1 file. Stop searching this file.
+            // This fixed issue #1829 where code hangs on too many hits.
+            // Adds one over MAX_TOTAL_RESULTS in order to know if the search has exceeded
+            // or is equal to MAX_TOTAL_RESULTS. Additional result removed in SearchModel
+            if (matches.length > MAX_TOTAL_RESULTS) {
+                queryExpr.lastIndex = 0;
+                break;
+            }
+
+            // Pathological regexps like /^/ return 0-length matches. Ensure we make progress anyway
+            if (totalMatchLength === 0) {
+                queryExpr.lastIndex++;
+            }
+        }
+
+        return matches;
+    }
+    
+    function getFileContentsForFile(filePath) {
+        if (projectCache[filePath]) {
+            return projectCache[filePath];
+        }
+//        fs.readFile(filePath, function (err, data) {
+//            if (err) {
+//                console.error("Error");
+//            } else {
+//                console.log("File contents" + data);
+//            }
+//        });
+        projectCache[filePath] = fs.readFileSync(filePath, 'utf8');
+        return projectCache[filePath];
+        
+    }
+    
+    function setResults(fullpath, resultInfo) {
+        if (results[fullpath]) {
+            numMatches -= results[fullpath].matches.length;
+            delete results[fullpath];
+        }
+
+        if (foundMaximum || !resultInfo || !resultInfo.matches || !resultInfo.matches.length) {
+            return;
+        }
+
+        // Make sure that the optional `collapsed` property is explicitly set to either true or false,
+        // to avoid logic issues later with comparing values.
+        resultInfo.collapsed = !!resultInfo.collapsed;
+
+        results[fullpath] = resultInfo;
+        numMatches += resultInfo.matches.length;
+        if (numMatches >= MAX_TOTAL_RESULTS) {
+            foundMaximum = true;
+
+            // Remove final result if there have been over MAX_TOTAL_RESULTS found
+            if (numMatches > MAX_TOTAL_RESULTS) {
+                results[fullpath].matches.pop();
+                numMatches--;
+                exceedsMaximum = true;
+            }
+        }
+    }
+    
+    function _doSearchInOneFile(filepath, text, queryExpr) {
+        var matches = _getSearchMatches(text, queryExpr);
+//        if (matches.length) {
+//            debugger;
+//        }
+        setResults(filepath, {matches: matches});
+    }
+    
+    function doSearchInFiles(fileList, queryExpr) {
+        var i;
+        if (fileList.length === 0) {
+            //console.log('no files found');
+            return;
+
+        } else {
+           // var numCompleted = 0;
+           // var hasFailed = false;
+
+            for (i = 0; i < fileList.length && !foundMaximum; i++) {
+                _doSearchInOneFile(fileList[i], getFileContentsForFile(fileList[i]), queryExpr);
+            }
+//            fileList.forEach(function (filePath, i) {
+//                
+//            });
+        }
+    }
+    
+    function regexEscape(str) {
+        return str.replace(/([.?*+\^$\[\]\\(){}|\-])/g, "\\$1");
+    }
+    
+    function parseQueryInfo(queryInfo) {
+        var queryExpr;
+
+        // TODO: only major difference between this one and the one in FindReplace is that
+        // this always returns a regexp even for simple strings. Reconcile.
+        if (!queryInfo || !queryInfo.query) {
+            return {empty: true};
+        }
+
+        // For now, treat all matches as multiline (i.e. ^/$ match on every line, not the whole
+        // document). This is consistent with how single-file find works. Eventually we should add
+        // an option for this.
+        var flags = "gm";
+        if (!queryInfo.isCaseSensitive) {
+            flags += "i";
+        }
+
+        // Is it a (non-blank) regex?
+        if (queryInfo.isRegexp) {
+            try {
+                queryExpr = new RegExp(queryInfo.query, flags);
+            } catch (e) {
+                return {valid: false, error: e.message};
+            }
+        } else {
+            // Query is a plain string. Turn it into a regexp
+            queryExpr = new RegExp(regexEscape(queryInfo.query), flags);
+        }
+        return {valid: true, queryExpr: queryExpr};
+    }
+    
+    function initCache(fileList) {
+        files = fileList;
+        files.forEach(function (file) {
+            getFileContentsForFile(file);
+        });
+        return true;
+    }
+    
+    function doSearch(searchObject) {
+        //console.log("doSearch");
+        
+        if (!files) {
+            //console.log("no file object found");
+            return {};
+        }
+        results = {};
+        numMatches = 0;
+        foundMaximum = false;
+        exceedsMaximum = false;
+        var queryObject = parseQueryInfo(searchObject.queryInfo);
+        doSearchInFiles(files, queryObject.queryExpr);
+        var send_object = {
+            "results":  results,
+            "numMatches": numMatches,
+            "foundMaximum":  foundMaximum,
+            "exceedsMaximum":  exceedsMaximum
+        };
+        return send_object;
+    }
+    
+    process.on('message', function (msg) {
+// fs.appendFile('C://Users//vaishnav//Desktop//nodeLog.txt', " Received Message ", function (err) {});
+        var data;
+        if (msg.msg === "initCache") {
+            var fileList = msg.fileList;
+            var bool = initCache(fileList);
+            data = {"msg" : "cacheComplete", "result" : bool};
+            process.send(data);
+
+        } else if (msg.msg === "doSearch") {
+            var searchObject = msg.searchObject;
+            var result = doSearch(searchObject);
+            data = {"msg" : "searchComplete", "result" : result};
+            process.send(data);
+        }
+        
+    });
+    
+}());
