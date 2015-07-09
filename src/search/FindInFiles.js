bb7ab3170bbab3b585b86788d54d9c72a2314e4f
rroshan1 <roshan@adobe.com>
2015-07-10 04:40:04 +0530

    Making the page navigations faster. Showing correct last page results for beyond 100,000 results.

--- src/search/FindInFiles.js
+++ src/search/FindInFiles.js
@@ -514,8 +514,8 @@ define(function (require, exports, module) {
                             searchModel.results = rcvd_object.results;
                             searchModel.numMatches = rcvd_object.numMatches;
                             searchModel.numFiles = rcvd_object.numFiles;
-                            //searchModel.foundMaximum = rcvd_object.foundMaximum;
-                            //searchModel.exceedsMaximum = rcvd_object.exceedsMaximum;
+//                            searchModel.foundMaximum = rcvd_object.foundMaximum;
+                            searchModel.exceedsMaximum = rcvd_object.exceedsMaximum;
                             searchModel.allResultsAvailable = rcvd_object.allResultsAvailable;
                             searchDeferred.resolve();
                         });
@@ -818,13 +818,13 @@ define(function (require, exports, module) {
                 } else {
                     searchModel.results = rcvd_object.results;
                 }
-                searchModel.numMatches = rcvd_object.numMatches;
-                if (rcvd_object.numMatches === 0) {
-                    searchModel.allResultsAvailable = true;
-                }
+//                searchModel.numMatches = rcvd_object.numMatches;
+//                if (rcvd_object.numMatches === 0) {
+//                    searchModel.allResultsAvailable = true;
+//                }
                 //searchModel.foundMaximum = rcvd_object.foundMaximum;
                 //searchModel.exceedsMaximum = rcvd_object.exceedsMaximum;
-                searchModel.numFiles = rcvd_object.numFiles;
+//                searchModel.numFiles = rcvd_object.numFiles;
                 searchModel.fireChanged();
                 searchDeferred.resolve();
             });
