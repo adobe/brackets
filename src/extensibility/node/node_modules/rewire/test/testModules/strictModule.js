a294b34a0d1070fc1e476b44c859b1800988f0b2
Marcel Gerber <mg.hain@gmx.de>
2015-08-11 20:29:10 +0200

    Remove cruft from extensibility's node_modules

--- src/extensibility/node/node_modules/rewire/test/testModules/strictModule.js
+++
@@ -1,7 +0,0 @@
-"use strict"; // run code in ES5 strict mode
-
-function doSomethingUnstrict() {
-    var caller = arguments.callee.caller;   // this should throw an error as a proof that strict mode is on
-}
-
-exports.doSomethingUnstrict = doSomethingUnstrict;
