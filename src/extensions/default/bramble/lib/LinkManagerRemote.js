805489ea7291ff950ede77ed2f3d66c79070c309
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2015-10-16 19:08:28 -0400

    Fix https://github.com/mozilla/thimble.webmaker.org/issues/1182 - allow javascript: and other common non-http schemes

--- src/extensions/default/bramble/lib/LinkManagerRemote.js
+++ src/extensions/default/bramble/lib/LinkManagerRemote.js
@@ -6,7 +6,13 @@
     function handleClick(e) {
         var url = e.target.getAttribute("href");
 
-        // For local paths vs. absolute URLs, try to open the right file
+        // For local paths vs. absolute URLs, try to open the right file.
+        // Special case (i.e., pass through) some common, non-http(s) protocol
+        // schemes so they work as expected.
+        if(/^(javascript|mailto|data|blob):/.test(url)) {
+            return true;
+        }
+
         if(!(/\:?\/\//.test(url)) && window._Brackets_LiveDev_Transport) {
             window._Brackets_LiveDev_Transport.send("bramble-navigate:" + url);
         } else {
