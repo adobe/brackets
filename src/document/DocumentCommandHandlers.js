b08c32bff821ce2b322b6c20561f60a62ebffcae
manoharprabhu <manoharagent13@gmail.com>
2015-10-29 23:44:54 +0530

    Fix for #11763 The new created file cannot be renamed/showed in file tree/showed in explorer with no messages if it isn't saved.
    The file is checked if it is an InMemoryFile before attempting to perform
    the above mentioned operations on it. If it is indeed not saved, then a
    message is shown to the user to save the document to proceed. Upon
    clicking 'Ok', the save dialog box opens up allowing the user to save the
    file.

--- src/document/DocumentCommandHandlers.js
+++ src/document/DocumentCommandHandlers.js
@@ -271,6 +271,19 @@ define(function (require, exports, module) {
             )
         );
     }
+    
+    /**
+    * Shows an error dialog indicating that the file needs to be saved before performing any further operation.  
+    * This will be shown when the user tries to 'Rename', 'Show in explorer', or 'Show in file tree' a freshly
+    * created file without saving it
+    */
+    function showSaveBeforeProceedError() {
+        return Dialogs.showModalDialog(
+            DefaultDialogs.DIALOG_ID_ERROR,
+            Strings.ERROR_PERFORMING_OPERATION,
+            Strings.SAVE_FILE_BEFORE_PROCEEDING
+        );
+    }
 
     /**
      * @private
@@ -1428,7 +1441,13 @@ define(function (require, exports, module) {
             entry = MainViewManager.getCurrentlyViewedFile();
         }
         if (entry) {
-            ProjectManager.renameItemInline(entry);
+            if (entry instanceof InMemoryFile) {
+                showSaveBeforeProceedError().done(function () {
+                    handleFileSave(entry);
+                });
+            } else {
+                ProjectManager.renameItemInline(entry);
+            }
         }
     }
 
@@ -1516,7 +1535,14 @@ define(function (require, exports, module) {
 
     /** Show in File Tree command handler **/
     function handleShowInTree() {
-        ProjectManager.showInTree(MainViewManager.getCurrentlyViewedFile(MainViewManager.ACTIVE_PANE));
+        var entry = MainViewManager.getCurrentlyViewedFile(MainViewManager.ACTIVE_PANE);
+        if (entry instanceof InMemoryFile) {
+            showSaveBeforeProceedError().done(function () {
+                handleFileSave(entry);
+            });
+        } else {
+            ProjectManager.showInTree(entry);
+        }
     }
 
     /** Delete file command handler  **/
@@ -1557,11 +1583,17 @@ define(function (require, exports, module) {
     function handleShowInOS() {
         var entry = ProjectManager.getSelectedItem();
         if (entry) {
-            brackets.app.showOSFolder(entry.fullPath, function (err) {
-                if (err) {
-                    console.error("Error showing '" + entry.fullPath + "' in OS folder:", err);
-                }
-            });
+            if (entry instanceof InMemoryFile) {
+                showSaveBeforeProceedError().done(function () {
+                    handleFileSave(entry);
+                });
+            } else {
+                brackets.app.showOSFolder(entry.fullPath, function (err) {
+                    if (err) {
+                        console.error("Error showing '" + entry.fullPath + "' in OS folder:", err);
+                    }
+                });
+            }
         }
     }
 
