c49028dba05ac8e58eaa9aa093770ed739d23bf0
Martin Zagora <zaggino@gmail.com>
2015-12-15 09:59:10 +1100

    indent the code to two spaces

--- src/extensions/default/ESLint/domain.js
+++ src/extensions/default/ESLint/domain.js
@@ -2,131 +2,131 @@
 /*global require, exports*/
 
 (function () {
-  'use strict';
+    'use strict';
 
-  var fs = require('fs');
-  var CLIEngine = require('eslint').CLIEngine;
-  var cli = new CLIEngine();
-  var currentProjectRoot = null;
-  var domainName = 'brackets-eslint';
-  var domainManager = null;
-  var noop = function () {};
-  var globalPackagesAvailable = false;
+    var fs = require('fs');
+    var CLIEngine = require('eslint').CLIEngine;
+    var cli = new CLIEngine();
+    var currentProjectRoot = null;
+    var domainName = 'brackets-eslint';
+    var domainManager = null;
+    var noop = function () {};
+    var globalPackagesAvailable = false;
 
-  function _setProjectRoot(projectRoot) {
-    var opts = { useEslintrc: true };
-    var configPath;
-    var rulesDirPath;
-    var ignorePath;
+    function _setProjectRoot(projectRoot) {
+        var opts = { useEslintrc: true };
+        var configPath;
+        var rulesDirPath;
+        var ignorePath;
 
-    if (projectRoot) {
-      configPath = projectRoot + '.eslintrc';
-      try {
-        if (fs.statSync(configPath).isFile()) {
-          noop();
+        if (projectRoot) {
+            configPath = projectRoot + '.eslintrc';
+            try {
+                if (fs.statSync(configPath).isFile()) {
+                    noop();
+                }
+            } catch (e) {
+                // config file not found, use default
+                opts.rules = require('eslint/conf/eslint.json').rules;
+            }
+
+            rulesDirPath = projectRoot + '.eslintrules';
+            try {
+                if (fs.statSync(rulesDirPath).isDirectory()) {
+                    opts.rulePaths = [rulesDirPath];
+                }
+            } catch (e) {
+                // no action required
+                noop(e);
+            }
+
+            ignorePath = projectRoot + '.eslintignore';
+            try {
+                if (fs.statSync(ignorePath).isFile()) {
+                    opts.ignore = true;
+                    opts.ignorePath = ignorePath;
+                }
+            } catch (e) {
+                // no action required
+                noop(e);
+            }
         }
-      } catch (e) {
-        // config file not found, use default
-        opts.rules = require('eslint/conf/eslint.json').rules;
-      }
 
-      rulesDirPath = projectRoot + '.eslintrules';
-      try {
-        if (fs.statSync(rulesDirPath).isDirectory()) {
-          opts.rulePaths = [rulesDirPath];
-        }
-      } catch (e) {
-        // no action required
-        noop(e);
-      }
-
-      ignorePath = projectRoot + '.eslintignore';
-      try {
-        if (fs.statSync(ignorePath).isFile()) {
-          opts.ignore = true;
-          opts.ignorePath = ignorePath;
-        }
-      } catch (e) {
-        // no action required
-        noop(e);
-      }
+        cli = new CLIEngine(opts);
     }
 
-    cli = new CLIEngine(opts);
-  }
-
-  require('enable-global-packages').on('ready', function () {
-    // global packages are available now
-    _setProjectRoot(currentProjectRoot);
-    globalPackagesAvailable = true;
-  });
-
-  function lintFile(fullPath, projectRoot, callback) {
-    if (!globalPackagesAvailable) {
-      setTimeout(function () {
-        lintFile(fullPath, projectRoot, callback);
-      }, 250);
-      return;
-    }
-    if (projectRoot !== currentProjectRoot) {
-      _setProjectRoot(projectRoot);
-      currentProjectRoot = projectRoot;
-    }
-    fs.readFile(fullPath, {encoding: 'utf8'}, function (err, text) {
-      if (err) {
-        return callback(err);
-      }
-      var relativePath = fullPath.indexOf(projectRoot) === 0 ? fullPath.substring(projectRoot.length) : fullPath;
-
-      // this is important for ESLint so .eslintrc is properly loaded
-      // we could go around this by parsing .eslintrc manually but that'd
-      // bring complexity we don't need here right now
-      // related: https://github.com/eslint/eslint/issues/4472
-      process.chdir(projectRoot);
-
-      var res;
-      try {
-        res = cli.executeOnText(text, relativePath);
-      } catch (e) {
-        err = e.toString();
-      }
-      callback(err, res);
+    require('enable-global-packages').on('ready', function () {
+        // global packages are available now
+        _setProjectRoot(currentProjectRoot);
+        globalPackagesAvailable = true;
     });
-  }
 
-  exports.init = function (_domainManager) {
-    domainManager = _domainManager;
+    function lintFile(fullPath, projectRoot, callback) {
+        if (!globalPackagesAvailable) {
+            setTimeout(function () {
+                lintFile(fullPath, projectRoot, callback);
+            }, 250);
+            return;
+        }
+        if (projectRoot !== currentProjectRoot) {
+            _setProjectRoot(projectRoot);
+            currentProjectRoot = projectRoot;
+        }
+        fs.readFile(fullPath, {encoding: 'utf8'}, function (err, text) {
+            if (err) {
+                return callback(err);
+            }
+            var relativePath = fullPath.indexOf(projectRoot) === 0 ? fullPath.substring(projectRoot.length) : fullPath;
 
-    if (!domainManager.hasDomain(domainName)) {
-      domainManager.registerDomain(domainName, {
-        major: 0,
-        minor: 1
-      });
+            // this is important for ESLint so .eslintrc is properly loaded
+            // we could go around this by parsing .eslintrc manually but that'd
+            // bring complexity we don't need here right now
+            // related: https://github.com/eslint/eslint/issues/4472
+            process.chdir(projectRoot);
+
+            var res;
+            try {
+                res = cli.executeOnText(text, relativePath);
+            } catch (e) {
+                err = e.toString();
+            }
+            callback(err, res);
+        });
     }
 
-    domainManager.registerCommand(
-      domainName,
-      'lintFile', // command name
-      lintFile, // handler function
-      true, // is async
-      'lint given file with eslint', // description
-      [
-        {
-          name: 'fullPath',
-          type: 'string'
-        },
-        {
-          name: 'projectRoot',
-          type: 'string'
-        }
-      ], [
-        {
-          name: 'report',
-          type: 'object'
-        }
-      ]
-    );
+    exports.init = function (_domainManager) {
+        domainManager = _domainManager;
 
-  };
+        if (!domainManager.hasDomain(domainName)) {
+            domainManager.registerDomain(domainName, {
+                major: 0,
+                minor: 1
+            });
+        }
+
+        domainManager.registerCommand(
+            domainName,
+            'lintFile', // command name
+            lintFile, // handler function
+            true, // is async
+            'lint given file with eslint', // description
+            [
+                {
+                    name: 'fullPath',
+                    type: 'string'
+                },
+                {
+                    name: 'projectRoot',
+                    type: 'string'
+                }
+            ], [
+                {
+                    name: 'report',
+                    type: 'object'
+                }
+            ]
+        );
+
+    };
 
 }());
--- src/extensions/default/ESLint/main.js
+++ src/extensions/default/ESLint/main.js
@@ -1,68 +1,68 @@
 /*global $, brackets, define*/
 
 define(function (require, exports, module) {
-  'use strict';
+    'use strict';
 
-  // imports
-  var CodeInspection = brackets.getModule('language/CodeInspection');
-  var LanguageManager = brackets.getModule('language/LanguageManager');
-  var ProjectManager = brackets.getModule('project/ProjectManager');
-  var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
-  var NodeDomain = brackets.getModule('utils/NodeDomain');
+    // imports
+    var CodeInspection = brackets.getModule('language/CodeInspection');
+    var LanguageManager = brackets.getModule('language/LanguageManager');
+    var ProjectManager = brackets.getModule('project/ProjectManager');
+    var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
+    var NodeDomain = brackets.getModule('utils/NodeDomain');
 
-  // constants
-  var JS_LANGUAGE = LanguageManager.getLanguageForExtension('js');
-  var LINTER_NAME = 'ESLint';
-  var nodeDomain = new NodeDomain('brackets-eslint', ExtensionUtils.getModulePath(module, 'domain'));
+    // constants
+    var JS_LANGUAGE = LanguageManager.getLanguageForExtension('js');
+    var LINTER_NAME = 'ESLint';
+    var nodeDomain = new NodeDomain('brackets-eslint', ExtensionUtils.getModulePath(module, 'domain'));
 
-  // this will map ESLint output to match format expected by Brackets
-  function remapResults(results) {
-    return {
-      errors: results.map(function (result) {
-        var message = result.message;
-        if (result.ruleId) {
-          message += ' [' + result.ruleId + ']';
-        }
+    // this will map ESLint output to match format expected by Brackets
+    function remapResults(results) {
         return {
-          message: message,
-          pos: {
-            line: result.line - 1,
-            ch: result.column
-          },
-          type: result.ruleId
+            errors: results.map(function (result) {
+                var message = result.message;
+                if (result.ruleId) {
+                    message += ' [' + result.ruleId + ']';
+                }
+                return {
+                    message: message,
+                    pos: {
+                        line: result.line - 1,
+                        ch: result.column
+                    },
+                    type: result.ruleId
+                };
+            })
         };
-      })
-    };
-  }
+    }
 
-  function handleLintSync(text, fullPath) {
-    throw new Error('ESLint sync is not available, use async for ' + fullPath);
-  }
+    function handleLintSync(text, fullPath) {
+        throw new Error('ESLint sync is not available, use async for ' + fullPath);
+    }
 
-  function handleLintAsync(text, fullPath) {
-    var deferred = new $.Deferred();
-    var projectRoot = ProjectManager.getProjectRoot().fullPath;
+    function handleLintAsync(text, fullPath) {
+        var deferred = new $.Deferred();
+        var projectRoot = ProjectManager.getProjectRoot().fullPath;
 
-    nodeDomain.exec('lintFile', fullPath, projectRoot)
-      .then(function (report) {
-        if (report.results.length > 1) {
-          console.warn('ESLint returned multiple results, where only one set was expected');
-        }
-        var results = report.results[0];
-        var remapped = remapResults(results.messages);
-        deferred.resolve(remapped);
-      }, function (err) {
-        deferred.reject(err);
-      });
+        nodeDomain.exec('lintFile', fullPath, projectRoot)
+            .then(function (report) {
+                if (report.results.length > 1) {
+                    console.warn('ESLint returned multiple results, where only one set was expected');
+                }
+                var results = report.results[0];
+                var remapped = remapResults(results.messages);
+                deferred.resolve(remapped);
+            }, function (err) {
+                deferred.reject(err);
+            });
 
-    return deferred.promise();
-  }
+        return deferred.promise();
+    }
 
-  // register a linter with CodeInspection
-  CodeInspection.register(JS_LANGUAGE.getId(), {
-    name: LINTER_NAME,
-    scanFile: handleLintSync,
-    scanFileAsync: handleLintAsync
-  });
+    // register a linter with CodeInspection
+    CodeInspection.register(JS_LANGUAGE.getId(), {
+        name: LINTER_NAME,
+        scanFile: handleLintSync,
+        scanFileAsync: handleLintAsync
+    });
 
 });
