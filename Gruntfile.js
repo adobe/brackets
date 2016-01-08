baefebb2cc3886a22b3e8a758dde4010d3268653
Gideon Thomas <gideon@mozillafoundation.org>
2016-01-08 15:01:54 -0500

    Fix #513 - Remove phantomjs and jasmine deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -577,7 +577,6 @@ module.exports = function (grunt) {
     // task: build
     grunt.registerTask('build', [
         'jshint:src',
-        'jasmine',
         'clean',
         'less',
         'postcss',
