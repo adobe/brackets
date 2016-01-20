adobe/brackets

commit 305fb223ec4b8658f1b8ad133dad7ea4ede0e373

Fix "Cannot read property 'send' of undefined" when clicking in a tutorial

src/extensions/default/bramble/nohost/HTMLServer.js
@@ -13,8 +13,9 @@ define(function (require, exports, module) {
         CSSRewriter             = brackets.getModule("filesystem/impls/filer/lib/CSSRewriter");
 
     var Compatibility           = require("lib/compatibility"),
-        MouseManager           = require("lib/MouseManager"),
-        LinkManager             = require("lib/LinkManager");
+        MouseManager            = require("lib/MouseManager"),
+        LinkManager             = require("lib/LinkManager"),
+        Tutorial                = require("lib/Tutorial");
 
     var fs = Filer.fs(),
         _shouldUseBlobURL;
@@ -151,7 +152,8 @@ define(function (require, exports, module) {
                 // Since we're not instrumenting this doc fully for some reason,
                 // at least inject the scroll manager so we can track scroll position.
                 body = body.replace(/<\/\s*head>/,
-                    MouseManager.getRemoteScript(path) + LinkManager.getRemoteScript() + "$&");
+                    (Tutorial.getOverride() ? "" : MouseManager.getRemoteScript(path)) +
+                    LinkManager.getRemoteScript() + "$&");
                 serve(body);
             });
         }

commit 7b11c1a2802886c886045de4859fffbdce799b5d

Fix mozilla/thimble.webmaker.org#1340 - Fix regression caused by 305fb22 which forgets scroll position

src/extensions/default/bramble/lib/MouseManagerRemote.js
@@ -66,6 +66,12 @@
         // Restore last scroll position for this session (if any)
         document.body.scrollTop = sessionStorage.getItem(SCROLL_KEY)|0;
 
+        // Do not listen for mouse events relevant for the inspector
+        // if there is no livedoc
+        if(!transport) {
+            return;
+        }
+
         startListener();
 
         // If the user clicks on an element, stop inspecting with mousemove (pin editor)

src/extensions/default/bramble/nohost/HTMLServer.js
@@ -14,8 +14,7 @@ define(function (require, exports, module) {
 
     var Compatibility           = require("lib/compatibility"),
         MouseManager            = require("lib/MouseManager"),
-        LinkManager             = require("lib/LinkManager"),
-        Tutorial                = require("lib/Tutorial");
+        LinkManager             = require("lib/LinkManager");
 
     var fs = Filer.fs(),
         _shouldUseBlobURL;
@@ -152,7 +151,7 @@ define(function (require, exports, module) {
                 // Since we're not instrumenting this doc fully for some reason,
                 // at least inject the scroll manager so we can track scroll position.
                 body = body.replace(/<\/\s*head>/,
-                    (Tutorial.getOverride() ? "" : MouseManager.getRemoteScript(path)) +
+                    MouseManager.getRemoteScript(path) +
                     LinkManager.getRemoteScript() + "$&");
                 serve(body);
             });