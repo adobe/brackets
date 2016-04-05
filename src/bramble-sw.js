9f79940f915ca08cb49476790f2e589214595704
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2016-04-05 19:08:37 -0400

    Fix #546 - Use sw-precache to make Bramble offline ready
    Update docs
    
    Review fixes

---
+++ src/bramble-sw.js
@@ -0,0 +1,7 @@
+/**
+ * This is a service worker stub, meant only to allow development builds
+ * to load properly. The actual bramble-sw.js file is generated at build
+ * time, see Gruntfile and swPrecache task.
+ */
+
+console.log("[Bramble] Service Worker cache not running in development.");
