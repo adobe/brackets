618d49e7d293e6b9ac5f944b7086df78b0d1b635
Marcel Gerber <mg.hain@gmx.de>
2016-08-01 15:02:36 +0200

    Immediately render the tree on panel resize

--- src/project/ProjectManager.js
+++ src/project/ProjectManager.js
@@ -1166,7 +1166,7 @@ define(function (require, exports, module) {
      */
     function _setFileTreeSelectionWidth(width) {
         model.setSelectionWidth(width);
-        _renderTree();
+        _renderTreeSync();
     }
 
     // Initialize variables and listeners that depend on the HTML DOM
