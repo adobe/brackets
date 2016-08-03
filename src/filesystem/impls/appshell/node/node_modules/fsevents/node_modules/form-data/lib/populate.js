a34d92662fe79b4545a0488b0c08bffa0e015331
Martin Zagora <zaggino@gmail.com>
2016-08-04 09:38:48 +1000

    do not commit node_modules to the project

--- src/filesystem/impls/appshell/node/node_modules/fsevents/node_modules/form-data/lib/populate.js
+++
@@ -1,9 +0,0 @@
-// populates missing values
-module.exports = function(dst, src) {
-  for (var prop in src) {
-    if (src.hasOwnProperty(prop) && !dst[prop]) {
-      dst[prop] = src[prop];
-    }
-  }
-  return dst;
-};
