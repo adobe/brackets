7733f4a41905d43b8ae95e8ebf99fab861263208
Martin Zagora <zaggino@gmail.com>
2016-08-04 14:26:34 +1000

    propagate ignores from FileSystem.js

--- src/filesystem/FileSystem.js
+++ src/filesystem/FileSystem.js
@@ -274,7 +274,7 @@ define(function (require, exports, module) {
         var impl = this._impl,
             recursiveWatch = impl.recursiveWatch,
             commandName = shouldWatch ? "watchPath" : "unwatchPath",
-            ignored = undefined;// PreferencesManager.get("fileWatchers.ignored");
+            ignored = ["**/.git/**", "**/.svn/**", "**/bower_components/**", "**/node_modules/**"];
 
         if (recursiveWatch) {
             // The impl can watch the entire subtree with one call on the root (we also fall into this case for
