30bbf5febda7a0e6683f8cba9f81bce891863fb3
Amr El-Naggar <amr@plots.io>
2016-08-27 01:40:51 +0200

    Use strict equal of length of added and removed sets against zero
    Addresses code review comments

--- src/search/FindInFiles.js
+++ src/search/FindInFiles.js
@@ -795,7 +795,7 @@ define(function (require, exports, module) {
         
         var addPromise;
         if (entry.isDirectory) {
-            if (!added || !removed || (!added.length && !removed.length)) {
+            if (!added || !removed || (added.length === 0 && removed.length === 0)) {
                 // If the added or removed sets are null, must redo the search for the entire subtree - we
                 // don't know which child files/folders may have been added or removed.
                 _removeSearchResultsForEntry(entry);
