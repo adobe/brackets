469748561bb018bd3d201feea1c7487e887b9933
Chen-Heng Chang <wandererm@gmail.com>
2016-10-30 04:00:40 -0500

    fix InlineImageViewer example extension (#9191)
    * fix InlineImageViewer example extension
    
    * separate style of inline image viewer

--- src/extensions/samples/InlineImageViewer/main.js
+++ src/extensions/samples/InlineImageViewer/main.js
@@ -28,6 +28,7 @@ define(function (require, exports, module) {
 
     // Brackets modules
     var EditorManager           = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils          = brackets.getModule("utils/ExtensionUtils"),
         ProjectManager          = brackets.getModule("project/ProjectManager");
 
     // Local modules
@@ -45,7 +46,7 @@ define(function (require, exports, module) {
 
         // If the pos is at the beginning of a name, token will be the
         // preceding whitespace or dot. In that case, try the next pos.
-        if (!/\S/.match(token.string) || token.string === ".") {
+        if (!/\S/.test(token.string) || token.string === ".") {
             token = hostEditor._codeMirror.getTokenAt({line: pos.line, ch: pos.ch + 1}, true);
         }
 
@@ -128,5 +129,6 @@ define(function (require, exports, module) {
         return result.promise();
     }
 
+    ExtensionUtils.loadStyleSheet(module, "style.css");
     EditorManager.registerInlineEditProvider(inlineImageViewerProvider);
 });
