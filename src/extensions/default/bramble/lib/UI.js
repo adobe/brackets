b987dfb7934bc6282b63912afcdb9d5a6fd8832a
Dmytro Sych <dmytrosych834@gmail.com>
2017-02-09 20:10:37 -0500

    changed first pane width to be percentage of the total width

--- src/extensions/default/bramble/lib/UI.js
+++ src/extensions/default/bramble/lib/UI.js
@@ -102,6 +102,17 @@ define(function (require, exports, module) {
                 SidebarView.hide();
             }
         }
+        var secondPaneWidth = BrambleStartupState.ui("secondPaneWidth");
+        var firstPaneWidth = BrambleStartupState.ui("firstPaneWidth");
+                         
+        firstPaneWidth = firstPaneWidth * 100 / (
+                         ((firstPaneWidth)? firstPaneWidth : 0) +
+                         ((sidebarWidth)? sidebarWidth : 0) +
+                         ((secondPaneWidth)? secondPaneWidth : 0)); // calculate width in %
+        
+        if(firstPaneWidth)
+            $("#first-pane").width(firstPaneWidth + "%");
+        
 
         var fontSize = BrambleStartupState.ui("fontSize");
         if(fontSize && /\d+px/.test(fontSize)) {
