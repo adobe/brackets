c5cf0d5d8ef1c452b6d5e091e0e7421205bce875
Martin Zagora <zaggino@gmail.com>
2017-03-02 12:48:32 +1100

    sort the rules in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,44 @@
 module.exports = {
     "rules": {
-        "no-bitwise": 2,
+        // the rules below should be sorted in a same way they are sorted on http://eslint.org/docs/rules page
+        // http://eslint.org/docs/rules/#possible-errors
+        "no-caller": 2,
+        "no-control-regex": 2,
+        "no-empty": 1,
+        "no-invalid-regexp": 2,
+        "no-regex-spaces": 2,
+        "valid-jsdoc": 0,
+        "valid-typeof": 2,
+        // http://eslint.org/docs/rules/#best-practices
         "curly": 2,
-        "eqeqeq": 2,
+        "eqeqeq": [2, "smart"],
         "guard-for-in": 0,
+        "no-else-return": 1,
+        "no-fallthrough": 2,
+        "no-invalid-this": 1,
+        "no-iterator": 2,
+        "no-loop-func": 2,
+        "no-multi-str": 2,
+        "no-new-func": 2,
+        "no-new-wrappers": 2,
+        "no-new": 2,
+        "no-proto": 2,
+        "no-script-url": 2,
         "wrap-iife": [2, "outside"],
+        // http://eslint.org/docs/rules/#strict-mode
+        "strict": 2,
+        // http://eslint.org/docs/rules/#variables
+        "no-shadow-restricted-names": 2,
+        "no-shadow": 1,
+        "no-undef": 2,
+        "no-unused-vars": [1, {"vars": "all", "args": "none"}],
         "no-use-before-define": 0,
+        // http://eslint.org/docs/rules/#nodejs-and-commonjs
+        "no-new-require": 2,
+        // http://eslint.org/docs/rules/#stylistic-issues
+        "eol-last": 1,
+        "indent": [1, 4],
+        "max-len": [1, 120],
         "new-cap": [0, {
             "capIsNewExceptions": [
                 "$.Deferred",
@@ -14,60 +47,27 @@ module.exports = {
                 "Immutable.Map",
                 "Immutable.List",
                 "JSLINT"
-            ]}],
-        "no-caller": 2,
-        "no-empty": 0,
-        "no-new": 2,
-        "no-invalid-regexp": 2,
-        "no-control-regex": 2,
-        "no-regex-spaces": 2,
-        "no-undef": 2,
-        "strict": 2,
-        "no-unused-vars": [0, {"vars": "all", "args": "none"}],
-        "semi": 2,
-
-        "no-iterator": 2,
-        "no-loop-func": 2,
-        "no-multi-str": 2,
-        "no-fallthrough": 2,
-        "no-proto": 2,
-        "no-script-url": 2,
-        "no-shadow": 0,
-        "no-shadow-restricted-names": 2,
-        "no-new-func": 2,
-        "no-new-wrappers": 2,
-        "no-new-require": 2,
+            ]
+        }],
         "new-parens": 2,
+        "no-bitwise": 2,
         "no-new-object": 2,
-        "no-invalid-this": 0,
-        "indent": [0, 4],
-
-        "valid-jsdoc": 0,
-        "valid-typeof": 2,
-
-        "no-trailing-spaces": 0,
-        "eol-last": 0,
-        "max-len": [1, 120],
-
-        // the rules below should be sorted in a same way they are sorted on http://eslint.org/docs/rules page
-        // http://eslint.org/docs/rules/#best-practices
-        "no-else-return": 1
+        "no-trailing-spaces": 1,
+        "semi-spacing": 1,
+        "semi": 2
     },
     "globals": {
-        "brackets": false,
-
-        "require": false,
-        "define": false,
         "$": false,
-
-        "window": false,
-        "console": false,
-        "setTimeout": false,
+        "brackets": false,
         "clearTimeout": false,
-
+        "console": false,
+        "define": false,
+        "require": false,
+        "setTimeout": false,
+        "window": false,
         "ArrayBuffer": false,
-        "XMLHttpRequest": false,
         "Uint32Array": false,
-        "WebSocket": false
+        "WebSocket": false,
+        "XMLHttpRequest": false
     }
 };
