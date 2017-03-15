18824346b8438ac4ea8dfaaf2286b0aa996ccf77
Martin Zagora <zaggino@gmail.com>
2017-03-15 18:48:06 +1100

    Zaggino/update extension manager deps (#12933)
    * delete node_modules from extensibility/node
    
    * add dependencies from extensibility/node to main package.json
    
    * update outdated deps
    
    * use webpack to bundle semver which doesnt provide amd build
    
    * update config.json
    
    * update webpack to latest

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,7 +341,7 @@ module.exports = function (grunt) {
     });
 
     // task: install
-    grunt.registerTask('install', ['write-config:dev', 'less', 'npm-install-source']);
+    grunt.registerTask('install', ['write-config:dev', 'less', 'npm-install-source', 'pack-web-dependencies']);
 
     // task: test
     grunt.registerTask('test', ['eslint', 'jasmine', 'nls-check']);
--- src/extensibility/node/node_modules/rewire/test/testModules/someOtherModule.js
+++
@@ -1,6 +0,0 @@
-"use strict"; // run code in ES5 strict mode
-
-__filename = "/test/testModules/someOtherModule.js";
-
-exports.fs = {};
-exports.filename = __filename;
