7af7bc8bf1cb90edfc0f635636da50dfd7598dce
Justin Russo <j.russo@outlook.com>
2017-03-28 12:36:17 -0400

    Style code (cleanup)

--- src/extensions/default/JavaScriptCodeHints/HintUtils.js
+++ src/extensions/default/JavaScriptCodeHints/HintUtils.js
@@ -29,7 +29,7 @@ define(function (require, exports, module) {
     var Acorn                       = require("node_modules/acorn/dist/acorn");
 
     var LANGUAGE_ID                 = "javascript",
-		JSX_LANGUAGE_ID             = "jsx",
+        JSX_LANGUAGE_ID             = "jsx",
         HTML_LANGUAGE_ID            = "html",
         PHP_LANGUAGE_ID             = "php",
         SUPPORTED_LANGUAGES         = [LANGUAGE_ID, JSX_LANGUAGE_ID, HTML_LANGUAGE_ID, PHP_LANGUAGE_ID],
