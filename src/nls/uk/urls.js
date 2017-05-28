4571f751d2f1f8570bde14cfaa6501f1bfe673e5
yarik-vv <yarik.vashchuk@gmail.com>
2017-05-28 21:38:25 +0300

    added path for getting started page in ukrainian language

--- src/nls/uk/urls.js
+++ src/nls/uk/urls.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 - present Adobe Systems Incorporated. All rights reserved.
+ * Copyright (c) 2012 - present Adobe Systems Incorporated. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -21,38 +21,9 @@
  *
  */
 
-define(function (require, exports, module) {
-
-    "use strict";
-
-    // Registry for languages that have specific per-language URLs or file paths that we use
-    // elsewhere in Brackets.
-    //
-    // TODO: dynamically populate the local prefix list below?
-    module.exports = {
-        root: true,
-        "bg": true,
-        "cs": true,
-        "da": true,
-        "de": true,
-        "es": true,
-        "fa-ir": true,
-        "fi": true,
-        "fr": true,
-        "hr": true,
-        "id": true,
-        "it": true,
-        "ja": true,
-        "ko": true,
-        "nb": true,
-        "pl": true,
-        "pt-br": true,
-        "pt-pt": true,
-        "ru": true,
-        "sv": true,
-        "zh-cn": true,
-        "zh-tw": true,
-        "tr": true,
-        "uk": true
-    };
+define({
+    // Relative to the samples folder
+    "GETTING_STARTED"           : "uk/Починаємо роботу",
+    "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty_ru/",
+    "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.ru"
 });
