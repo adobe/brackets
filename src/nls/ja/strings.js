581070988fe40b29efb120829dca9f97da97c81c
walf <walf@adobe.com>
2017-06-16 09:43:00 -0700

    Updated by ALF automation.

--- src/nls/ja/strings.js
+++ src/nls/ja/strings.js
@@ -801,5 +801,5 @@ define({
 	"DESCRIPTION_INDENT_LINE_COMMENT": "行コメントのインデントを有効にするには true",
 	"DESCRIPTION_RECENT_FILES_NAV": "最近使用したファイルのナビゲーションを有効化/無効化",
 	"DESCRIPTION_LIVEDEV_WEBSOCKET_PORT": "ライブプレビューで WebSocket サーバーが実行されるポート",
-	"DESCRIPTION_LIVE_DEV_HIGHLIGHT_SETTINGS": "LivePreview のハイライト設定"
+	"DESCRIPTION_LIVE_DEV_HIGHLIGHT_SETTINGS": "ライブプレビューハイライト設定"
 });
