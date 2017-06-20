50de2f6858927100d0315871436d00ae02066673
rex nathan <593626202@qq.com>
2017-06-20 10:06:50 +0800

    Update urls.js

--- src/nls/zh-cn/urls.js
+++ src/nls/zh-cn/urls.js
@@ -23,6 +23,7 @@
 
 define({
     // Relative to the samples folder
+    "GETTING_STARTED"           : "zh-cn/Getting Started",
     "ADOBE_THIRD_PARTY"         : "http://www.adobe.com/go/thirdparty/",
     "WEB_PLATFORM_DOCS_LICENSE" : "http://creativecommons.org/licenses/by/3.0/deed.zh"
 });
