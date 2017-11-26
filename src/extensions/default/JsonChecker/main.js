767d555ea53428f2940772033b923a60703197ad
Ana Santos <anaezes@gmail.com>
2017-11-26 14:13:33 +0000

    extensions/default/JsonChecker: handling of asynchronous calls to parse json files

--- src/extensions/default/JsonChecker/main.js
+++ src/extensions/default/JsonChecker/main.js
@@ -25,18 +25,48 @@
     // Function to run when the menu item is clicked
     var FileSystem = brackets.getModule("filesystem/FileSystem");
     var FileUtils = brackets.getModule("file/FileUtils");
-    var path = require.toUrl("./.brackets.json");
-    var file = FileSystem.getFileForPath(path);
 
-    FileUtils.readAsText(file).done(function (text) {
-        try {
-            JSON.parse(text);
-        } catch (e) {
-            console.log("JsonChecker: Error in " + path);
-        }
+    //Main json files of Brackets
+    var jsonFiles = [
+    "../../../brackets.config.dev.json", 
+    "../../../brackets.config.dist.json", 
+    "../../../brackets.config.json", 
+    "../../../config.json", 
+    "../../../package.json", 
+    "../../../supported-encodings.json",
+    "../../../../npm-shrinkwrap.json",
+    "../../../../build.json",
+    "../../../../.brackets.json",
+    "../../../../package.json"
+    ];
 
-    }).fail(function () {
-        console.log("JsonChecker: Error reading file " + path);
-    });
+   
+    function jsonParse(path) {
+
+        var result = new $.Deferred();
+        var file = FileSystem.getFileForPath(path);
+        var promise = FileUtils.readAsText(file).then(function (text) {
+            try {
+                JSON.parse(text);
+                result.resolve();
+            } catch (e) {
+                console.log("JsonChecker: Error in " + path);
+                result.resolve();
+            }
+
+        }).fail(function () {
+            console.log("JsonChecker: Error reading file " + path);
+            result.reject();
+        });
+
+   return result.promise();
+  }
+
+
+  var i = 0, path;
+  while(i < jsonFiles.length) {
+    path = require.toUrl(jsonFiles[i]);
+    jsonParse(path).then(i++);  
+    }
 
 });
