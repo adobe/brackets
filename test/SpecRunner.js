bcdce8c431892b27793a2c0281f74618fc59b9a8
Naveen Choudhary <navc@adobe.com>
2017-12-06 16:15:29 +0530

    Fixting test cases

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -73,6 +73,7 @@ define(function (require, exports, module) {
     require("utils/NodeDomain");
     require("utils/ColorUtils");
     require("preferences/PreferencesBase");
+    require("JSUtils/ScopeManager");
 
     // Load modules that self-register and just need to get included in the test-runner window
     require("document/ChangedDocumentTracker");
