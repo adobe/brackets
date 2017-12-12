2b5b98e023b8d13b6deb08c95a251b5ac6ccca6b
Boopesh Mahendran <bmahendr@adobe.com>
2017-12-12 16:26:10 +0530

    Remove unwanted changes

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1035,7 +1035,7 @@ define(function (require, exports, module) {
         this._codeMirror.on("focus", function () {
             self._focused = true;
             self.trigger("focus", self);
-
+            
         });
 
         this._codeMirror.on("blur", function () {
@@ -2221,7 +2221,7 @@ define(function (require, exports, module) {
             if (outerMode.name === 'htmlmixed' && primarySel.start.line === primarySel.end.line && primarySel.start.ch === primarySel.end.ch) {
                 var tagInfo = HTMLUtils.getTagInfo(this, primarySel.start, true),
                     tokenType = tagInfo.position.tokenType;
-
+ 
                 if (tokenType === HTMLUtils.ATTR_VALUE && tagInfo.attr.name.toLowerCase() === 'style') {
                     return 'css';
                 }
@@ -2754,10 +2754,10 @@ define(function (require, exports, module) {
             $holder.toggleClass("show-line-padding", Boolean(showLinePadding));
         });
     };
-
+    
     Editor.LINE_NUMBER_GUTTER_PRIORITY = LINE_NUMBER_GUTTER_PRIORITY;
     Editor.CODE_FOLDING_GUTTER_PRIORITY = CODE_FOLDING_GUTTER_PRIORITY;
-
+    
     // Set up listeners for preference changes
     editorOptions.forEach(function (prefName) {
         PreferencesManager.on("change", prefName, function () {
