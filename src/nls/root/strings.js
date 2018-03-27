17c2bf0b8d2d29bc36fae43377bc8bb1f95a361b
Bhavika Miglani <miglani@adobe.com>
2018-03-27 17:04:39 +0530

    Adding strings for AutoUpdate and Related Files features

--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -839,5 +839,31 @@ define({
     "DESCRIPTION_RECENT_FILES_NAV"                   : "Enable/disable navigation in recent files",
     "DESCRIPTION_LIVEDEV_WEBSOCKET_PORT"             : "Port on which WebSocket Server runs for Live Preview",
     "DESCRIPTION_LIVE_DEV_HIGHLIGHT_SETTINGS"        : "Live Preview Highlight settings",
-    "DESCRIPTION_LIVEDEV_ENABLE_REVERSE_INSPECT"     : "false to disable live preview reverse inspect"
+    "DESCRIPTION_LIVEDEV_ENABLE_REVERSE_INSPECT"     : "false to disable live preview reverse inspect",
+    
+    // Strings for Auto Update
+    "DOWNLOAD_FAILED"                                : "Download failed.",
+    "DOWNLOAD_COMPLETE"                              : "Download complete!",
+    "UPDATE_SUCCESSFUL"                              : "Update successful!",
+    "UPDATE_FAILED"                                  : "Update failed!",
+    "VALIDATION_FAILED"                              : "Validation failed!",
+    "INITIALISATION_FAILED"                          : "Initialization failed!",
+    "CLEANUP_FAILED"                                 : "Clean-up failed!",
+    "WARNING_TYPE"                                   : "Warning!",
+    "CLICK_RESTART_TO_UPDATE"                        : "Click Restart to update Brackets.",
+    "UPDATE_ON_NEXT_LAUNCH"                          : "The update will be applied on relaunch.",
+    "VALIDATE_EXTENSIONS"                            : "Please validate all your extensions.",
+    "GO_TO_SITE"                                     : "Go to <a href = \"http://brackets.io/\"> brackets.io </a> to retry.",
+    "INTERNET_UNAVAILABLE"                           : "No Internet connection available.",
+    "UPDATEDIR_READ_FAILED"                          : "Update directory could not be read.",
+    "UPDATEDIR_CLEAN_FAILED"                         : "Update directory could not be cleaned.",
+	"INITIAL_DOWNLOAD"							 	 : "Downloading Update...",
+	"RETRY_DOWNLOAD"								 : "Download failed. Retrying...Attempt ",
+	"VALIDATING_INSTALLER"							 : "Download complete! Validating Installer...",
+	"CHECKSUM_DID_NOT_MATCH"						 : "Checksum didn't match.",
+	"INSTALLER_NOT_FOUND"							 : "Installer not found.",
+	"DOWNLOAD_ERROR"								 : "Error occurred while downloading.",
+    
+    // Strings for Related Files
+    "CMD_FIND_RELATED_FILES"                         : "Find Related Files"
 });
