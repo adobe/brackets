fb95fb9568dac7cc892b4aa34ec70438383f54f6
Boopesh Mahendran <boopeshmahendran@gmail.com>
2018-05-02 23:49:08 -0700

    Change styles of the drag Image

--- src/project/FileTreeView.js
+++ src/project/FileTreeView.js
@@ -297,10 +297,8 @@ define(function (require, exports, module) {
 
         setDragImage: function(e) {
             var div = window.document.createElement('div');
-            div.style.position = 'absolute';
-            div.style.backgroundColor = '#2D2E30';
-            div.style.color = '#fff';
             div.textContent = this.props.name;
+            div.classList.add('jstree-dragImage');
             window.document.body.appendChild(div);
             e.dataTransfer.setDragImage(div, -10, -10);
             setTimeout(function() {
