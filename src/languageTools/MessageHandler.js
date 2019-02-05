362073963e8e2c8c01b678540245fbb082cd1341
Shubham Yadav <shubyada@shubhams-mbp.corp.adobe.com>
2019-02-05 17:21:53 +0530

    Adding handlers on Language Client Proxy, fixing eslint errors

--- src/languageTools/MessageHandler.js
+++ src/languageTools/MessageHandler.js
@@ -21,120 +21,208 @@
  *
  */
 
+/*eslint no-console: 0*/
+/*eslint max-len: ["error", { "code": 200 }]*/
 define(function (require, exports, module) {
     "use strict";
-    
-    var ToolingInfo     = JSON.parse(require("text!languageTools/ToolingInfo.json")),
-        NodeDomain = require("utils/NodeDomain"),
-        BracketsToNodeInterface = require("languageTools/Interface/nodeInterface").BracketsToNodeInterface,
-        ProjectManager  = require("project/ProjectManager");
-    
-    var messages = ToolingInfo.getMessages,
-        capabilities = ToolingInfo.getCapabilities,
-        nodeDomains = {},
-        nodeInterfaces = {},
-        _currentNodeDomain = null,
-        lastRequestForLanguageClient = {};
 
-    
-    function _createNodeDomain(domainName, domainPath)
-    {
-        return new NodeDomain(domainName, domainPath); ;
+    var ToolingInfo = JSON.parse(require("text!languageTools/ToolingInfo.json")),
+        NodeDomain = require("utils/NodeDomain"),
+        EditorManager = require('editor/EditorManager'),
+        BracketsToNodeInterface = require("languageTools/Interface/nodeInterface").BracketsToNodeInterface,
+        ProjectManager = require("project/ProjectManager");
+
+    var nodeDomains = {},
+        nodeInterfaces = {};
+
+
+    function _createNodeDomain(domainName, domainPath) {
+        return new NodeDomain(domainName, domainPath);
     }
-    
+
+    function _createParams(type, params) {
+        return {
+            type: type,
+            params: params
+        };
+    }
     /*
         RequestParams creator - sendNotifications/request
     */
     function constructRequestParams(type, params) {
-        var jsonParams = {},
-            params = params ? params :{};
-        
+        var jsonParams = null,
+            activeEditor = EditorManager.getActiveEditor();
+
+        params = params ? params : {};
         switch (type) {
-            case ToolingInfo.LANGUAGE_SERVICE.START : {
+        case ToolingInfo.LANGUAGE_SERVICE.START:
+            {
                 jsonParams = {
-                    rootPath : ProjectManager.getProjectRoot().fullPath,
-                    capabilities : params.capabilities ? params.capabilities : false
+                    rootPath: params.rootPath ? params.rootPath : ProjectManager.getProjectRoot().fullPath,
+                    capabilities: params.capabilities ? params.capabilities : false
+                };
+                break;
+            }
+        case ToolingInfo.FEATURES.CODE_HINTS:
+        case ToolingInfo.FEATURES.PARAMETER_HINTS:
+        case ToolingInfo.FEATURES.JUMP_TO_DECLARATION:
+        case ToolingInfo.FEATURES.JUMP_TO_DEFINITION:
+        case ToolingInfo.FEATURES.JUMP_TO_IMPL:
+            {
+                jsonParams = _createParams(type, {
+                    filePath: params.filePath ? params.filePath : (activeEditor.document.file._path || activeEditor.document.file.fullPath),
+                    cursorPos: params.cursorPos ? params.cursorPos : activeEditor.getCursorPos()
+                });
+                break;
+            }
+        case ToolingInfo.FEATURES.CODE_HINT_INFO:
+            {
+                jsonParams = _createParams(type, params);
+                break;
+            }
+        case ToolingInfo.FEATURES.FIND_REFERENCES:
+            {
+                jsonParams = _createParams(type, {
+                    filePath: params.filePath ? params.filePath : (activeEditor.document.file._path || activeEditor.document.file.fullPath),
+                    cursorPos: params.cursorPos ? params.cursorPos : activeEditor.getCursorPos(),
+                    includeDeclaration: params.includeDeclaration ? params.includeDeclaration : false
+                });
+                break;
+            }
+        case ToolingInfo.FEATURES.DOCUMENT_SYMBOLS:
+            {
+                jsonParams = _createParams(type, {
+                    filePath: params.filePath ? params.filePath : (activeEditor.document.file._path || activeEditor.document.file.fullPath)
+                });
+                break;
+            }
+        case ToolingInfo.FEATURES.PROJECT_SYMBOLS:
+            {
+                if (params && params.query && typeof params.query === "string") {
+                    jsonParams = _createParams(type, params);
                 }
                 break;
             }
         }
-        
+
         return jsonParams;
     }
 
     /*
         ReponseParams transformer - used by OnNotifications
     */
-    function constructNotificationParams(type) {
-        
+    function constructNotificationParams(type, params) {
+        var jsonParams = null;
+
+        params = params ? params : {};
+        switch (type) {
+        case ToolingInfo.SYNCHRONIZE_EVENTS.DOCUMENT_OPENED:
+            {
+                if (params.filePath && params.fileContent && params.languageId) {
+                    jsonParams = _createParams(type, params);
+                }
+                break;
+            }
+        case ToolingInfo.SYNCHRONIZE_EVENTS.DOCUMENT_CHANGED:
+            {
+                if (params.filePath && params.fileContent) {
+                    jsonParams = _createParams(type, params);
+                }
+                break;
+            }
+        case ToolingInfo.SYNCHRONIZE_EVENTS.DOCUMENT_SAVED:
+            {
+                if (params.filePath) {
+                    jsonParams = _createParams(type, params);
+                }
+                break;
+            }
+        case ToolingInfo.SYNCHRONIZE_EVENTS.DOCUMENT_CLOSED:
+            {
+                if (params.filePath) {
+                    jsonParams = _createParams(type, params);
+                }
+                break;
+            }
+        case ToolingInfo.SYNCHRONIZE_EVENTS.PROJECT_FOLDERS_CHANGED:
+            {
+                    //TODO
+                jsonParams = _createParams(type, params);
+                break;
+            }
+        }
+
+        return jsonParams;
     }
-    
+
     function loadLanguageClientDomain(clientName, domainPath) {
         //generate a random hash name for the domain, this is the client id
         var domainName = clientName,
             result = $.Deferred(),
             languageClientDomain = _createNodeDomain(domainName, domainPath);
-        
+
         if (languageClientDomain) {
             languageClientDomain.promise()
                 .done(function () {
-                    console.log(domainPath+ " domain successfully created");
-                    result.resolve(languageClientDomain); 
+                    console.log(domainPath + " domain successfully created");
+                    result.resolve(languageClientDomain);
                 })
                 .fail(function (err) {
-                    console.error(domainPath+" domain could not be created.");
+                    console.error(domainPath + " domain could not be created.");
                     result.reject();
                 });
         } else {
-            console.error(domainPath+" domain could not be created.");
+            console.error(domainPath + " domain could not be created.");
             result.reject();
         }
-        
+
         return result;
     }
-    
+
     function createNodeInterfaceForDomain(languageClientDomain) {
         var nodeInterface = new BracketsToNodeInterface(languageClientDomain);
 
-        return nodeInterface; 
+        return nodeInterface;
     }
-    
+
     function registerLanguageClientInterface(domainName, languageClientInterface) {
         nodeInterfaces[domainName] = languageClientInterface;
     }
-    
+
     function registerLanguageClientDomain(domainName, languageClientDomain) {
         nodeDomains[domainName] = languageClientDomain;
     }
-    
-    function _getNodeDomain(domainName) {
+
+    function getNodeDomain(domainName) {
         return nodeDomains[domainName];
     }
-    
-    function _getNodeInterface(domainName) {
+
+    function getNodeInterface(domainName) {
         return nodeInterfaces[domainName];
     }
-    
+
     function initiateLanguageClient(clientName, clientFilePath) {
         var result = $.Deferred();
-        
+
         loadLanguageClientDomain(clientName, clientFilePath)
             .then(function (languageClientDomain) {
                 var languageClientInterface = createNodeInterfaceForDomain(languageClientDomain);
-            
+
                 registerLanguageClientDomain(clientName, languageClientDomain);
                 registerLanguageClientInterface(clientName, languageClientInterface);
-            
+
                 result.resolve({
-                    name : clientName,
+                    name: clientName,
                     interface: languageClientInterface
                 });
             }, result.reject);
-        
+
         return result;
     }
 
     exports.initiateLanguageClient = initiateLanguageClient;
     exports.constructRequestParams = constructRequestParams;
     exports.constructNotificationParams = constructNotificationParams;
+    exports.getNodeDomain = getNodeDomain;
+    exports.getNodeInterface = getNodeInterface;
 });
