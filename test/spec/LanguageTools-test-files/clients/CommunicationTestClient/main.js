619949bf6d069cc7d17935a4e1f9a3b3d3bd141d
Shubham Yadav <shubyada@adobe.com>
2019-03-20 09:30:11 +0530

    Adding banner, fixing lint errors

--- test/spec/LanguageTools-test-files/clients/CommunicationTestClient/main.js
+++ test/spec/LanguageTools-test-files/clients/CommunicationTestClient/main.js
@@ -1,16 +1,33 @@
+/*
+ * Copyright (c) 2019 - present Adobe. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+/*eslint max-len: ["error", { "code": 200 }]*/
+/*eslint indent: 0*/
 define(function (require, exports, module) {
     "use strict";
 
     var LanguageTools = brackets.getModule("languageTools/LanguageTools"),
         AppInit = brackets.getModule("utils/AppInit"),
-        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
-        ProjectManager = brackets.getModule("project/ProjectManager"),
-        CodeHintManager = brackets.getModule("editor/CodeHintManager"),
-        ParameterHintManager = brackets.getModule("features/ParameterHintsManager"),
-        JumpToDefManager = brackets.getModule("features/JumpToDefManager"),
-        CodeInspection = brackets.getModule("language/CodeInspection"),
-        DefaultProviders = brackets.getModule("languageTools/DefaultProviders"),
-        DefaultEventHandlers = brackets.getModule("languageTools/DefaultEventHandlers");
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
     var clientFilePath = ExtensionUtils.getModulePath(module, "client.js"),
         clientName = "CommunicationTestClient",
@@ -27,16 +44,16 @@ define(function (require, exports, module) {
         if ($.isFunction(clientPromise.promise)) {
             clientPromise.then(function (textClient) {
                 client = textClient;
-                
+
                 client.sendCustomRequest({
                     messageType: "brackets",
                     type: "setModulePath",
                     params: {
                         modulePath: ExtensionUtils.getModulePath(module)
                     }
-                }).then(retval.resolve)
-                
-                
+                }).then(retval.resolve);
+
+
             }, retval.reject);
         } else {
             retval.reject();
@@ -47,5 +64,5 @@ define(function (require, exports, module) {
 
     exports.getClient = function () {
         return client;
-    }
+    };
 });
