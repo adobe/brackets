006612fe119f25f8b33123e485dedcd557e5dfd6
Nitesh Kumar <niteskum@adobe.com>
2019-03-28 03:25:09 +0530

    Added some unit test Cases

---
+++ src/extensions/default/PhpTooling/unittest-files/test/test2.php
@@ -0,0 +1,30 @@
+<?php
+
+$x = 75;
+
+$x = 34;
+
+namespace test;
+abstract class TestCase extends testA
+{
+    use testA;
+}
+
+$A11 = 23;
+$A12 = 34;
+$A13 = 45;
+$A1
+
+fo
+
+    
+$
+
+function watchparameterhint() {
+    echo "Hello World!";
+}
+$A11()
+fopen("",)
+watchparameterhint()
+
+?>
