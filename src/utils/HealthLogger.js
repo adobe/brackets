a09cc848b87655b35594dbe0ecd62a1b43cbcd78
niteskum <niteskum@adobe.com>
2019-04-04 14:22:50 +0530

    Addressed review comments

--- src/utils/HealthLogger.js
+++ src/utils/HealthLogger.js
@@ -218,18 +218,18 @@ define(function (require, exports, module) {
         function _sendData(fileSize) {
             var subType = "";
 
-            if(fileSize/1024 < 1) {
+            if(fileSize/1024 <= 1) {
 
-                if(fileSize === -1) {
+                if(fileSize < 0) {
                     subType = "";
                 }
-                if(fileSize < 10) {
+                if(fileSize <= 10) {
                     subType = "Size_0_10KB";
-                } else if (fileSize < 50) {
+                } else if (fileSize <= 50) {
                     subType = "Size_10_50KB";
-                } else if (fileSize < 100) {
+                } else if (fileSize <= 100) {
                     subType = "Size_50_100KB";
-                } else if (fileSize < 500) {
+                } else if (fileSize <= 500) {
                     subType = "Size_100_500KB";
                 } else {
                     subType = "Size_500KB_1MB";
@@ -237,9 +237,9 @@ define(function (require, exports, module) {
 
             } else {
                 fileSize = fileSize/1024;
-                if(fileSize < 2) {
+                if(fileSize <= 2) {
                     subType = "Size_1_2MB";
-                } else if(fileSize < 5) {
+                } else if(fileSize <= 5) {
                     subType = "Size_2_5MB";
                 } else {
                     subType = "Size_Above_5MB";
