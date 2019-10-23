8df750316e1274c22a8402fe38feb785784200b8
Parth Nagarkar <nagarkarparth875@gmail.com>
2019-10-23 23:40:36 +0530

    Updated FileUtils.js

--- src/file/FileUtils.js
+++ src/file/FileUtils.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2012 - present Adobe Systems Incorporated. All rights reserved.
  *
-    These changes have been made by the GOAT
+    These changes have been made by the GOAT for the hacktoberfesssst
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
