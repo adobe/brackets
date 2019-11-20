18897afc976ff50e4ca9bf5b4995875979815362
walf <walf@adobe.com>
2019-11-20 09:55:00 -0800

    Updated by ALF automation.

--- src/nls/fr/strings.js
+++ src/nls/fr/strings.js
@@ -900,5 +900,8 @@ define({
 	"CMD_FIND_PROJECT_SYMBOLS": "Rechercher des symboles de projet",
 
    // Remote debugging enabled
-	"REMOTE_DEBUGGING_ENABLED": "Remote debugging enabled on localhost:"
+	"REMOTE_DEBUGGING_ENABLED": "Remote debugging enabled on localhost:",
+
+   // Remote debugging port argument is invalid
+	"REMOTE_DEBUGGING_PORT_INVALID": "Cannot enable remote debugging on port {0}. Port numbers should be between {1} and {2}."
 });
