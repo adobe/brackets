34ba03413d6c82ae15f9fc5f2abd95e5b14b5b79
abose <arun@core.ai>
2021-08-23 20:09:14 +0530

    Fix build failure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,7 @@ module.exports = function (grunt) {
                     }
                 ]
             },
-            thirdpartys: {
+            thirdparty: {
                 files: [
                     {
                         expand: true,
